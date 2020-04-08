319056b2da1c0d1916fee60b280e0daed7b20af6
Hammad Mazhar <hmazhar@nvidia.com>
2020-04-08 13:45:04 -0700

    restructure shapenet extension so its like the rest

--- source/extensions/omni.isaac/shapenet/premake5.lua
+++ source/extensions/omni.isaac/shapenet/premake5.lua
@@ -7,9 +7,17 @@ local ext_folder = "_build/$platform/$config/exts/"..ext_id
 
 group ("extensions/"..ext_id)
 
-repo_build.prebuild_link {
-    { ext_source.."/python/scripts", ext_folder.."/omni/isaac/shapenet/scripts" },
-}
+    repo_build.prebuild_link {
+        { ext_source.."/config", ext_folder.."/config" },
+    }
+
+    repo_build.prebuild_link {
+        { ext_source.."/python/scripts", ext_folder.."/omni/isaac/shapenet/scripts" },
+    }
+
+    repo_build.prebuild_copy {
+        { ext_source.."/python/*.py", ext_folder.."/omni/isaac/shapenet" },
+    }
 
     -- Example of python extension. Contains python sources, doesn't build or run, only for MSVS.
     if os.target() == "windows" then
