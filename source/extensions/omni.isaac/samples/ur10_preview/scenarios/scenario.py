c8ee86c5666eca88a422807430476edf55805ebe
Renato Gasoto <rgasoto@nvidia.com>
2020-04-24 17:41:00 -0400

    change fof_bmw_assets folder

--- source/extensions/omni.isaac/samples/ur10_preview/scenarios/scenario.py
+++ source/extensions/omni.isaac/samples/ur10_preview/scenarios/scenario.py
@@ -173,11 +173,11 @@ class Scenario:
         self._executor = None
         self._created = False
 
-        self.asset_path = "omni:/Projects/gtc_sj_2020"
+        self.asset_path = "omni:/Projects/fof_bmw"
         # use local content if not connected to omni server
         if len(omni.kit.connectionhub.get_connection_hub_interface().get_connection_handles()) <= 0:
             print("Use local content")
-            self.asset_path = "art_assets/gtc_sj_2020"
+            self.asset_path = "art_assets/fof_bmw"
         else:
             print("Use server content")
 
