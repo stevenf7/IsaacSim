96d55149376dee453511f5121517aa3d3c5aea0f
Hammad Mazhar <hmazhar@nvidia.com>
2020-04-27 15:34:43 -0700

    Improve URDF importer file picking

--- source/extensions/omni.isaac/urdf/python/scripts/extension.py
+++ source/extensions/omni.isaac/urdf/python/scripts/extension.py
@@ -17,18 +17,22 @@ class Extension(omni.ext.IExt):
         self._urdf_interface = _urdf.acquire_urdf_interface()
         menu_path = f"Window/{EXTENSION_NAME}"
         self._window = omni.kit.ui.Window(EXTENSION_NAME, 960, 600, menu_path=menu_path)
+        self._btn_load = self._window.layout.add_child(omni.kit.ui.Button("Load URDF"))
+        self._btn_load.set_clicked_fn(self._select_file)
 
-        layout = self._window.layout.add_child(omni.kit.ui.RowColumnLayout(2))
-        layout.set_column_width(0, 150)
+    def _select_picked_folder_callback(self, path):
+        if path.startswith("file:"):
+            path = path[5:]
+            self._urdf_interface.importUrdf(path)
+        else:
+            print("Only local paths supported currently")
 
-        self.urdf_path = layout.add_child(omni.kit.ui.TextBox("/path/to/robot.urdf"))
-        self.urdf_path.width = -1
-        self._create_btn = self._window.layout.add_child(omni.kit.ui.Button("Load URDF"))
-        self._create_btn.set_clicked_fn(self._on_create_fn)
+    def _select_file(self, btn_widget):
+        self._filepicker = omni.kit.ui.FilePicker("Select URDF File", file_type=omni.kit.ui.FileDialogSelectType.FILE)
+        self._filepicker.set_file_selected_fn(self._select_picked_folder_callback)
+        self._filepicker.add_filter("URDF Files (*.urdf)", r".*.urdf$")
+        self._filepicker.show()
 
     def on_shutdown(self):
         print("Shutting down URDF Extension")
         _urdf.release_urdf_interface(self._urdf_interface)
-
-    def _on_create_fn(self, widget):
-        self._urdf_interface.importUrdf(self.urdf_path.value)
