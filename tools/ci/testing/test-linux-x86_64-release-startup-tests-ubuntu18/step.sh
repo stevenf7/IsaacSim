30cad9f75f98494e4db56b65cf72ce3a1df80b64
Sheikh Dawood Abdul Ajees <sabdulajees@nvidia.com>
2020-05-07 12:50:47 -0700

    Add omniverse-kit-robotics package

--- tools/ci/testing/test-linux-x86_64-release-startup-tests-ubuntu18/step.sh
+++ tools/ci/testing/test-linux-x86_64-release-startup-tests-ubuntu18/step.sh
@@ -20,3 +20,23 @@ fi
 
 # publish artifacts to teamcity
 echo "##teamcity[publishArtifacts '_builtpackages/*']"
+
+
+
+if [ ! -z "$TEAMCITY_VERSION" ]
+then
+    package="--from-package"
+    ARCHIVE_PATTERN="_builtpackages/omniverse-kit-robotics*.7z"
+
+    # Startup Test
+    "$SCRIPT_DIR/../../../test_runner.sh" --suite startuptest --config release $package -e="--carb/rtx/shaderDb/obfuscateCode=true" -e="--carb/rtx/materialDb/compileMdlAsLibrary=true" $*
+
+    # Package shader cache
+    if [ "$package" == "--from-package" ]
+    then
+        "$SCRIPT_DIR/../../../packman/python.sh" "$SCRIPT_DIR/../../../repoman/package_cache.py" --platform linux-x86_64 --config release
+    fi
+
+    # publish artifacts to teamcity
+    echo "##teamcity[publishArtifacts '_builtpackages/*']"
+fi
