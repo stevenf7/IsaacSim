17a0b667771ad5affe1c192febf172ba1e2bcb45
Sheikh Dawood Abdul Ajees <sabdulajees@nvidia.com>
2020-05-21 13:29:45 -0700

    Enable Windows tests

--- tools/ci/build-and-packaging/linux-x86_64/step-debug.sh
+++ tools/ci/build-and-packaging/linux-x86_64/step-debug.sh
@@ -8,34 +8,34 @@ ROOT_DIR="$SCRIPT_DIR/../../../.."
 if [ -z "$TEAMCITY_VERSION" ]
 then
 # Verify formatting
-echo "##teamcity[progressStart 'Verify formatting...']"
+echo "##teamcity[blockOpened name='Verify formatting...']"
 "$ROOT_DIR/format_code.sh" --verify
-echo "##teamcity[progressFinish 'Verify formatting...']"
+echo "##teamcity[blockClosed name='Verify formatting...']"
 fi
 
 # Full rebuild
-echo "##teamcity[progressStart 'Full rebuild...']"
+echo "##teamcity[blockOpened name='Full rebuild...']"
 "$ROOT_DIR/build.sh" -c
 "$ROOT_DIR/build.sh" -d
-echo "##teamcity[progressFinish 'Full rebuild...']"
+echo "##teamcity[blockClosed name='Full rebuild...']"
 
 # Docs
-echo "##teamcity[progressStart 'Docs...']"
+echo "##teamcity[blockOpened name='Docs...']"
 "$ROOT_DIR/tools/build_docs.sh" -c debug
-echo "##teamcity[progressFinish 'Docs...']"
+echo "##teamcity[blockClosed name='Docs...']"
 
 # Gathering licenses
-echo "##teamcity[progressStart 'Gathering licenses...']"
+echo "##teamcity[blockOpened name='Gathering licenses...']"
 "$ROOT_DIR/tools/licensing.sh" gather \
     -p $ROOT_DIR/deps/isaac-sim.packman.xml \
     $ROOT_DIR/deps/kit-sdk.packman.xml \
     $ROOT_DIR/deps/rtx-plugins.packman.xml \
     $ROOT_DIR/deps/omni-physics.packman.xml \
     -d $ROOT_DIR/_build
-echo "##teamcity[progressFinish 'Gathering licenses...']"
+echo "##teamcity[blockClosed name='Gathering licenses...']"
 
 # Validating licenses
-#echo "##teamcity[progressStart 'Validating licenses...']"
+#echo "##teamcity[blockOpened name='Validating licenses...']"
 #"$ROOT_DIR/tools/licensing.sh" validate \
 #    -p $ROOT_DIR/deps/isaac-sim.packman.xml \
 #    $ROOT_DIR/deps/kit-sdk.packman.xml \
@@ -43,16 +43,21 @@ echo "##teamcity[progressFinish 'Gathering licenses...']"
 #    $ROOT_DIR/deps/omni-physics.packman.xml \
 #    -d $ROOT_DIR/_build \
 #    -b linux-x86_64/release
-#echo "##teamcity[progressFinish 'Validating licenses...']"
+#echo "##teamcity[blockClosed name='Validating licenses...']"
 
 # Package
-echo "##teamcity[progressStart 'Packaging...']"
+echo "##teamcity[blockOpened name='Build packages...']"
+echo "##teamcity[progressMessage 'Packaging test_runner...']"
 "$ROOT_DIR/tools/package.sh" -m test_runner -c debug
+echo "##teamcity[progressMessage 'Packaging docs...']"
 "$ROOT_DIR/tools/package.sh" -m docs -c debug
+echo "##teamcity[progressMessage 'Packaging isaac-sim...']"
 "$ROOT_DIR/tools/package.sh" -m isaac-sim -c debug
+echo "##teamcity[progressMessage 'Packaging omniverse-kit-robotics...']"
 "$ROOT_DIR/tools/package.sh" -m omniverse-kit-robotics -c debug
+echo "##teamcity[progressMessage 'Packaging omni_domain_randomization...']"
 "$ROOT_DIR/tools/package.sh" -m omni_domain_randomization -c debug
-echo "##teamcity[progressFinish 'Packaging...']"
+echo "##teamcity[blockClosed name='Build packages...']"
 
 # publish artifacts to teamcity
 echo "##teamcity[publishArtifacts '_build/packages']"
--- tools/ci/testing/test-linux-x86_64-release-startup-tests-ubuntu18/step.sh
+++ tools/ci/testing/test-linux-x86_64-release-startup-tests-ubuntu18/step.sh
@@ -10,34 +10,35 @@ then
 fi
 
 # Startup Test
-echo "##teamcity[testSuiteStarted name='isaac-sim']"
+echo "##teamcity[testSuiteStarted name='isaac-sim-startuptests']"
 "$SCRIPT_DIR/../../../test_runner.sh" --suite startuptest --config release $package -e="--carb/rtx/shaderDb/obfuscateCode=true" -e="--carb/rtx/materialDb/compileMdlAsLibrary=true" $*
+echo "##teamcity[testSuiteFinished name='isaac-sim-startuptests']"
 
-# Package shader cache
+
 if [ ! -z "$TEAMCITY_VERSION" ]
 then
+    # Package shader cache
+    echo "##teamcity[testSuiteStarted name='Packaging the shader cache...']"
     "$SCRIPT_DIR/../../../packman/python.sh" "$SCRIPT_DIR/../../../repoman/package_cache.py" --platform linux-x86_64 --config release --experience isaac-sim
+    echo "##teamcity[testSuiteFinished name='Packaging the shader cache...']"
+    
+    # publish artifacts to teamcity
+    echo "##teamcity[publishArtifacts '_builtpackages/isaac-sim*.7z']"
 fi
-
-echo "##teamcity[testSuiteFinished name='isaac-sim']"
-
-# publish artifacts to teamcity
-echo "##teamcity[publishArtifacts '_builtpackages/isaac-sim*.7z']"
-
-
 
 if [ ! -z "$TEAMCITY_VERSION" ]
 then
     export ARCHIVE_PATTERN="_builtpackages/omniverse-kit-robotics*.7z"
 
     # Startup Test
-    echo "##teamcity[testSuiteStarted name='omniverse-kit-robotics']"
+    echo "##teamcity[testSuiteStarted name='omniverse-kit-robotics-startuptests']"
     "$SCRIPT_DIR/../../../test_runner.sh" --suite startuptest --config release $package -e="--carb/rtx/shaderDb/obfuscateCode=true" -e="--carb/rtx/materialDb/compileMdlAsLibrary=true" $*
+    echo "##teamcity[testSuiteFinished name='omniverse-kit-robotics-startuptests']"
 
     # Package shader cache
+    echo "##teamcity[testSuiteStarted name='Packaging the shader cache...']"
     "$SCRIPT_DIR/../../../packman/python.sh" "$SCRIPT_DIR/../../../repoman/package_cache.py" --platform linux-x86_64 --config release --experience omniverse-kit-robotics
-
-    echo "##teamcity[testSuiteFinished name='omniverse-kit-robotics']"
+    echo "##teamcity[testSuiteFinished name='Packaging the shader cache...']"
     
     # publish artifacts to teamcity
     echo "##teamcity[publishArtifacts '_builtpackages/omniverse-kit-robotics*.7z']"
