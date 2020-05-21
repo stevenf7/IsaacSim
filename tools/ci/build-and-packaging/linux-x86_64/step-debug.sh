ca35c49df3c934b35fcb4fbc5c83685f5d5e424c
Sheikh Dawood <sabdulajees@nvidia.com>
2020-05-21 06:18:38 -0500

    Split release and debug builds for TeamCity

---
+++ tools/ci/build-and-packaging/linux-x86_64/step-debug.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
+ROOT_DIR="$SCRIPT_DIR/../../../.."
+
+if [ -z "$TEAMCITY_VERSION" ]
+then
+# Verify formatting
+echo "##teamcity[progressStart 'Verify formatting...']"
+"$ROOT_DIR/format_code.sh" --verify
+echo "##teamcity[progressFinish 'Verify formatting...']"
+fi
+
+# Full rebuild
+echo "##teamcity[progressStart 'Full rebuild...']"
+"$ROOT_DIR/build.sh" -c
+"$ROOT_DIR/build.sh" -d
+echo "##teamcity[progressFinish 'Full rebuild...']"
+
+# Docs
+echo "##teamcity[progressStart 'Docs...']"
+"$ROOT_DIR/tools/build_docs.sh" -c debug
+echo "##teamcity[progressFinish 'Docs...']"
+
+# Gathering licenses
+echo "##teamcity[progressStart 'Gathering licenses...']"
+"$ROOT_DIR/tools/licensing.sh" gather \
+    -p $ROOT_DIR/deps/isaac-sim.packman.xml \
+    $ROOT_DIR/deps/kit-sdk.packman.xml \
+    $ROOT_DIR/deps/rtx-plugins.packman.xml \
+    $ROOT_DIR/deps/omni-physics.packman.xml \
+    -d $ROOT_DIR/_build
+echo "##teamcity[progressFinish 'Gathering licenses...']"
+
+# Validating licenses
+#echo "##teamcity[progressStart 'Validating licenses...']"
+#"$ROOT_DIR/tools/licensing.sh" validate \
+#    -p $ROOT_DIR/deps/isaac-sim.packman.xml \
+#    $ROOT_DIR/deps/kit-sdk.packman.xml \
+#    $ROOT_DIR/deps/rtx-plugins.packman.xml \
+#    $ROOT_DIR/deps/omni-physics.packman.xml \
+#    -d $ROOT_DIR/_build \
+#    -b linux-x86_64/release
+#echo "##teamcity[progressFinish 'Validating licenses...']"
+
+# Package
+echo "##teamcity[progressStart 'Packaging...']"
+"$ROOT_DIR/tools/package.sh" -m test_runner -c debug
+"$ROOT_DIR/tools/package.sh" -m docs -c debug
+"$ROOT_DIR/tools/package.sh" -m isaac-sim -c debug
+"$ROOT_DIR/tools/package.sh" -m omniverse-kit-robotics -c debug
+"$ROOT_DIR/tools/package.sh" -m omni_domain_randomization -c debug
+echo "##teamcity[progressFinish 'Packaging...']"
+
+# publish artifacts to teamcity
+echo "##teamcity[publishArtifacts '_build/packages']"
+
