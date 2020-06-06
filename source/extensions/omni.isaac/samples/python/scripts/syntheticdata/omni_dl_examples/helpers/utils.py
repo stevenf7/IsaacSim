dfea8d1736476d63ef13f269a66a4839545d27e0
Jean-Francois Lafleche <jlafleche@nvidia.com>
2020-06-05 17:44:36 -0700

    Initialize with simple demo
    Signed-off-by: JF <jfl@outlook.com>

---
+++ source/extensions/omni.isaac/samples/python/scripts/syntheticdata/omni_dl_examples/helpers/utils.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+
+import torch
+import torch.nn.functional as F
+
+
+def torch_isin(tensor, test_elements):
+    """PyTorch version of the numpy `is1d` function
+    https://github.com/numpy/numpy/blob/v1.17.0/numpy/lib/arraysetops.py#L484
+
+    Test whether each elements of `tensor` are also present in `test_elements`.
+
+    Returns:
+        Bool array of same shape as `tensor` with values `True` if the
+        corresponding tensor element is also found in `test_elements`.
+    """
+
+    ar1, rev_idx = torch.unique(tensor, return_inverse=True)
+    ar2 = torch.unique(test_elements)
+
+    ar = torch.cat((ar1, ar2))
+
+    # Pytorch argsort is not stable, must use numpy with 'mergesort'.
+    order = ar.cpu().numpy().argsort(kind="mergesort")
+    sar = ar[order]
+
+    bool_ar = sar[1:] == sar[:-1]
+
+    flag = F.pad(bool_ar, (0, 1), value=False)
+    ret = torch.zeros(ar.shape, dtype=bool, device=ar.device)
+    ret[order] = flag
+
+    return ret[rev_idx]
---
+++ source/extensions/omni.isaac/samples/python/scripts/syntheticdata/omni_dl_examples/syntheticdata_demo/demo.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+
+"""Demonstration of using OmniKit to generate a scene, collect groundtruth and visualize
+the results.
+"""
+
+
+import random
+import numpy as np
+from pxr import UsdGeom, Semantics
+from omni_dl_examples.helpers import visualization as vis
+from omni_dl_examples.helpers.camera import project_points
+from omni_dl_examples.helpers.omnikit import OmniKitHelper
+from omni_dl_examples.helpers.syntheticdata import SyntheticDataHelper
+import matplotlib.pyplot as plt
+
+
+TRANSLATION_RANGE = 300.0
+SCALE = 50.0
+
+
+def main():
+    kit = OmniKitHelper()
+    sd_helper = SyntheticDataHelper()
+
+    # SCENE SETUP
+    # Get the current stage
+    stage = kit.get_stage()
+
+    # Add a distant light
+    stage.DefinePrim("/World/Light", "DistantLight")
+
+    # Create 10 randomly positioned and coloured spheres and cube
+    # We will assign each a semantic label based on their shape (sphere/cube)
+    for i in range(10):
+        prim_type = random.choice(["Cube", "Sphere"])
+        prim = stage.DefinePrim(f"/World/cube{i}", prim_type)
+        translation = np.random.rand(3) * TRANSLATION_RANGE
+        UsdGeom.XformCommonAPI(prim).SetTranslate(translation.tolist())
+        UsdGeom.XformCommonAPI(prim).SetScale((SCALE, SCALE, SCALE))
+        prim.GetAttribute("primvars:displayColor").Set([np.random.rand(3).tolist()])
+
+        # Add semantic label based on prim type
+        sem = Semantics.SemanticsAPI.Apply(prim, "Semantics")
+        sem.CreateSemanticTypeAttr()
+        sem.CreateSemanticDataAttr()
+        sem.GetSemanticTypeAttr().Set("class")
+        sem.GetSemanticDataAttr().Set(prim_type)
+
+    # Get groundtruth
+    gt = sd_helper.get_groundtruth(
+        [
+            "rgb",
+            "depth",
+            "boundingBox2DTight",
+            "boundingBox2DLoose",
+            "instanceSegmentation",
+            "semanticSegmentation",
+            "boundingBox3D",
+            "camera",
+        ]
+    )
+
+    # GROUNDTRUTH VISUALIZATION
+    def to_numpy(data):
+        """Helper to ensure data is on the CPU as a numpy array.
+        """
+        if isinstance(data, np.ndarray):
+            return data
+        elif type(data).__name__ == "Tensor":
+            return data.cpu().numpy()
+        else:
+            raise ValueError(f"Unable to convert to numpy data of type {type(data)}.")
+
+    # Setup a figure
+    _, axes = plt.subplots(2, 4, figsize=(20, 7))
+    axes = axes.flat
+    for ax in axes:
+        ax.axis("off")
+
+    # RGB
+    axes[0].set_title("RGB")
+    for ax in axes[:-1]:
+        ax.imshow(to_numpy(gt["rgb"]))
+
+    # DEPTH
+    axes[1].set_title("Depth")
+    axes[1].imshow(to_numpy(gt["depth"]))
+
+    # BBOX2D TIGHT
+    random.seed(1)
+    axes[2].set_title("BBox 2D Tight")
+    bboxes = gt["boundingBox2DTight"][["x_min", "y_min", "x_max", "y_max"]]
+    labels = gt["boundingBox2DLoose"]["semanticId"]
+    vis.plot_boxes(axes[2], bboxes, labels)
+
+    # BBOX2D LOOSE
+    random.seed(1)
+    axes[3].set_title("BBox 2D Loose")
+    bboxes = gt["boundingBox2DLoose"][["x_min", "y_min", "x_max", "y_max"]]
+    labels = gt["boundingBox2DLoose"]["semanticId"]
+    vis.plot_boxes(axes[3], bboxes, labels)
+
+    # # INSTANCE SEGMENTATION
+    random.seed(1)
+    axes[4].set_title("Instance Segmentation")
+    _, instance_seg = gt["instanceSegmentation"]
+    instance_rgb = vis.instance_segmentation_to_rgb(to_numpy(instance_seg))
+    axes[4].imshow(instance_rgb, alpha=0.7)
+
+    # SEMANTIC SEGMENTATION
+    random.seed(1)
+    axes[5].set_title("Semantic Segmentation")
+    _, semantic_seg = gt["semanticSegmentation"]
+    semantic_rgb = vis.semantic_segmentation_to_rgb(to_numpy(semantic_seg))
+    axes[5].imshow(semantic_rgb, alpha=0.7)
+
+    # BBOX 3D
+    axes[6].set_title("BBox 3D")
+
+    width = gt["camera"]["resolution"]["width"]
+    height = gt["camera"]["resolution"]["height"]
+    view_proj_mat = gt["camera"]["view_projection_matrix"]
+    points = gt["boundingBox3D"].reshape(-1, 3)
+    projected = project_points(view_proj_mat, points)[..., :2] * np.array([[width, height]])
+
+    face_idx_list = [[0, 1, 3, 2], [4, 5, 7, 6], [2, 3, 7, 6], [0, 1, 5, 4], [0, 2, 6, 4], [1, 3, 7, 5]]
+    colours = vis.random_colours(len(face_idx_list))
+    for p in projected.reshape(-1, 8, 2):
+        for face_idxs, colour in zip(face_idx_list, colours):
+            face = plt.Polygon(p[face_idxs], alpha=0.3, color=colour)
+            axes[6].add_patch(face)
+
+    # Display figure
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
