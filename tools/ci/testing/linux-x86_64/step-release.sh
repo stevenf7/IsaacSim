feb32271ba6c928dfbde736e21bfe6d37d67678d
Sheikh Dawood <sabdulajees@nvidia.com>
2020-07-22 17:12:42 -0500

    OM-16014 Python Tests only Isaac Sim package

--- tools/ci/testing/linux-x86_64/step-release.sh
+++ tools/ci/testing/linux-x86_64/step-release.sh
@@ -14,10 +14,10 @@ echo "##teamcity[testSuiteStarted name='isaac-sim-pythontests-release']"
 "$SCRIPT_DIR/../../../test_runner.sh" --suite pythontests --config release $package $*
 echo "##teamcity[testSuiteFinished name='isaac-sim-pythontests-release']"
 
-if [ ! -z "$TEAMCITY_VERSION" ]
-then
-    export ARCHIVE_PATTERN="_builtpackages/omniverse-kit-robotics*.7z"
-    echo "##teamcity[testSuiteStarted name='omniverse-kit-robotics-pythontests-release']"
-    "$SCRIPT_DIR/../../../test_runner.sh" --suite pythontests --config release $package $*
-    echo "##teamcity[testSuiteFinished name='omniverse-kit-robotics-pythontests-release']"
-fi
+# if [ ! -z "$TEAMCITY_VERSION" ]
+# then
+#     export ARCHIVE_PATTERN="_builtpackages/omniverse-kit-robotics*.7z"
+#     echo "##teamcity[testSuiteStarted name='omniverse-kit-robotics-pythontests-release']"
+#     "$SCRIPT_DIR/../../../test_runner.sh" --suite pythontests --config release $package $*
+#     echo "##teamcity[testSuiteFinished name='omniverse-kit-robotics-pythontests-release']"
+# fi
--- tools/ci/testing/windows-x86_64/step-release.bat
+++ tools/ci/testing/windows-x86_64/step-release.bat
@@ -15,21 +15,21 @@ echo ##teamcity[testSuiteStarted name='isaac-sim-pythontests-vulkan-release']
 call "%~dp0..\..\..\test_runner.bat" --suite pythontests --config release %package% -e="--vulkan"
 echo ##teamcity[testSuiteFinished name='isaac-sim-pythontests-vulkan-release']
 
-if not defined TEAMCITY_VERSION (
-    goto :Success
-)
+REM if not defined TEAMCITY_VERSION (
+REM     goto :Success
+REM )
 
-set "ARCHIVE_PATTERN=_builtpackages\omniverse-kit-robotics*.7z"
+REM set "ARCHIVE_PATTERN=_builtpackages\omniverse-kit-robotics*.7z"
 
-:: Python Tests - DX12
-echo ##teamcity[testSuiteStarted name='omniverse-kit-robotics-pythontests-dx12-release']
-call "%~dp0..\..\..\test_runner.bat" --suite pythontests --config release %package%
-echo ##teamcity[testSuiteFinished name='omniverse-kit-robotics-pythontests-dx12-release']
+REM :: Python Tests - DX12
+REM echo ##teamcity[testSuiteStarted name='omniverse-kit-robotics-pythontests-dx12-release']
+REM call "%~dp0..\..\..\test_runner.bat" --suite pythontests --config release %package%
+REM echo ##teamcity[testSuiteFinished name='omniverse-kit-robotics-pythontests-dx12-release']
 
-:: Python Tests - Vulkan
-echo ##teamcity[testSuiteStarted name='omniverse-kit-robotics-pythontests-vulkan-release']
-call "%~dp0..\..\..\test_runner.bat" --suite pythontests --config release %package% -e="--vulkan"
-echo ##teamcity[testSuiteFinished name='omniverse-kit-robotics-pythontests-vulkan-release']
+REM :: Python Tests - Vulkan
+REM echo ##teamcity[testSuiteStarted name='omniverse-kit-robotics-pythontests-vulkan-release']
+REM call "%~dp0..\..\..\test_runner.bat" --suite pythontests --config release %package% -e="--vulkan"
+REM echo ##teamcity[testSuiteFinished name='omniverse-kit-robotics-pythontests-vulkan-release']
 
 :Success
 exit /b 0
