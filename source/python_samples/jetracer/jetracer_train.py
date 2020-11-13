03bb9faaa0b60e76532310165c518b4c38a631d2
mgussert <mgussert@nvidia.com>
2020-11-13 10:49:58 -0700

    updated arg names

--- source/python_samples/jetracer/jetracer_train.py
+++ source/python_samples/jetracer/jetracer_train.py
@@ -40,30 +40,30 @@ def train(args):
     policy_kwargs = {"net_arch": net_arch, "features_extractor_class": CustomCNN, "activation_fn": torch.nn.ReLU}
 
     # create a new model
-    if args.loadedCheckpoint=="":
+    if args.loaded_checkpoint=="":
 
         model = PPO(
             "CnnPolicy",
             env,
             verbose=1,
-            tensorboard_log=args.tensorboardDir,
+            tensorboard_log=args.tensorboard_dir,
             policy_kwargs=policy_kwargs,
             device="cuda",
             n_steps=args.step_freq,
         )
     else:
 
-        model = PPO.load(args.loadedCheckpoint, env)
+        model = PPO.load(args.loaded_checkpoint, env)
     model.learn(
         total_timesteps=args.total_steps,
         callback=checkpoint_callback,
         eval_env=env,
         eval_freq=args.eval_freq,
-        eval_log_path=args.evaluationDir,
-        reset_num_timesteps=args.resetNumTimesteps,
+        eval_log_path=args.evaluation_dir,
+        reset_num_timesteps=args.reset_num_timesteps,
     )
 
-    model.save(args.checkpointName)
+    model.save(args.checkpoint_name)
 
 
 def runEval(args):
@@ -76,7 +76,7 @@ def runEval(args):
     }
     # load a zip file to evaluate here. PPO also saves the best model so far in the eval_log folder.
     # You can evaluate those zip files in the params folder as well (i.e params/rl_model_125999_steps.zip)
-    agent = PPO.load(args.evaluationDir+"/best_model.zip", device="cuda")
+    agent = PPO.load(args.evaluation_dir+"/best_model.zip", device="cuda")
 
     omniverse_kit = OmniKitHelper(CUSTOM_CONFIG)
 
@@ -101,12 +101,12 @@ def runEval(args):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("loadedCheckpoint", help="path to checkpoint to be loaded", default="", nargs="?", type=str)
+    parser.add_argument("loaded_checkpoint", help="path to checkpoint to be loaded", default="", nargs="?", type=str)
 
     parser.add_argument("-E", "--eval", help="evaluate checkpoint", action="store_true")
 
     parser.add_argument(
-        "-R", "--resetNumTimesteps", help="reset the current timestep number (used in logging)", action="store_true"
+        "-R", "--reset_num_timesteps", help="reset the current timestep number (used in logging)", action="store_true"
     )
 
     parser.add_argument(
@@ -116,12 +116,12 @@ if __name__ == "__main__":
     parser.add_argument("-H", "--headless", help="run in headless mode (no GUI)", action="store_true")
 
     parser.add_argument(
-        "--checkpointName", help="name of checkpoint file (no suffix)", default="checkpoint_25k", type=str
+        "--checkpoint_name", help="name of checkpoint file (no suffix)", default="checkpoint_25k", type=str
     )
 
-    parser.add_argument("--tensorboardDir", help="path to tensorboard log directory", default="tensorboard", type=str)
+    parser.add_argument("--tensorboard_dir", help="path to tensorboard log directory", default="tensorboard", type=str)
 
-    parser.add_argument("--evaluationDir", help="path to evaluation log directory", default="eval_log", type=str)
+    parser.add_argument("--evaluation_dir", help="path to evaluation log directory", default="eval_log", type=str)
 
     parser.add_argument("--save_freq", help="number of steps before saving a checkpoint", default=1000, type=int)
 
