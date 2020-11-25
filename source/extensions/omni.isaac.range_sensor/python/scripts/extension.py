951e054a3f3247a8596b253c37f31df1c83b3791
Hammad Mazhar <hmazhar@nvidia.com>
2020-11-24 16:13:03 -0800

    update to create RC 16

--- source/extensions/omni.isaac.range_sensor/python/scripts/extension.py
+++ source/extensions/omni.isaac.range_sensor/python/scripts/extension.py
@@ -13,6 +13,6 @@ class Extension(omni.ext.IExt):
     def on_shutdown(self):
         self._menu.shutdown()
         self._menu = None
-        _range_sensor.release_lidar_interface(self._lidar)
-        _range_sensor.release_ultrasonic_interface(self._ultrasonic)
-        _range_sensor.release_radar_interface(self._radar)
+        _range_sensor.release_lidar_sensor_interface(self._lidar)
+        _range_sensor.release_ultrasonic_sensor_interface(self._ultrasonic)
+        _range_sensor.release_radar_sensor_interface(self._radar)
