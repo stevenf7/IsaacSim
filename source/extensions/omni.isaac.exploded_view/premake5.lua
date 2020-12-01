55ed78572c744f67b992349607f8cb763a6f958e
Hammad Mazhar <hmazhar@nvidia.com>
2020-11-30 23:51:55 -0800

    fix exploded view extension build

--- source/extensions/omni.isaac.exploded_view/premake5.lua
+++ source/extensions/omni.isaac.exploded_view/premake5.lua
@@ -1,20 +1,10 @@
 local ext = get_current_extension_info()
 project_ext (ext)
-    
 
 repo_build.prebuild_link {
-    {"/python/scripts", ext.target_dir.."/omni/isaac/exploded_view/scripts" },
+    { "python/scripts", ext.target_dir.."/omni/isaac/exploded_view/scripts" },
 }
 
 repo_build.prebuild_copy {
     { "python/*.py", ext.target_dir.."/omni/isaac/exploded_view" },
 }
-
-
--- -- project "omni.isaac.exploded_view"
--- -- kind "None"
--- -- --add_impl_folder("")
-
--- -- vpaths { ["*"] = ext_folder }
--- -- files { ext_folder.."/**.py" }
--- -- files { ext_folder.."/**.toml" }
