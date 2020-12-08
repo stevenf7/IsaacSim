0f2b5f5b9374fbb4111236132901e470b5d27a6c
Shoubhik Debnath <sdebnath@nvidia.com>
2020-12-07 20:53:19 -0800

    Array editor and polygon movement randomization

--- source/extensions/omni.isaac.dr/plugins/DRComponentMovement.cpp
+++ source/extensions/omni.isaac.dr/plugins/DRComponentMovement.cpp
@@ -106,12 +106,87 @@ void DRComponentMovement::onComponentChange()
     for (auto target : lookAtTargets)
         mLookAtTargetPaths.push_back(target.GetString());
 
+    mPolygonPoints.clear();
+    pxr::VtArray<pxr::GfVec3f> polygonPoints;
+    movPrim.GetPolygonPointsAttr().Get<pxr::VtArray<pxr::GfVec3f>>(&polygonPoints);
+    for (unsigned int ind = 0; ind < polygonPoints.size(); ind++)
+        mPolygonPoints.push_back(polygonPoints[ind]);
+
     update();
     CARB_LOG_INFO("Movement Update: %s", mCompName.c_str());
 }
 void DRComponentMovement::stop()
 {
     CARB_LOG_INFO("DR Movement Component Stopped");
+}
+pxr::GfVec3f DRComponentMovement::randomPointTriangle(std::vector<pxr::GfVec3f>& samplePoints)
+{
+    double r1 = randomRangeFloat(0, 1);
+    double r2 = randomRangeFloat(0, 1);
+    pxr::GfVec3f randomPoint =
+        (1 - sqrt(r1)) * samplePoints[0] + (sqrt(r1) * (1 - r2)) * samplePoints[1] + (r2 * sqrt(r1)) * samplePoints[2];
+    // CARB_LOG_WARN("New point: (%lf, %lf, %lf)", randomPoint[0], randomPoint[1], randomPoint[2]);
+    return randomPoint;
+}
+std::vector<std::vector<pxr::GfVec3f>> triangulatePolygon(std::vector<pxr::GfVec3f>& samplePoints)
+{
+    std::vector<std::vector<pxr::GfVec3f>> triangulatedPolygon;
+    for (unsigned int i = 0; i < samplePoints.size() - 2; i++)
+    {
+        std::vector<pxr::GfVec3f> triplets;
+        triplets.push_back(samplePoints[i]);
+        triplets.push_back(samplePoints[i + 1]);
+        triplets.push_back(samplePoints[i + 2]);
+        triangulatedPolygon.push_back(triplets);
+    }
+    return triangulatedPolygon;
+}
+// A utility function to find distance between two points in a plane
+double dist(pxr::GfVec3f p1, pxr::GfVec3f p2)
+{
+    return sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
+}
+double areaTriangle(std::vector<pxr::GfVec3f> trianglePts)
+{
+    double a = dist(trianglePts[0], trianglePts[1]);
+    double b = dist(trianglePts[1], trianglePts[2]);
+    double c = dist(trianglePts[0], trianglePts[2]);
+    double s = (a + b + c) / 2.0;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
+std::vector<double> generateDistribution(std::vector<std::vector<pxr::GfVec3f>> allTriangles)
+{
+    double totalArea = 0.0;
+    std::vector<double> cumulativeAreaDistribution;
+    cumulativeAreaDistribution.push_back(totalArea);
+    for (std::vector<pxr::GfVec3f> triangle : allTriangles)
+    {
+        double newArea = areaTriangle(triangle);
+        totalArea += newArea;
+        cumulativeAreaDistribution.push_back(totalArea);
+    }
+    for (unsigned int i = 0; i < cumulativeAreaDistribution.size(); i++)
+        cumulativeAreaDistribution[i] /= totalArea;
+    return cumulativeAreaDistribution;
+}
+pxr::GfVec3f DRComponentMovement::randomPointPolygon(std::vector<pxr::GfVec3f>& samplePoints)
+{
+    pxr::GfVec3f randomPoint(0, 0, 0);
+    std::vector<std::vector<pxr::GfVec3f>> allTriangles = triangulatePolygon(samplePoints);
+    std::vector<double> cumulativeAreaDistribution = generateDistribution(allTriangles);
+    double randomArea = randomRangeFloat(0, 1);
+    int randomIndex = 0;
+    for (unsigned int i = 0; i < cumulativeAreaDistribution.size() - 1; i++)
+    {
+        // CARB_LOG_WARN("%lf", cumulativeAreaDistribution[i]);
+        if (randomArea > cumulativeAreaDistribution[i] && randomArea <= cumulativeAreaDistribution[i + 1])
+        {
+            randomIndex = i;
+            break;
+        }
+    }
+    // CARB_LOG_WARN("Area: %lf, Index: %d", randomArea, randomIndex);
+    return randomPointTriangle(allTriangles[randomIndex]);
 }
 void DRComponentMovement::tick()
 {
@@ -124,6 +199,14 @@ void DRComponentMovement::tick()
             float y = randomRangeFloat(mYRange[0], mYRange[1]);
             float z = randomRangeFloat(mZRange[0], mZRange[1]);
             pxr::GfVec3d eyeUsd(x, y, z);
+            if (mPolygonPoints.size() > 2)
+            {
+                pxr::GfVec3f randPoint = randomPointPolygon(mPolygonPoints);
+                if (pxr::UsdGeomGetStageUpAxis(mStage) == pxr::UsdGeomTokens->z)
+                    eyeUsd = pxr::GfVec3d(randPoint[0], randPoint[1], z);
+                else
+                    eyeUsd = pxr::GfVec3d(randPoint[0], y, randPoint[2]);
+            }
             pxr::GfMatrix4d currentTransformMat, finalTransformMat, scaledTransformMat, scaleMat;
             currentTransformMat = omni::usd::UsdUtils::getLocalTransformMatrix(prim);
             pxr::GfTransform currentTr(currentTransformMat);
