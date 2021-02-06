3c26f07c03d8353c63368249970088e396649d8a
Hammad Mazhar <hmazhar@nvidia.com>
2021-02-05 19:59:21 -0800

    Sample UI Cleanup

--- source/extensions/omni.isaac.ros_bridge/python/scripts/samples/rosservices.py
+++ source/extensions/omni.isaac.ros_bridge/python/scripts/samples/rosservices.py
@@ -12,35 +12,39 @@
 from pxr import Sdf, Gf, UsdGeom, UsdLux
 import omni.usd
 import omni
-import omni.kit.ui
+import omni.ui as ui
 import omni.isaac.RosBridgeSchema as ROSSchema
 import asyncio
+from omni.kit.menu.utils import add_menu_items, remove_menu_items, MenuItemDescription
+import weakref
+
+EXTENSION_NAME = "Ros Services"
 
 
 class Extension(omni.ext.IExt):
     def on_startup(self):
         # setting up the UI on the menu bar for this example
-        self._window = omni.kit.ui.Window(
-            "Rosservices",
-            300,
-            200,
-            menu_path="Isaac/ROS/Rosservices",
-            open=False,
-            dock=omni.kit.ui.DockPreference.LEFT_BOTTOM,
-        )
-        sublayout = self._window.layout.add_child(omni.kit.ui.ColumnLayout())
 
-        clean_stage_btn = sublayout.add_child(omni.kit.ui.Button("Clean Stage"))
-        clean_stage_btn.set_clicked_fn(self._on_clean_stage)
-        clean_stage_btn.tooltip = omni.kit.ui.Label("Clean the stage")
+        self._window = ui.Window(EXTENSION_NAME, width=600, height=400, visible=False)
+        self._menu_items = [
+            MenuItemDescription(
+                name="ROS",
+                sub_menu=[
+                    MenuItemDescription(
+                        name=EXTENSION_NAME, onclick_fn=lambda a=weakref.proxy(self): a._menu_callback()
+                    )
+                ],
+            )
+        ]
+        add_menu_items(self._menu_items, "Isaac")
 
-        add_cube_btn = sublayout.add_child(omni.kit.ui.Button("Add Cube"))
-        add_cube_btn.set_clicked_fn(self._on_add_cube)
-        add_cube_btn.tooltip = omni.kit.ui.Label("Add a Cube and start its pose service")
-
-        add_cone_btn = sublayout.add_child(omni.kit.ui.Button("Add Cone"))
-        add_cone_btn.set_clicked_fn(self._on_add_cone)
-        add_cone_btn.tooltip = omni.kit.ui.Label("Add a Cone and start a second pose service")
+        with self._window.frame:
+            with ui.VStack():
+                ui.Button("Clean Stage", tooltip="Clean the stage", clicked_fn=self._on_clean_stage)
+                ui.Button("Add Cube", tooltip="Add a Cube and start its pose service", clicked_fn=self._on_add_cube)
+                ui.Button(
+                    "Add Cone", tooltip="Add a Cone and start a second pose service", clicked_fn=self._on_add_cone
+                )
 
         self._viewport = omni.kit.viewport.get_default_viewport_window()
         self._viewport.set_camera_position("/OmniverseKit_Persp", 103.4, 13.8, 19.8, True)
@@ -48,9 +52,13 @@ class Extension(omni.ext.IExt):
         self._timeline = omni.timeline.get_timeline_interface()
 
     def on_shutdown(self):
+        remove_menu_items(self._menu_items, "Isaac")
         self._window = None
 
-    def _on_clean_stage(self, widget):
+    def _menu_callback(self):
+        self._window.visible = not self._window.visible
+
+    def _on_clean_stage(self):
         load_stage = asyncio.ensure_future(omni.usd.get_context().new_stage_async())
         asyncio.ensure_future(self._load_stage(load_stage))
 
@@ -65,7 +73,7 @@ class Extension(omni.ext.IExt):
             self._viewport.set_camera_target("/OmniverseKit_Persp", -225.0, -23.78, -26.17, True)
 
     # add cube
-    def _on_add_cube(self, widget):
+    def _on_add_cube(self):
         # create a cube
         self._stage = omni.usd.get_context().get_stage()
         distantLight = UsdLux.DistantLight.Define(self._stage, Sdf.Path("/World/defaultLight"))
@@ -93,7 +101,7 @@ class Extension(omni.ext.IExt):
             self._timeline.play()
 
     # add cone
-    def _on_add_cone(self, widget):
+    def _on_add_cone(self):
         # create a cube
         self._stage = omni.usd.get_context().get_stage()
         ConePath = "/Cone"
--- source/extensions/omni.isaac.ros_bridge/python/scripts/samples/rostopics.py
+++ source/extensions/omni.isaac.ros_bridge/python/scripts/samples/rostopics.py
@@ -10,27 +10,33 @@
 # This is an example script showing how to use rosbridge to publish joint_states of an articulated robot
 
 import carb
-from pxr import UsdPhysics, Sdf, Gf, UsdGeom
+from pxr import UsdPhysics, Sdf, Gf, UsdGeom, PhysxSchema
 import omni.usd
 import omni
-import omni.kit.ui
+import omni.ui as ui
 import omni.isaac.RosBridgeSchema as ROSSchema
 from omni.isaac.utils.scripts.test_utils import load_test_file
 import asyncio
+from omni.kit.menu.utils import add_menu_items, remove_menu_items, MenuItemDescription
+import weakref
+import math
+
+EXTENSION_NAME = "Ros Topics"
 
 
-def set_drive_parameters(drive, target_type, target_value, stiffness, damping, max_force):
+def set_drive_parameters(drive, target_type, target_value, stiffness, damping, max_force=None):
     """Enable velocity drive for a given joint"""
 
-    if not drive.GetTargetTypeAttr():
-        drive.CreateTargetTypeAttr(target_type)
-    else:
-        drive.GetTargetTypeAttr().Set(target_type)
-
-    if not drive.GetTargetAttr():
-        drive.CreateTargetAttr(target_value)
-    else:
-        drive.GetTargetAttr().Set(target_value)
+    if target_type == "position":
+        if not drive.GetTargetPositionAttr():
+            drive.CreateTargetPositionAttr(target_value)
+        else:
+            drive.GetTargetPositionAttr().Set(target_value)
+    elif target_type == "velocity":
+        if not drive.GetTargetVelocityAttr():
+            drive.CreateTargetVelocityAttr(target_value)
+        else:
+            drive.GetTargetVelocityAttr().Set(target_value)
 
     if not drive.GetStiffnessAttr():
         drive.CreateStiffnessAttr(stiffness)
@@ -42,58 +48,66 @@ def set_drive_parameters(drive, target_type, target_value, stiffness, damping, m
     else:
         drive.GetDampingAttr().Set(damping)
 
-    if not drive.GetMaxForceAttr():
-        drive.CreateMaxForceAttr(max_force)
-    else:
-        drive.GetMaxForceAttr().Set(max_force)
+    if max_force is not None:
+        if not drive.GetMaxForceAttr():
+            drive.CreateMaxForceAttr(max_force)
+        else:
+            drive.GetMaxForceAttr().Set(max_force)
 
 
 class Extension(omni.ext.IExt):
     def on_startup(self):
+        ext_manager = omni.kit.app.get_app().get_extension_manager()
+        ext_id = ext_manager.get_enabled_extension_id("omni.isaac.dynamic_control")
+        self._dc_extension_path = ext_manager.get_extension_path(ext_id)
         # setting up the UI on the menu bar for this example
-        self._window = omni.kit.ui.Window(
-            "Rostopics",
-            300,
-            200,
-            menu_path="Isaac/ROS/Rostopics",
-            open=False,
-            dock=omni.kit.ui.DockPreference.LEFT_BOTTOM,
-        )
-        sublayout = self._window.layout.add_child(omni.kit.ui.ColumnLayout())
-        load_robot_btn = sublayout.add_child(omni.kit.ui.Button("Load Robot"))
-        load_robot_btn.set_clicked_fn(self._on_load_robot)
+        self._window = omni.ui.Window(EXTENSION_NAME, width=600, height=400, visible=False)
+        self._menu_items = [
+            MenuItemDescription(
+                name="ROS",
+                sub_menu=[
+                    MenuItemDescription(
+                        name=EXTENSION_NAME, onclick_fn=lambda a=weakref.proxy(self): a._menu_callback()
+                    )
+                ],
+            )
+        ]
+        add_menu_items(self._menu_items, "Isaac")
+        with self._window.frame:
+            with ui.VStack():
+                ui.Button("Load Robot", tooltip="Loads robot into stage", clicked_fn=self._on_load_robot)
+                with ui.HStack(height=0):
+                    ui.Label("Control Mode")
+                    self._velocity_combo = ui.ComboBox(0, "Position Control", "Velocity Control").model
+                    ui.Button(
+                        "Connect Joint State Topics",
+                        tooltip="start a joint_state and joint_command topic to publish and receive joint states",
+                        clicked_fn=self._on_connect_js,
+                    )
+                ui.Button(
+                    "Connect Camera Topics",
+                    tooltip="Connect Camera Ros Topics and start publishing",
+                    clicked_fn=self._on_connect_camera,
+                )
+                ui.Button(
+                    "Connect TF Topics",
+                    tooltip="Connect TF Ros Topics and start publishing",
+                    clicked_fn=self._on_connect_tf,
+                )
+                ui.Button(
+                    "Add Cube", tooltip="Add a Cube to the scene and append to TF tree", clicked_fn=self._on_add_cube
+                )
 
-        joint_state_layout = omni.kit.ui.RowColumnLayout(2, True)
-        sublayout.add_child(joint_state_layout)
-        joint_state_layout.set_column_width(0, 200)
-        joint_state_layout.set_column_width(1, 160)
-
-        self.control_mode = joint_state_layout.add_child(omni.kit.ui.ComboBox("Control Mode"))
-        self.control_mode.add_item("position control")
-        self.control_mode.add_item("velocity control")
-
-        connect_js_btn = joint_state_layout.add_child(omni.kit.ui.Button("Connect Joint State Topics"))
-        connect_js_btn.set_clicked_fn(self._on_connect_js)
-        connect_js_btn.tooltip = omni.kit.ui.Label(
-            "start a joint_state and joint_command topic to publish and receive joint states"
-        )
-
-        connect_camera_btn = sublayout.add_child(omni.kit.ui.Button("Connect Camera Topics"))
-        connect_camera_btn.set_clicked_fn(self._on_connect_camera)
-
-        connect_tf_btn = sublayout.add_child(omni.kit.ui.Button("Connect TF Topics"))
-        connect_tf_btn.set_clicked_fn(self._on_connect_tf)
-
-        add_cube_btn = sublayout.add_child(omni.kit.ui.Button("Add Cube"))
-        add_cube_btn.set_clicked_fn(self._on_add_cube)
-        add_cube_btn.tooltip = omni.kit.ui.Label("Add a Cube to the scene and append to TF tree")
-
-        self.stage = omni.usd.get_context().get_stage()
+        self._stage = omni.usd.get_context().get_stage()
         self._viewport = omni.kit.viewport.get_default_viewport_window()
         self._timeline = omni.timeline.get_timeline_interface()
 
     def on_shutdown(self):
+        remove_menu_items(self._menu_items, "Isaac")
         self._window = None
+
+    def _menu_callback(self):
+        self._window.visible = not self._window.visible
 
     # Fix camera location and angle
     async def _setup_camera(self, task):
@@ -107,19 +121,19 @@ class Extension(omni.ext.IExt):
                 self._timeline.play()
 
     # load robot
-    def _on_load_robot(self, widget):
-        task = asyncio.ensure_future(load_test_file("data/usd/robots/franka/franka.usd"))
+    def _on_load_robot(self):
+        task = asyncio.ensure_future(load_test_file(self._dc_extension_path + "/data/usd/robots/franka/franka.usd"))
         asyncio.ensure_future(self._setup_camera(task))
 
-    def _on_connect_js(self, widget):
+    def _on_connect_js(self):
 
         # check robot is loaded and articulation exist
-        self.stage = omni.usd.get_context().get_stage()
-        robot_prim = self.stage.GetPrimAtPath("/panda")
-        assert robot_prim.HasAPI(UsdPhysics.ArticulationAPI)
+        self._stage = omni.usd.get_context().get_stage()
+        robot_prim = self._stage.GetPrimAtPath("/panda")
+        assert robot_prim.HasAPI(PhysxSchema.PhysxArticulationAPI)
 
         # setup Rostopic to publish and receive joint state info
-        js_prim = ROSSchema.RosJointState.Define(self.stage, Sdf.Path("/ROS_JointState"))
+        js_prim = ROSSchema.RosJointState.Define(self._stage, Sdf.Path("/ROS_JointState"))
 
         # adding prefix to the published /joint_state topic if needed
         js_prim.CreateEnabledAttr(True)
@@ -132,63 +146,77 @@ class Extension(omni.ext.IExt):
         js_prim.CreateQueueSizeAttr(0)
 
         # The joint_state rostopic must be connected to the root of the robot's articulation in order to publish its states
-        ROS_prim = self.stage.GetPrimAtPath("/ROS_JointState")
+        ROS_prim = self._stage.GetPrimAtPath("/ROS_JointState")
         ROS_prim.GetRelationship("articulationPrim").SetTargets(["/panda"])
         panda_joint1_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link0/panda_joint1"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link0/panda_joint1"), "angular"
         )
         panda_joint2_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link1/panda_joint2"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link1/panda_joint2"), "angular"
         )
         panda_joint3_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link2/panda_joint3"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link2/panda_joint3"), "angular"
         )
         panda_joint4_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link3/panda_joint4"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link3/panda_joint4"), "angular"
         )
         panda_joint5_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link4/panda_joint5"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link4/panda_joint5"), "angular"
         )
         panda_joint6_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link5/panda_joint6"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link5/panda_joint6"), "angular"
         )
         panda_joint7_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_link6/panda_joint7"), "angular"
+            self._stage.GetPrimAtPath("/panda/panda_link6/panda_joint7"), "angular"
         )
         panda_finger1_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_hand/panda_finger_joint1"), "linear"
+            self._stage.GetPrimAtPath("/panda/panda_hand/panda_finger_joint1"), "linear"
         )
         panda_finger2_drive = UsdPhysics.DriveAPI.Get(
-            self.stage.GetPrimAtPath("/panda/panda_hand/panda_finger_joint2"), "linear"
+            self._stage.GetPrimAtPath("/panda/panda_hand/panda_finger_joint2"), "linear"
         )
 
-        if self.control_mode.selected_index == 1:
+        if self._velocity_combo.get_item_value_model().as_int == 1:
             # set all joints to velocity control
-            set_drive_parameters(panda_joint1_drive, "velocity", 0.0, 0, 1e7, 1e8)
-            set_drive_parameters(panda_joint2_drive, "velocity", 0.0, 0, 1e7, 1e8)
-            set_drive_parameters(panda_joint3_drive, "velocity", 0.0, 0, 1e7, 1e8)
-            set_drive_parameters(panda_joint4_drive, "velocity", 0.0, 0, 1e7, 1e8)
-            set_drive_parameters(panda_joint5_drive, "velocity", 0.0, 0, 1e7, 1e8)
-            set_drive_parameters(panda_joint6_drive, "velocity", 0.0, 0, 1e7, 1e8)
-            set_drive_parameters(panda_joint7_drive, "velocity", 0.0, 0, 1e7, 1e8)
+            set_drive_parameters(panda_joint1_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
+            set_drive_parameters(panda_joint2_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
+            set_drive_parameters(panda_joint3_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
+            set_drive_parameters(panda_joint4_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
+            set_drive_parameters(panda_joint5_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
+            set_drive_parameters(panda_joint6_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
+            set_drive_parameters(panda_joint7_drive, "velocity", 0.0, 0, math.radians(1e7), 1e8)
 
         else:
             # set all joints to position control
-            set_drive_parameters(panda_joint1_drive, "position", 0.0, 60000, 3000, 87000)
-            set_drive_parameters(panda_joint2_drive, "position", -1.3, 60000, 3000, 87000)
-            set_drive_parameters(panda_joint3_drive, "position", 0.0, 60000, 3000, 87000)
-            set_drive_parameters(panda_joint4_drive, "position", -2.87, 60000, 3000, 87000)
-            set_drive_parameters(panda_joint5_drive, "position", 0, 25000, 3000, 12000)
-            set_drive_parameters(panda_joint6_drive, "position", 2, 25000, 3000, 12000)
-            set_drive_parameters(panda_joint7_drive, "position", 0.75, 5000, 3000, 12000)
-            set_drive_parameters(panda_finger1_drive, "position", 0, 6000, 1000, 1200)
-            set_drive_parameters(panda_finger2_drive, "position", 0, 6000, 1000, 1200)
+            set_drive_parameters(
+                panda_joint1_drive, "position", math.degrees(0.0), math.radians(60000), math.radians(3000), 87000
+            )
+            set_drive_parameters(
+                panda_joint2_drive, "position", math.degrees(-1.3), math.radians(60000), math.radians(3000), 87000
+            )
+            set_drive_parameters(
+                panda_joint3_drive, "position", math.degrees(0.0), math.radians(60000), math.radians(3000), 87000
+            )
+            set_drive_parameters(
+                panda_joint4_drive, "position", math.degrees(-2.87), math.radians(60000), math.radians(3000), 87000
+            )
+            set_drive_parameters(
+                panda_joint5_drive, "position", math.degrees(0), math.radians(25000), math.radians(3000), 12000
+            )
+            set_drive_parameters(
+                panda_joint6_drive, "position", math.degrees(2), math.radians(25000), math.radians(3000), 12000
+            )
+            set_drive_parameters(
+                panda_joint7_drive, "position", math.degrees(0.75), math.radians(5000), math.radians(3000), 12000
+            )
+            set_drive_parameters(panda_finger1_drive, "position", 0, math.radians(6000), math.radians(1000), 1200)
+            set_drive_parameters(panda_finger2_drive, "position", 0, math.radians(6000), math.radians(1000), 1200)
 
     # adding camera topic
-    def _on_connect_camera(self, widget):
-        self.stage = omni.usd.get_context().get_stage()
+    def _on_connect_camera(self):
+        self._stage = omni.usd.get_context().get_stage()
         # add camera prim to path
-        camera_prim = ROSSchema.RosCamera.Define(self.stage, Sdf.Path("/ROS_Camera"))
+        camera_prim = ROSSchema.RosCamera.Define(self._stage, Sdf.Path("/ROS_Camera"))
         # adding prefix to the publisher topic if needed
         camera_prim.CreateRosNodePrefixAttr("")
         camera_prim.CreateEnabledAttr(True)
@@ -215,10 +243,10 @@ class Extension(omni.ext.IExt):
         # rosrun image_view image_view image:=/depth
 
     # adding the tf topic
-    def _on_connect_tf(self, widget):
-        self.stage = omni.usd.get_context().get_stage()
+    def _on_connect_tf(self):
+        self._stage = omni.usd.get_context().get_stage()
         # setup rostpic for the tf tree
-        tf_prim = ROSSchema.RosPoseTree.Define(self.stage, Sdf.Path("/ROS_PoseTree"))
+        tf_prim = ROSSchema.RosPoseTree.Define(self._stage, Sdf.Path("/ROS_PoseTree"))
         tf_prim.CreateEnabledAttr(True)
         # create the publishing topic
         tf_prim.CreatePoseTreePubTopicAttr("/tf")
@@ -226,11 +254,11 @@ class Extension(omni.ext.IExt):
         tf_prim.CreateQueueSizeAttr(0)
 
         # The tf rostopic must be connected to the root of the robot's articulation in order to publish its transforms
-        ROS_prim = self.stage.GetPrimAtPath("/ROS_PoseTree")
+        ROS_prim = self._stage.GetPrimAtPath("/ROS_PoseTree")
         # if one doesn't exist already, create one.
         if not ROS_prim:
             # create the topic if one does not exist
-            tf_prim = ROSSchema.RosPoseTree.Define(self.stage, Sdf.Path("/ROS_PoseTree"))
+            tf_prim = ROSSchema.RosPoseTree.Define(self._stage, Sdf.Path("/ROS_PoseTree"))
             tf_prim.CreateEnabledAttr(True)
             # create the publishing topic
             tf_prim.CreatePoseTreePubTopicAttr("/tf")
@@ -244,28 +272,28 @@ class Extension(omni.ext.IExt):
         if not self._timeline.is_playing():
             self._timeline.play()
 
-    def _on_add_cube(self, widget):
+    def _on_add_cube(self):
         # first create a cube
-        self.stage = omni.usd.get_context().get_stage()
+        self._stage = omni.usd.get_context().get_stage()
         CubePath = "/cube"
         # offset to some position in space
         offset = Gf.Vec3f(50.0, 0.0, 50.0)
         size = 10  # cm
-        cubeGeom = UsdGeom.Cube.Define(self.stage, CubePath)
+        cubeGeom = UsdGeom.Cube.Define(self._stage, CubePath)
         cubeGeom.CreateSizeAttr(size)
         cubeGeom.AddTranslateOp().Set(offset)
 
         # add the cube to tf tree
-        ROS_prim = self.stage.GetPrimAtPath("/ROS_PoseTree")
+        ROS_prim = self._stage.GetPrimAtPath("/ROS_PoseTree")
         if not ROS_prim:
             # create the topic if one does not exist
-            tf_prim = ROSSchema.RosPoseTree.Define(self.stage, Sdf.Path("/ROS_PoseTree"))
+            tf_prim = ROSSchema.RosPoseTree.Define(self._stage, Sdf.Path("/ROS_PoseTree"))
             tf_prim.CreateEnabledAttr(True)
             # create the publishing topic
             tf_prim.CreatePoseTreePubTopicAttr("/tf")
             tf_prim.CreateTargetPrimsRel()
             tf_prim.CreateQueueSizeAttr(0)
-            ROS_prim = self.stage.GetPrimAtPath("/ROS_PoseTree")
+            ROS_prim = self._stage.GetPrimAtPath("/ROS_PoseTree")
             ROS_prim.GetRelationship("targetPrims").SetTargets(["/cube"])
         else:
             ROS_prim.GetRelationship("targetPrims").AddTarget(Sdf.Path("/cube"))
