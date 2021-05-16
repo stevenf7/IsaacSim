dcc316755277b6d68c51db5f45271f0763c156e5
Shoubhik Debnath <sdebnath@nvidia.com>
2021-05-16 12:20:18 -0700

    Update viewport settings based on mDoStart flag

--- source/extensions/omni.isaac.robot_engine_bridge/plugins/Gxf/CameraComponent.h
+++ source/extensions/omni.isaac.robot_engine_bridge/plugins/Gxf/CameraComponent.h
@@ -84,6 +84,7 @@ private:
                        float focalLength,
                        float horizontalAperture,
                        float verticalAperture);
+    void updateViewportSettings();
     carb::Framework* mFramework = nullptr;
     omni::kit::IViewport* mViewportInterface = nullptr;
     omni::syntheticdata::SyntheticData* mSyntheticDataInterface = nullptr;
