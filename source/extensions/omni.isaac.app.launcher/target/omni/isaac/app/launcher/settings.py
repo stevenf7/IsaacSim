a8c3277fa74f24e6d1460ab3cdb64372b85b3b35
Hammad Mazhar <hmazhar@nvidia.com>
2021-06-19 19:23:41 +0000

    Update licenses, formatting

--- source/extensions/omni.isaac.app.launcher/target/omni/isaac/app/launcher/settings.py
+++ source/extensions/omni.isaac.app.launcher/target/omni/isaac/app/launcher/settings.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2018-2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
 # this file own the settings Strings so they can easily be shared and access from the other part of the app Launcher
 SHOW_CONSOLE_SETTING = "/persistent/ext/omni.isaac.launcher/show_console"
 PERSISTENT_LAUNCHER_SETTING = "/persistent/ext/omni.isaac.launcher/persistent_launcher"
