08b746e7cd44f46495635530a87800643e361573
Hammad Mazhar <hmazhar@nvidia.com>
2021-07-02 03:56:12 +0000

    update extension publish script to use release mode, disable default etm for now

--- tools/publish_extension.sh
+++ tools/publish_extension.sh
@@ -2,5 +2,8 @@ set -e
 
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
 cd "$SCRIPT_DIR"
-../build.sh
-../_build/linux-x86_64/release/isaac-sim.sh --ext-folder exts --ext-folder apps --publish $@
+../repo.sh publish_exts -c release $@
+
+# keeping manual command below for reference
+# ../build.sh
+# ../_build/linux-x86_64/release/isaac-sim.sh --ext-folder exts --ext-folder apps --publish
