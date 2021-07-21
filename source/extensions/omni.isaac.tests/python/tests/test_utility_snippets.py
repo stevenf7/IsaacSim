e2fe5a3a52bec8d0912f513f0c7fa8fe2bfc94ad
Hammad Mazhar <hmazhar@nvidia.com>
2021-07-21 03:56:06 +0000

    Add contact sample and camera snippet

--- source/extensions/omni.isaac.tests/python/tests/test_utility_snippets.py
+++ source/extensions/omni.isaac.tests/python/tests/test_utility_snippets.py
@@ -338,3 +338,38 @@ class TestUtilitySnippets(omni.kit.test.AsyncTestCaseFailOnLogError):
         task = asyncio.ensure_future(omni.kit.app.get_app().next_update_async())
         asyncio.ensure_future(pause_sim(task))
         ###
+
+    async def test_camera_intrinsics(self):
+        import omni
+        from omni.syntheticdata import helpers
+        import math
+
+        stage = omni.usd.get_context().get_stage()
+        viewport = omni.kit.viewport.get_viewport_interface()
+
+        # acquire the viewport window
+        viewport_handle = viewport.get_instance("Viewport")
+        viewport_window = viewport.get_viewport_window(viewport_handle)
+        # Set viewport resolution, changes will occur on next frame
+        viewport_window.set_texture_resolution(512, 512)
+        # get resolution
+        width, height = viewport_window.get_texture_resolution()
+        aspect_ratio = width / height
+        # get camera prim attached to viewport
+        camera = stage.GetPrimAtPath(viewport_window.get_active_camera())
+        focal_length = camera.GetAttribute("focalLength").Get()
+        horiz_aperture = camera.GetAttribute("horizontalAperture").Get()
+        vert_aperture = camera.GetAttribute("verticalAperture").Get()
+        # Pixels are square so we can also do:
+        # vert_aperture = height / width * horiz_aperture
+        near, far = camera.GetAttribute("clippingRange").Get()
+        fov = 2 * math.atan(horiz_aperture / (2 * focal_length))
+
+        # helper to compute projection matrix
+        proj_mat = helpers.get_projection_matrix(fov, aspect_ratio, near, far)
+
+        # compute focal point and center
+        focal_x = height * focal_length / vert_aperture
+        focal_y = width * focal_length / horiz_aperture
+        center_x = height * 0.5
+        center_y = width * 0.5
