ef99a27363591eb95015bc0359bc3d7862b33d73
Hammad Mazhar <hmazhar@nvidia.com>
2021-07-27 23:10:14 +0000

    Extension Cleanup

--- source/extensions/omni.isaac.samples_internal/premake5.lua
+++ source/extensions/omni.isaac.samples_internal/premake5.lua
@@ -1,21 +1,7 @@
 local ext = get_current_extension_info()
 project_ext (ext)
 
-    
-if os.target() == "linux" then
-    repo_build.prebuild_link {
-        { "config/linux", ext.target_dir.."/config" },
-    }
-else
-    repo_build.prebuild_link {
-        { "config/windows", ext.target_dir.."/config" },
-    }
-end
-
 repo_build.prebuild_link {
-    { "python/scripts", ext.target_dir.."/omni/isaac/samples_internal/scripts" },
-}
-
-repo_build.prebuild_copy {
-    { "python/*.py", ext.target_dir.."/omni/isaac/samples_internal" },
+    { "docs", ext.target_dir.."/docs" },
+    { "omni", ext.target_dir.."/omni" },
 }
