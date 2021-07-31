4958cdd4c2652f33d80bfbc7874740296e6e889d
Hammad Mazhar <hmazhar@nvidia.com>
2021-07-31 14:00:22 -0700

    cleanup splash and launcher folder structure

--- source/extensions/omni.isaac.app.launcher/premake5.lua
+++ source/extensions/omni.isaac.app.launcher/premake5.lua
@@ -1,7 +1,9 @@
--- Use folder name to build extension name and tag. Version is specified explicitly.
 local ext = get_current_extension_info()
-
--- That will also link whole current "target" folder into as extension target folder:
 project_ext (ext, { 
     define_test = false
 })
+
+repo_build.prebuild_link {
+    { "icons", ext.target_dir.."/icons" },
+    { "omni", ext.target_dir.."/omni" },
+}
