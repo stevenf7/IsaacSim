d625382a3e20727cd9deba7417b7461d1b32d68b
Hammad Mazhar <hmazhar@nvidia.com>
2021-07-31 11:35:20 -0700

    Cleanup dependencies

--- source/extensions/omni.isaac.tests/premake5.lua
+++ source/extensions/omni.isaac.tests/premake5.lua
@@ -1,21 +1,6 @@
 local ext = get_current_extension_info()
 project_ext (ext)
 
-
-if os.target() == "linux" then
-    repo_build.prebuild_link {
-        { "config/linux", ext.target_dir.."/config" },
-    }
-else
-    repo_build.prebuild_link {
-        { "config/windows", ext.target_dir.."/config" },
-    }
-end
-
--- no data exists currently, need to add back once we have multi-extension unit tests here. 
--- repo_build.prebuild_link {
---     { "data", ext.target_dir.."/data" },
--- }
 repo_build.prebuild_link {
 { "python/tests", ext.target_dir.."/omni/isaac/tests/tests" },
 { "data", ext.target_dir.."/data" },
