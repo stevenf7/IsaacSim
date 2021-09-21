c4d9e56260afaa85d49365b85c88500e33557d69
Michael Gussert <mgussert@nvidia.com>
2021-09-21 12:10:43 -0700

    consolidated visualize_groundtruth samples into one file

--- source/python_samples/syntheticdata/basic/visualize_groundtruth.py
+++ source/python_samples/syntheticdata/basic/visualize_groundtruth.py
@@ -22,6 +22,9 @@ import matplotlib.pyplot as plt
 TRANSLATION_RANGE = 300.0
 SCALE = 50.0
 
+ENABLE_PHYSICS = True
+AND_ADD_GLASS = True
+
 
 def main():
     kit = OmniKitHelper(
@@ -32,6 +35,8 @@ def main():
 
     sd_helper = SyntheticDataHelper()
     from omni.syntheticdata import visualize, helpers
+    from omni.physx.scripts import utils
+    from pxr import UsdPhysics, PhysxSchema, PhysicsSchemaTools, Sdf, Gf, UsdShade
 
     # SCENE SETUP
     # Get the current stage
@@ -40,8 +45,27 @@ def main():
     # Add a distant light
     stage.DefinePrim("/World/Light", "DistantLight")
 
+    if ENABLE_PHYSICS:
+        # Add physics scene
+        scene = UsdPhysics.Scene.Define(stage, Sdf.Path("/World/physicsScene"))
+        # Set gravity vector
+        scene.CreateGravityDirectionAttr().Set(Gf.Vec3f(0.0, 0.0, -1.0))
+        scene.CreateGravityMagnitudeAttr().Set(981.0)
+        # Set physics scene to use cpu physics
+        PhysxSchema.PhysxSceneAPI.Apply(stage.GetPrimAtPath("/World/physicsScene"))
+        physxSceneAPI = PhysxSchema.PhysxSceneAPI.Get(stage, "/World/physicsScene")
+        physxSceneAPI.CreateEnableCCDAttr(True)
+        physxSceneAPI.CreateEnableStabilizationAttr(True)
+        physxSceneAPI.CreateEnableGPUDynamicsAttr(False)
+        physxSceneAPI.CreateBroadphaseTypeAttr("MBP")
+        physxSceneAPI.CreateSolverTypeAttr("TGS")
+
+        # Create a ground plane
+        PhysicsSchemaTools.addGroundPlane(stage, "/World/groundPlane", "Z", 1000, Gf.Vec3f(0, 0, -100), Gf.Vec3f(1.0))
+
     # Create 10 randomly positioned and coloured spheres and cube
     # We will assign each a semantic label based on their shape (sphere/cube)
+    prims = []
     for i in range(10):
         prim_type = random.choice(["Cube", "Sphere"])
         prim = stage.DefinePrim(f"/World/cube{i}", prim_type)
@@ -56,6 +80,55 @@ def main():
         sem.CreateSemanticDataAttr()
         sem.GetSemanticTypeAttr().Set("class")
         sem.GetSemanticDataAttr().Set(prim_type)
+
+        if ENABLE_PHYSICS:
+            # Add physics to prims
+            utils.setRigidBody(prim, "convexHull", False)
+            # Set Mass to 1 kg
+            mass_api = UsdPhysics.MassAPI.Apply(prim)
+            mass_api.CreateMassAttr(1)
+            prims.append(prim)
+
+    if ENABLE_PHYSICS:
+
+        # Apply glass material
+        if AND_ADD_GLASS:
+            for prim in prims:
+                # Create Glass material
+                mtl_created_list = []
+                kit.execute(
+                    "CreateAndBindMdlMaterialFromLibrary",
+                    mdl_name="OmniGlass.mdl",
+                    mtl_name="OmniGlass",
+                    mtl_created_list=mtl_created_list,
+                )
+                mtl_prim = stage.GetPrimAtPath(mtl_created_list[0])
+
+                # Set material inputs, these can be determined by looking at the .mdl file
+                # or by selecting the Shader attached to the Material in the stage window and looking at the details panel
+                color = Gf.Vec3f(random.random(), random.random(), random.random())
+                omni.usd.create_material_input(mtl_prim, "glass_color", color, Sdf.ValueTypeNames.Color3f)
+                omni.usd.create_material_input(mtl_prim, "glass_ior", 1.25, Sdf.ValueTypeNames.Float)
+                # This value is the volumetric light absorption scale, reduce to zero to make glass clearer
+                omni.usd.create_material_input(mtl_prim, "depth", 0.001, Sdf.ValueTypeNames.Float)
+                # Enable for thin glass objects if needed
+                omni.usd.create_material_input(mtl_prim, "thin_walled", False, Sdf.ValueTypeNames.Bool)
+                # Bind the material to the prim
+                prim_mat_shade = UsdShade.Material(mtl_prim)
+                UsdShade.MaterialBindingAPI(prim).Bind(prim_mat_shade, UsdShade.Tokens.strongerThanDescendants)
+        # force RayTracedLighting mode for better performance while simulating physics
+        kit.set_setting("/rtx/rendermode", "RayTracedLighting")
+
+        # start simulation
+        kit.play()
+        # Step simulation so that objects fall to rest
+        # wait until all materials are loaded
+        frame = 0
+        print("simulating physics...")
+        while frame < 60 or kit.is_loading():
+            kit.update(1 / 60.0)
+            frame = frame + 1
+        print("done")
 
     # Get groundtruth
     kit.update()
