3914a1d77cf2077be6c36c31c793481729388a04
Hammad Mazhar <hmazhar@nvidia.com>
2021-09-23 23:27:52 -0700

    Update Kaya Joystick

--- source/extensions/omni.isaac.samples/python/scripts/kaya_joystick.py
+++ source/extensions/omni.isaac.samples/python/scripts/kaya_joystick.py
@@ -13,18 +13,19 @@ import omni.appwindow
 import gc
 import numpy as np
 from omni.isaac.core.prims.xform_prim import XFormPrim
-from omni.isaac.jetbot import Jetbot
-from omni.isaac.jetbot.controllers import DifferentialController
+from omni.isaac.kaya import Kaya
+from omni.isaac.kaya.controllers import HolonomicController
 from omni.isaac.utils.scripts.nucleus_utils import find_nucleus_server
 from omni.isaac.core.tasks.task import BaseTask
 from omni.isaac.core.scenes.scene import Scene
 from omni.isaac.samples.scripts.base_sample import BaseSample
+from omni.isaac.manip import _manip, GamePadAxis
 
 
 class DriveTask(BaseTask):
     def __init__(self) -> None:
-        super().__init__("Drive Jetbot")
-        self.jetbot = None
+        super().__init__("Drive Kaya")
+        self.kaya = None
 
     def set_up_scene(self, scene: Scene) -> None:
         super().set_up_scene(scene)
@@ -35,15 +36,16 @@ class DriveTask(BaseTask):
             return
         asset_path = nucleus_server + "/Isaac"
 
-        self.jetbot = scene.add(
-            Jetbot(
+        self.kaya = scene.add(
+            Kaya(
                 stage=scene.stage,
-                prim_path="/jetbot",
-                name="my_jetbot",
+                prim_path="/kaya",
+                name="my_kaya",
                 position=np.array([0, 0.0, 2.0]),
                 orientation=np.array([1.0, 0.0, 0.0, 0.0]),
             )
         )
+
         prim = scene.stage.DefinePrim("/background", "Xform")
         prim.GetReferences().AddReference(asset_path + "/Environments/Grid/gridroom_curved.usd")
         XFormPrim(prim, "background", position=np.array([0, 0, -9]))
@@ -59,77 +61,66 @@ class DriveTask(BaseTask):
 class Extension(BaseSample):
     def on_startup(self, ext_id: str):
         super().on_startup(ext_id)
-        overview = "This Example shows how to simulate an NVIDIA Jetbot robot in Isaac Sim."
-        overview += "\n\tKeybord Input:"
-        overview += "\n\t\tw: Forward"
-        overview += "\n\t\ts: Reverse"
-        overview += "\n\t\ta: Spin Left"
-        overview += "\n\t\td: Spin Right"
+        overview = "This Example shows how to drive a NVIDIA Kaya robot using a Gamepad in Isaac Sim."
+        overview += "\n\nConnect a gamepad to the robot, and the press PLAY to begin simulating."
         overview += "\n\nPress the 'Open in IDE' button to view the source code."
 
         super()._on_startup(
             menu_name="Controlling",
             submenu_name="Input Devices",
-            name="Jetbot Keyboard",
+            name="Kaya Joystick",
             buttons_mapping={},
-            title="NVIDIA Jetbot Navigation Example",
-            doc_link="https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/sample_jetbot.html",
+            title="NVIDIA Kaya Joystick Example",
+            doc_link="https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/ext_omni_isaac_manip.html",
             overview=overview,
             file_path=os.path.abspath(__file__),
             add_ground_plane=False,
             stage_units_in_meters=0.01,
         )
         self._controller = None
-        self._command = [0.0, 0.0]
+        self._command = [0.0, 0.0, 0.0]
+        self._gains = (40.0, 40.0, 2.0)
+        self._joystick_deadzone = 0.2
 
     def _load_task(self):
         return DriveTask()
 
     def _setup_controllers(self):
-        self._controller = DifferentialController(name="simple_control")
+        self._controller = HolonomicController(name="simple_control")
         self._appwindow = omni.appwindow.get_default_app_window()
-        self._input = carb.input.acquire_input_interface()
-        self._keyboard = self._appwindow.get_keyboard()
-        self._sub_keyboard = self._input.subscribe_to_keyboard_events(self._keyboard, self._sub_keyboard_event)
-        self._world.add_physics_callback("jetbot_step", callback_fn=self._on_editor_step)
+        self._manip = _manip.acquire_manip_interface()
+        self._manip.bind_gamepad(self._sub_joystick_event)
+        self._world.add_physics_callback("kaya_step", callback_fn=self._on_editor_step)
         self._world.play()
 
     def _on_editor_step(self, step):
-        self._task.jetbot.apply_wheel_actions(self._controller.forward(command=self._command))
+        self._task.kaya.apply_wheel_actions(
+            self._controller.forward(self._command[0], self._command[1], self._command[2])
+        )
 
-    def _sub_keyboard_event(self, event, *args, **kwargs):
-        """Handle keyboard events
-        w,s,a,d as arrow keys for jetbot movement
+    def _sub_joystick_event(self, axis, signal):
+        if abs(signal) < self._joystick_deadzone:
+            signal = 0
 
-        Args:
-            event (int): keyboard event type
-        """
-        if (
-            event.type == carb.input.KeyboardEventType.KEY_PRESS
-            or event.type == carb.input.KeyboardEventType.KEY_REPEAT
-        ):
-            if event.input == carb.input.KeyboardInput.W:
-                self._command = [0.5, 0.0]
-            if event.input == carb.input.KeyboardInput.S:
-                self._command = [-0.5, 0.0]
-            if event.input == carb.input.KeyboardInput.A:
-                self._command = [0.0, 1.0]
-            if event.input == carb.input.KeyboardInput.D:
-                self._command = [0.0, -1.0]
-        if event.type == carb.input.KeyboardEventType.KEY_RELEASE:
-            self._command = [0.0, 0.0]
-
-        return True
+        if axis == GamePadAxis.eLeftStickY:
+            self._command[0] = signal * self._gains[0]
+        elif axis == GamePadAxis.eLeftStickX:
+            self._command[1] = -signal * self._gains[1]
+        elif axis == GamePadAxis.eRightStickX:
+            self._command[2] = -signal * self._gains[2]
+        else:
+            pass
 
     def on_shutdown(self):
         """Cleanup objects on extension shutdown"""
         super().on_shutdown()
-        self._controller = None
-        self._sub_keyboard = None
+        if self._controller:
+            self._controller = None
+            self._manip.unbind_gamepad()
         gc.collect()
 
     def _reset_call(self):
         self._controller.reset()
-        self._world.remove_physics_callback("jetbot_step")
-        self._world.add_physics_callback("jetbot_step", callback_fn=self._on_editor_step)
+        self._world.remove_physics_callback("kaya_step")
+        self._world.add_physics_callback("kaya_step", callback_fn=self._on_editor_step)
         self._world.play()
