3ed1f911c67f0a146e68592a8f824761037e004e
stambde <stambde@nvidia.com>
2021-11-17 20:18:11 +0530

    Fix train_shapenet example exit
    Nvbug:3439883

--- source/standalone_examples/replicator/online_generation/train_shapenet.py
+++ source/standalone_examples/replicator/online_generation/train_shapenet.py
@@ -56,7 +56,7 @@ def main(args):
     for i, train_batch in enumerate(train_loader):
         if i > args.max_iters or train_set.exiting:
             print("Exiting ...")
-            train_set.kit.shutdown()
+            train_set.kit.close()
             break
 
         model.train()
