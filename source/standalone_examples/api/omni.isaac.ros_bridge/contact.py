98b785ddc9c2b97bc80302bee7a54f9f3b835638
Qian Wan <qwan@nvidia.com>
2021-11-18 17:45:05 -0800

    Qwan/ros custom messages

--- source/standalone_examples/api/omni.isaac.ros_bridge/contact.py
+++ source/standalone_examples/api/omni.isaac.ros_bridge/contact.py
@@ -6,86 +6,61 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-import json
 import carb
 from omni.isaac.kit import SimulationApp
 
-# Example ROS bridge sample showing rospy and rosclock interaction
-kit = SimulationApp({"renderer": "RayTracedLighting", "headless": True})
+simulation_app = SimulationApp({"renderer": "RayTracedLighting", "headless": True})
+
 import omni
 from omni.isaac.contact_sensor import _contact_sensor
-from pxr import UsdGeom, UsdPhysics, Gf, PhysicsSchemaTools
 from omni.isaac.core.utils.extensions import enable_extension
+from omni.isaac.core import World
+from omni.isaac.core.objects import DynamicCuboid
 
 # enable ROS bridge extension
 enable_extension("omni.isaac.ros_bridge")
+
 # check if rosmaster node is running
 # this is to prevent this sample from waiting indefinetly if roscore is not running
 # can be removed in regular usage
-kit.update()
+simulation_app.update()
 result, check = omni.kit.commands.execute("RosBridgeRosMasterCheck")
 if not check:
     carb.log_error("Please run roscore before executing this script")
-    kit.close()
+    simulation_app.close()
     exit()
+
 # Note that this is not the system level rospy, but one compiled for omniverse
-from std_msgs.msg import String
+import numpy as np
 import rospy
+from isaac_tutorials.msg import ContactSensor
+
 
 rospy.init_node("contact_sample", anonymous=True, disable_signals=True, log_level=rospy.ERROR)
 
 timeline = omni.timeline.get_timeline_interface()
-ros_pub = rospy.Publisher("contact_report", String, queue_size=0)
+contact_pub = rospy.Publisher("/contact_report", ContactSensor, queue_size=0)
 cs = _contact_sensor.acquire_contact_sensor_interface()
-stage = omni.usd.get_context().get_stage()
-UsdGeom.SetStageMetersPerUnit(stage, 1.0)
-meters_per_unit = UsdGeom.GetStageMetersPerUnit(stage)
 
-# Add physics
-scene = UsdPhysics.Scene.Define(stage, "/physics/scene")
-scene.CreateGravityDirectionAttr().Set(Gf.Vec3f(0.0, 0.0, -1.0))
-scene.CreateGravityMagnitudeAttr().Set(9.81)
+meters_per_unit = 0.01
+ros_world = World(stage_units_in_meters=meters_per_unit)
 
+# add a cube in the world
 cube_path = "/cube"
-cube_geom = UsdGeom.Cube.Define(stage, cube_path)
-cubePrim = stage.GetPrimAtPath(cube_path)
-
-cube_geom.CreateSizeAttr(1)
-cube_geom.AddTranslateOp().Set((0, 0, 1.5))
-rigid_api = UsdPhysics.RigidBodyAPI.Apply(cubePrim)
-rigid_api.CreateRigidBodyEnabledAttr(True)
-# Prim that we are detecting collision events for must have collision API
-UsdPhysics.CollisionAPI.Apply(cubePrim)
+cube_1 = ros_world.scene.add(
+    DynamicCuboid(
+        prim_path=cube_path, name="cube_1", position=np.array([0, 0, 1.5]) * 100, size=np.array([1, 1, 1]) * 100
+    )
+)
 # Add a plane for cube to collide with
-PhysicsSchemaTools.addGroundPlane(stage, "/groundPlane", "Z", 1500, Gf.Vec3f(0, 0, 0), Gf.Vec3f(0.5))
-kit.update()
+ros_world.scene.add_default_ground_plane()
 
-
-def serialize(contact):
-    c = {}
-    c["time"] = float(contact["time"])
-    c["value"] = float(contact["value"] * meters_per_unit)
-    c["in_contact"] = bool(contact["inContact"])
-    return json.dumps(c)
-
-
-def serialize_raw(contact):
-    c = {}
-    c["time"] = float(contact["time"])
-    c["body0"] = str(cs.decode_body_name(contact["body0"]))
-    c["body1"] = str(cs.decode_body_name(contact["body1"]))
-    c["position"] = [
-        float(contact["position"][0]) * meters_per_unit,
-        float(contact["position"][1]) * meters_per_unit,
-        float(contact["position"][2]) * meters_per_unit,
-    ]
-    c["normal"] = [float(contact["normal"][0]), float(contact["normal"][1]), float(contact["normal"][2])]
-    c["impulse"] = [
-        float(contact["impulse"][0]) * meters_per_unit,
-        float(contact["impulse"][1]) * meters_per_unit,
-        float(contact["impulse"][2]) * meters_per_unit,
-    ]
-    return json.dumps(c)
+# putting contact sensor in the ContactSensor Message format
+def format_contact(c_out, contact):
+    c_out.time = float(contact["time"])
+    c_out.value = float(contact["value"] * meters_per_unit)
+    c_out.in_contact = bool(contact["inContact"])
+    return c_out
 
 
 # Setup contact sensor on cube
@@ -96,24 +71,27 @@ props.maxThreshold = 1000000000000
 props.sensorPeriod = 1.0 / 60.0
 body_path = cube_path
 sensor_handle = cs.add_sensor_on_body(body_path, props)
+
+# initiate the message handle
+c_out = ContactSensor()
+
 # start simulation
-omni.timeline.get_timeline_interface().play()
-for frame in range(60):
-    kit.update()
+timeline.play()
+for frame in range(10000):
+    ros_world.step(render=False)
+
     # Get processed contact data
     reading = cs.get_sensor_readings(sensor_handle)
     if reading.shape[0]:
         for r in reading:
-            print(serialize(r))
-            ros_pub.publish(serialize(r))
-    # Get raw contact data
-    raw_readings = cs.get_body_contact_raw_data(cube_path)
-    if raw_readings.shape[0]:
-        for r in raw_readings:
-            print(serialize_raw(r))
-            ros_pub.publish(serialize_raw(r))
+            print(r)
+            # pack the raw data into ContactSensor format and publish it
+            c = format_contact(c_out, r)
+            contact_pub.publish(c)
+
+
 # Cleanup
-ros_pub.unregister()
+contact_pub.unregister()
 rospy.signal_shutdown("contact_sample complete")
-omni.timeline.get_timeline_interface().stop()
-kit.close()
+timeline.stop()
+simulation_app.close()
