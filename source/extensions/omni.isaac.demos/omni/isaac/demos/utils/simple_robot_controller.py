86497217793a421e3559c29eee957311865e65f6
rgasoto <rgasoto@nvidia.com>
2021-11-19 12:47:55 -0500

    Remove Stanley controller code from simple controller file

--- source/extensions/omni.isaac.demos/omni/isaac/demos/utils/simple_robot_controller.py
+++ source/extensions/omni.isaac.demos/omni/isaac/demos/utils/simple_robot_controller.py
@@ -15,49 +15,6 @@ from omni.isaac.dynamic_control import _dynamic_control
 import numpy as np
 from omni.debugdraw import _debugDraw
 from .quintic_path_planner import QuinticPolynomial, quintic_polynomials_planner
-k = 0.5  # control gain
-max_steer = np.radians(5.0)  # [rad] max steering angle
-Kp = 0.1  # speed proportional gain
-
-
-class State(object):
-    """
-    Class representing the state of a vehicle.
-
-    :param x: (float) x-coordinate
-    :param y: (float) y-coordinate
-    :param yaw: (float) yaw angle
-    :param v: (float) speed
-    """
-
-    def __init__(self, wheel_base, x=0.0, y=0.0, yaw=0.0, v=0.0):
-        """Instantiate the object."""
-        super(State, self).__init__()
-        self.wheel_base = wheel_base
-        self.x = x
-        self.y = y
-        self.yaw = yaw
-        self.v = v
-        self.w = 0
-
-    def update(self, acceleration, delta, dt):
-        """
-        Update the state of the vehicle.
-
-        Stanley Control uses bicycle model.
-
-        :param acceleration: (float) Acceleration
-        :param delta: (float) Steering
-        """
-        delta = np.clip(delta, -max_steer, max_steer)
-
-        self.x += self.v * np.cos(self.yaw) * dt
-        self.y += self.v * np.sin(self.yaw) * dt
-        self.w = self.v / self.wheel_base * np.tan(delta)
-        self.yaw += self.w * dt
-        self.yaw = normalize_angle(self.yaw)
-        self.v += acceleration * dt
-
 
 def calc_speed_profile(cyaw, max_speed, target_speed, min_speed=1):
     speed_profile = np.array(cyaw) / max([abs(c) for c in cyaw]) * max_speed
@@ -91,84 +48,6 @@ def calc_speed_profile(cyaw, max_speed, target_speed, min_speed=1):
 
     return speed_profile
 
-
-def normalize_angle(angle):
-    """
-    Normalize an angle to [-pi, pi].
-
-    :param angle: (float)
-    :return: (float) Angle in radian in [-pi, pi]
-    """
-    while angle > np.pi:
-        angle -= 2.0 * np.pi
-
-    while angle < -np.pi:
-        angle += 2.0 * np.pi
-
-    return angle
-
-
-def calc_target_index(state, cx, cy):
-    """
-    Compute index in the trajectory list of the target.
-
-    :param state: (State object)
-    :param cx: [float]
-    :param cy: [float]
-    :return: (int, float)
-    """
-    # Calc front axle position
-    fx = state.x + state.wheel_base * np.cos(state.yaw)
-    fy = state.y + state.wheel_base * np.sin(state.yaw)
-
-    # Search nearest point index
-    dx = [fx - icx for icx in cx]
-    dy = [fy - icy for icy in cy]
-    d = np.hypot(dx, dy)
-    target_idx = np.argmin(d)
-
-    # Project RMS error onto front axle vector
-    front_axle_vec = [-np.cos(state.yaw + np.pi / 2), -np.sin(state.yaw + np.pi / 2)]
-    error_front_axle = np.dot([dx[target_idx], dy[target_idx]], front_axle_vec)
-
-    return target_idx, error_front_axle
-
-
-def pid_control(target, current):
-    """
-    Proportional control for the speed.
-
-    :param target: (float)
-    :param current: (float)
-    :return: (float)
-    """
-    return Kp * (target - current)
-
-
-def stanley_control(state, cx, cy, cyaw, last_target_idx):
-    """
-    Stanley steering control.
-
-    :param state: (State object)
-    :param cx: ([float])
-    :param cy: ([float])
-    :param cyaw: ([float])
-    :param last_target_idx: (int)
-    :return: (float, int)
-    """
-    current_target_idx, error_front_axle = calc_target_index(state, cx, cy)
-
-    if last_target_idx >= current_target_idx:
-        current_target_idx = last_target_idx
-
-    # theta_e corrects the heading error
-    theta_e = normalize_angle(cyaw[current_target_idx] - state.yaw)
-    # theta_d corrects the cross track error
-    theta_d = np.arctan2(k * error_front_axle, state.v)
-    # Steering control
-    delta = theta_e + theta_d
-
-    return delta, current_target_idx
 
 
 class RobotController:
