4b151dbdde6b683a5a6612c57529e8291b27a3f5
Sheikh Dawood <sabdulajees@nvidia.com>
2021-12-09 12:17:23 -0600

    (OM-41824) Fix postinstall

--- source/scripts/omni.isaac.sim.post.install.sh
+++ source/scripts/omni.isaac.sim.post.install.sh
@@ -3,7 +3,9 @@ set -e
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
 
 # Add symlink to Isaac Examples
+pushd ${SCRIPT_DIR}
 ln -s exts/omni.isaac.examples/omni/isaac/examples extension_examples
+popd
 # Warm up shader cache
 ${SCRIPT_DIR}/omni.isaac.sim.warmup.sh
 # Install default Python packages 
