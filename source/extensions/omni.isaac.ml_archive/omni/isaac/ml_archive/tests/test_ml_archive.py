db2d8cc700d57705df3785bd7c968d8c61e4cf6c
Hammad Mazhar <hmazhar@nvidia.com>
2022-01-13 14:11:13 -0800

    split pip archive to core archive and ml archive

---
+++ source/extensions/omni.isaac.ml_archive/omni/isaac/ml_archive/tests/test_ml_archive.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
+import omni.kit.test
+import omni.kit.pipapi
+
+
+class TestPipArchive(omni.kit.test.AsyncTestCase):
+    async def test_ml_archive(self):
+        # Take one of packages from deps/pip.toml, it should be prebundled and available without need for going into online index
+        omni.kit.pipapi.install("torch", version="1.9.1+cu111", use_online_index=False)
+        import scipy
+
+        self.assertIsNotNone(scipy)
+
+    # import all packages to make sure dependencies were not missed
+    async def test_import_all(self):
+        import torch
+        import torchvision
+
+        self.assertIsNotNone(torch)
+        self.assertIsNotNone(torchvision)
