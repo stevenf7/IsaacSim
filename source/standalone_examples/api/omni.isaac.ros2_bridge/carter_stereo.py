8609254b7da1bfd89d22f70d553ba2c144b7ebad
Ayush Ghosh <ayushg@nvidia.com>
2022-03-17 19:14:58 -0700

    Added ROS2 Standalone Examples

--- source/standalone_examples/api/omni.isaac.ros2_bridge/carter_stereo.py
+++ source/standalone_examples/api/omni.isaac.ros2_bridge/carter_stereo.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2021, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2020-2022, NVIDIA CORPORATION.  All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
 # and proprietary rights in and to this software, related documentation
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(description="Generate Occluded and Unoccluded d
 parser.add_argument("--test", action="store_true")
 args, unknown = parser.parse_known_args()
 
-# Example ROS bridge sample showing manual control over messages
+# Example ROS2 bridge sample showing manual control over messages
 kit = SimulationApp({"renderer": "RayTracedLighting", "headless": False})
 import omni
 from omni.isaac.core.utils.nucleus import get_assets_root_path
@@ -23,8 +23,8 @@ from pxr import Sdf
 
 from omni.isaac.core.utils.extensions import enable_extension
 
-# enable ROS bridge extension
-enable_extension("omni.isaac.ros_bridge")
+# enable ROS2 bridge extension
+enable_extension("omni.isaac.ros2_bridge")
 
 # Locate assets root folder to load sample
 assets_root_path = get_assets_root_path()
@@ -75,15 +75,15 @@ omni.kit.commands.execute(
 omni.kit.commands.execute("ChangeProperty", prop_path=Sdf.Path("/World/ROS_Clock.enabled"), value=False, prev=None)
 simulation_context.play()
 simulation_context.step()
-# Tick all of the components once to make sure all of the ROS nodes are initialized
+# Tick all of the components once to make sure all of the ROS2 nodes are initialized
 # For cameras this also handles viewport initialization etc.
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Right")
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Left")
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Lidar")
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_DifferentialBase")
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Sensors_Broadcaster")
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Broadcaster")
-omni.kit.commands.execute("RosBridgeTickComponent", path="/World/ROS_Clock")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Right")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Left")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Lidar")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_DifferentialBase")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Sensors_Broadcaster")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Broadcaster")
+omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/ROS_Clock")
 # Simulate for one second to warm up sim and let everything settle
 for frame in range(60):
     simulation_context.step()
@@ -96,44 +96,42 @@ if right_viewport is not None and left_viewport is not None:
 right_viewport = None
 left_viewport = None
 
-result, check = omni.kit.commands.execute("RosBridgeRosMasterCheck")
-if not check:
-    carb.log_error("Please run roscore before executing this script")
-    kit.close()
-    exit()
-
 # Create a rostopic to publish message to spin robot in place
-# Note that this is not the system level rospy, but one compiled for omniverse
-from geometry_msgs.msg import Twist
-import rospy
+# Note that this is not the system level rclpy, but one compiled for omniverse
 
-rospy.init_node("carter_stereo", anonymous=True, disable_signals=True, log_level=rospy.ERROR)
-pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+import rclpy
+
+rclpy.init()
+
+from geometry_msgs.msg import Twist
+
+node = rclpy.create_node("carter_stereo")
+publisher = node.create_publisher(Twist, "cmd_vel", 10)
 
 frame = 0
 while kit.is_running():
     # Run with a fixed step size
     simulation_context.step()
     # Publish clock every frame
-    omni.kit.commands.execute("RosBridgeTickComponent", path="/World/ROS_Clock")
+    omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/ROS_Clock")
     # publish TF and Lidar every 2 frames
     if frame % 2 == 0:
-        omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Lidar")
-        omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_DifferentialBase")
-        omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Sensors_Broadcaster")
-        omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Broadcaster")
+        omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Lidar")
+        omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_DifferentialBase")
+        omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Sensors_Broadcaster")
+        omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Carter_Broadcaster")
         # because we only tick the differential base component every two frames, we can also publish the ROS message at the same rate
         message = Twist()
         message.angular.z = 0.2  # spin in place
-        pub.publish(message)
+        publisher.publish(message)
     # Publish cameras every 15 frames
     if frame % 15 == 0:
-        omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Right")
-        omni.kit.commands.execute("RosBridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Left")
+        omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Right")
+        omni.kit.commands.execute("Ros2BridgeTickComponent", path="/World/Carter_ROS/ROS_Camera_Stereo_Left")
     if args.test and frame > 120:
         break
     frame = frame + 1
-pub.unregister()
-rospy.signal_shutdown("carter_stereo complete")
+node.destroy_node()
+rclpy.shutdown()
 simulation_context.stop()
 kit.close()
