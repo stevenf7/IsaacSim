29afeebb478adc45205b7c44ad496bc8e8c55944
Renato Gasoto <rgasoto@nvidia.com>
2022-04-06 11:35:07 -0700

    Implement Conveyor belt utility

---
+++ source/extensions/omni.isaac.conveyor/python/scripts/extension.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+import omni.ext
+import omni.ui as ui
+import gc
+import carb
+import omni.kit.commands
+from omni.kit.menu.utils import add_menu_items, remove_menu_items, MenuItemDescription
+from pxr import Sdf, UsdGeom, Gf
+import weakref
+
+
+class Extension(omni.ext.IExt):
+    def __init__(self) -> None:
+        menu_items = [
+            MenuItemDescription(
+                name="Warehouse Items",
+                sub_menu=[
+                    MenuItemDescription(name="Conveyor", onclick_fn=lambda a=weakref.proxy(self): a._add_conveyor())
+                ],
+            )
+        ]
+
+        self._menu_items = [MenuItemDescription(name="Isaac", glyph="plug.svg", sub_menu=menu_items)]
+
+        add_menu_items(self._menu_items, "Create")
+
+    def on_shutdown(self):
+        remove_menu_items(self._menu_items, "Create")
+        gc.collect()
+
+    def _add_conveyor(self, *args, **kwargs):
+        result, prim = omni.kit.commands.execute("CreateConveyorBelt")
