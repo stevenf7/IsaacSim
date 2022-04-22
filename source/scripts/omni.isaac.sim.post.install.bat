ef597aa06787e5b885ce53285bee0fcf881da73f
Sheikh Dawood Abdul Ajees <sabdulajees@nvidia.com>
2022-04-22 09:36:39 -0700

    Clean up kit files and add production tag

--- source/scripts/omni.isaac.sim.post.install.bat
+++ source/scripts/omni.isaac.sim.post.install.bat
@@ -1,3 +1,12 @@
 @echo off
 setlocal
+
+@REM Warm up shader cache
+echo "Warming up cache..."
 call "%~dp0omni.isaac.sim.warmup.bat"
+
+@REM Install default Python packages 
+echo "Installing Python packages..."
+call "%~dp0python.sh -m pip install -r %~dp0requirements.txt
+
+echo "Isaac Sim post installation script completed!"
--- source/scripts/omni.isaac.sim.warmup.sh
+++ source/scripts/omni.isaac.sim.warmup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
+
+# Use half of available CPU cores for the warmup not to take all the resources from user's PC during installation
 TASKING_THREAD_CNT=$(nproc --all)
 TASKING_THREAD_CNT=$(($TASKING_THREAD_CNT/2))
-exec "$SCRIPT_DIR/kit/kit" "$SCRIPT_DIR/apps/omni.isaac.sim.warmup.kit" --no-window --ext-folder "$SCRIPT_DIR/exts" --ext-folder "$SCRIPT_DIR/apps" --/exts/omni.isaac.assets_check/nucleusCheckOverride=true --/plugins/carb.tasking.plugin/threadCount=$TASKING_THREAD_CNT
+exec "$SCRIPT_DIR/kit/kit" "$SCRIPT_DIR/apps/omni.isaac.sim.kit" \
+    --no-window \
+    --ext-folder "$SCRIPT_DIR/exts" \
+    --ext-folder "$SCRIPT_DIR/apps" \
+    --/app/settings/persistent=0 \
+    --/app/settings/loadUserConfig=0 \
+    --/structuredLog/enable=0 \
+    --/app/hangDetector/enabled=0 \
+    --/app/content/emptyStageOnStart=1 \
+    --/rtx/materialDb/syncLoads=1 \
+    --/omni.kit.plugin/syncUsdLoads=1 \
+    --/rtx/hydra/materialSyncLoads=1 \
+    --/app/asyncRendering=0 \
+    --/app/quitAfter=10 \
+    --/persistent/exts/omni.isaac.assets_check/nucleusCheck=false \
+    --/exts/omni.kit.registry.nucleus/registries/0/name=0 \
+    --/plugins/carb.tasking.plugin/threadCount=$TASKING_THREAD_CNT \
+    "$@"
