929027f0d8b6f3c39c389854080cbe0aeb21a189
Matthew Trepte <mtrepte@nvidia.com>
2022-06-14 13:09:01 -0700

    Isaac Statistics extension and plotting util

--- source/extensions/omni.isaac.statistics_logging/omni/isaac/statistics_logging/extension.py
+++ source/extensions/omni.isaac.statistics_logging/omni/isaac/statistics_logging/extension.py
@@ -7,48 +7,99 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 #
 
-import asyncio
-import sys
+import datetime
 import os
+import time
+import yaml
+
 import carb.settings
 import carb.tokens
 import omni
-from omni.isaac.core.utils.statistics import get_memory_stats
+from omni.isaac.core.utils.statistics import get_memory_stats, plot_statistics_log
 
 
 class Extension(omni.ext.IExt):
     def on_startup(self, ext_id: str):
-        # printing all settings for illustrative purposes
+
+        # Get settings
         settings = carb.settings.get_settings()
-        carb.log_warn(f'{settings.get("/exts/omni.isaac.statistics_logging/resetLogOnStart")}')
-        carb.log_warn(f'{settings.get("/exts/omni.isaac.statistics_logging/logFilePath")}')
-        carb.log_warn(f'{settings.get("/exts/omni.isaac.statistics_logging/logMode")}')
-        carb.log_warn(f'{settings.get("/exts/omni.isaac.statistics_logging/logEveryNSeconds")}')
-        carb.log_warn(f'{settings.get("/exts/omni.isaac.statistics_logging/logEveryNFrames")}')
+        self.reset_log_on_start = settings.get("/exts/omni.isaac.statistics_logging/resetLogOnStart")
+        self.log_file_path = settings.get("/exts/omni.isaac.statistics_logging/logFilePath")
+        self.log_mode = settings.get("/exts/omni.isaac.statistics_logging/logMode")
+        self.log_verbose = settings.get("/exts/omni.isaac.statistics_logging/verbose")
+        self.log_every_n_seconds = settings.get("/exts/omni.isaac.statistics_logging/logEveryNSeconds")
+        self.log_every_n_frames = settings.get("/exts/omni.isaac.statistics_logging/logEveryNFrames")
+        self.output_plot = settings.get("/exts/omni.isaac.statistics_logging/outputPlot")
 
-        # get the path to the log file
-        log_file_path = settings.get("/exts/omni.isaac.statistics_logging/logFilePath")
-        # the file is blank, locate default log directory
-        if log_file_path == "":
-            log_file_path = carb.tokens.get_tokens_interface().resolve("${logs}") + "/isaac_statistics_log.yaml"
+        if self.log_mode not in ["seconds", "frames"]:
+            raise ValueError(
+                "Isaac Statistics log mode setting: '{}' must be either 'seconds' or 'frames'.".format(self.log_mode)
+            )
 
-        carb.log_warn(f"log_file_path: {log_file_path}")
+        # Default log file path, if needed
+        if self.log_file_path == "":
+            self.log_file_path = carb.tokens.get_tokens_interface().resolve("${logs}") + "/isaac_statistics/log.yaml"
+
+        carb.log_info(f"Logging statistics to file: {self.log_file_path}")
+
+        # Create log path, if needed
+        log_path = os.path.dirname(self.log_file_path)
+        if not os.path.exists(log_path):
+            os.makedirs(log_path, exist_ok=True)
+
+        # Create log file, if needed
+        create_new_file = self.reset_log_on_start or not os.path.exists(self.log_file_path)
+        if create_new_file:
+            open(self.log_file_path, "w").close()
+
+        # Initialize vars
+        self._frame_counter = 0
+        self._timer = 0
+        self._last_update_time = time.time()
 
         self._update_event_subscription = (
             omni.kit.app.get_app().get_update_event_stream().create_subscription_to_pop(self._on_update_event)
         )
-        # dummy counter
-        self._counter = 0
-        pass
 
     def on_shutdown(self):
-        # do any cleanup here
+        if self.output_plot:
+            plot_statistics_log(self.log_file_path)
+
         self._update_event_subscription = None
-        pass
 
     def _on_update_event(self, delta):
-        # check if N frames or N seconds have passed here
-        if self._counter % 100 == 0:
-            carb.log_warn("N Frames have passed")
-        self._counter += 1
-        pass
+        if self.log_mode == "seconds":
+            current_time = time.time()
+            time_delta = current_time - self._last_update_time
+            self._last_update_time = current_time
+
+            self._timer += time_delta
+            while self._timer > self.log_every_n_seconds:
+                self._log_stamp()
+                self._timer -= self.log_every_n_seconds
+        else:
+            self._frame_counter += 1
+            if self._frame_counter > self.log_every_n_frames:
+                self._log_stamp()
+                self._frame_counter = 0
+
+    def _log_stamp(self):
+        memory_stats = get_memory_stats()
+
+        if not self.log_verbose:
+            # Filter-out statistics
+            memory_stats = {"System Memory": memory_stats["System Memory"]}
+
+        memory_stats["mode"] = self.log_mode
+        if self.log_mode == "seconds":
+            memory_stats["step"] = self.log_every_n_seconds
+        else:
+            memory_stats["step"] = self.log_every_n_frames
+
+        # Key log entry with timestamp
+        timestamp = str(datetime.datetime.now())
+        memory_stats = {timestamp: memory_stats}
+
+        # Append to the end of log
+        with open(self.log_file_path, "a") as f:
+            yaml.safe_dump(memory_stats, f)
