207d00425dce5032c8ebd7b3ba215cf1d07f7cd4
Hammad Mazhar <hmazhar@nvidia.com>
2022-06-27 09:53:34 -0700

    Reorganize statistics logging code

--- source/extensions/omni.isaac.kit/omni/isaac/kit/simulation_app.py
+++ source/extensions/omni.isaac.kit/omni/isaac/kit/simulation_app.py
@@ -16,6 +16,7 @@ import re
 import carb
 import omni.kit.app
 import builtins
+import warnings
 
 
 class SimulationApp:
@@ -97,7 +98,7 @@ class SimulationApp:
         max_volume_bounces (int): Maximum number of bounces for volumetric materials, used for `PathTracing` only. Defaults to 4
         open_usd (str): This is the name of the usd to open when the app starts. It will not be saved over. Default is None and an empty stage is created on startup.
         livesync_usd (str): This is the location of the usd that you want to do your interactive work in.  The existing file is overwritten. Default is None
-        memory_report (bool): Set to true to print a memory usage report on exit. Default is False
+        memory_report (bool): Deprecated, enable the omni.isaac.statistics_logging extension to log directly to disk
     """
 
     def __init__(self, launch_config: dict = None, experience: str = "") -> None:
@@ -217,9 +218,10 @@ class SimulationApp:
         self._wait_for_viewport()
 
         if self.config.get("memory_report"):
-            from omni.isaac.core.utils.statistics import get_memory_stats
-
-            self.start_memory_stats = get_memory_stats()
+            warnings.warn(
+                "memory_report option is deprecated, please enable the omni.isaac.statistics_logging extension to log directly to disk",
+                DeprecationWarning,
+            )
         # Notify toolkit is running
         self._app.print_and_log("Simulation App Startup Complete")
 
@@ -425,13 +427,6 @@ class SimulationApp:
         if not self._exiting:
             self._exiting = True
             self._app.print_and_log("Simulation App Shutting Down")
-            if self.config.get("memory_report"):
-                from pprint import pprint
-                from omni.isaac.core.utils.statistics import get_memory_stats, get_memory_delta
-
-                self.end_memory_stats = get_memory_stats()
-                print("Memory usage delta:\n")
-                pprint(get_memory_delta(self.start_memory_stats, self.end_memory_stats))
 
             # We are exisitng but something is still loading, wait for it to load to avoid a deadlock
             from .utils import is_stage_loading
--- source/extensions/omni.isaac.statistics_logging/docs/CHANGELOG.md
+++ source/extensions/omni.isaac.statistics_logging/docs/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [1.1.0] - 2022-06-23
+### Added
+- statistics.py from omni.isaac.core
+- read_log_file
+- summarize_statistics_log
+
 ## [1.0.0] - 2022-06-14
 
 ### Changed
