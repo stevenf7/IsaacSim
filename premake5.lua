0cfafcb9f8b4ecd653b91935c9e3c2d98152c10d
Mark Carlson <mcarlson@nvidia.com>
2022-10-19 21:50:08 -0700

    RTX Lidar cos check

--- premake5.lua
+++ premake5.lua
@@ -376,6 +376,7 @@ repo_build.prebuild_copy {
     {"source/extensions/omni.isaac.isaac_sensor/data/lidar_configs/hesei/*.json", "_build/%{platform}/%{config}/exts/omni.drivesim.sensors.nv.lidar/data"},
     {"source/extensions/omni.isaac.isaac_sensor/data/lidar_configs/ouster/*.json", "_build/%{platform}/%{config}/exts/omni.drivesim.sensors.nv.lidar/data"},
     {"source/extensions/omni.isaac.isaac_sensor/data/lidar_configs/slamtec/*.json", "_build/%{platform}/%{config}/exts/omni.drivesim.sensors.nv.lidar/data"},
+    {"source/extensions/omni.isaac.isaac_sensor/data/lidar_configs/*.json", "_build/%{platform}/%{config}/exts/omni.drivesim.sensors.nv.lidar/data"},
 }
 
 group "python_samples"
