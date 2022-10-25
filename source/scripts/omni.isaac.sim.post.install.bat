ce181bc830385d0cd6dee9ee723af3e732ffddf0
sabdulajees <sabdulajees@nvidia.com>
2022-10-25 11:45:35 -0500

    Add ISAACSIM_SKIP_WARMUP check to windows postinstall

--- source/scripts/omni.isaac.sim.post.install.bat
+++ source/scripts/omni.isaac.sim.post.install.bat
@@ -6,10 +6,12 @@ echo "Creating extension_examples symlink..."
 cmd /C "%~dp0omni.isaac.sim.create_junction.bat"
 
 @REM Warm up shader cache
-echo "Warming up cache for main app..."
-call "%~dp0omni.isaac.sim.warmup.bat"
-echo "Warming up cache for python app..."
-call "%~dp0python.bat" "%~dp0standalone_examples\api\omni.isaac.kit\hello_world.py"
+@If Not Defined ISAACSIM_SKIP_WARMUP (
+    echo "Warming up cache for main app..."
+    call "%~dp0omni.isaac.sim.warmup.bat"
+    echo "Warming up cache for python app..."
+    call "%~dp0python.bat" "%~dp0standalone_examples\api\omni.isaac.kit\hello_world.py"
+)
 
 @REM Install default Python packages 
 echo "Installing Python packages..."
