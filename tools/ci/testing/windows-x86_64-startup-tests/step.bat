7e7aae074ffc0a8692da025fc4d2cd08f690c47f
sabdulajees <sabdulajees@nvidia.com>
2022-10-27 11:34:08 -0500

    Split startup tests

--- tools/ci/testing/windows-x86_64-startup-tests/step.bat
+++ tools/ci/testing/windows-x86_64-startup-tests/step.bat
@@ -1,8 +1,6 @@
 @echo off
 
 :: tests
-call "%~dp0..\..\..\..\tools\test.bat" --suite postinstalltests --from-package --config release
 call "%~dp0..\..\..\..\tools\test.bat" --suite startuptests --from-package --config release
-call "%~dp0..\..\..\..\tools\test.bat" --suite selectortests --from-package --config release
 if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
