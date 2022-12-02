95e1514db5d1dc3faa0144effcb759e49a6ee2e2
Andrei Haidu <ahaidu@nvidia.com>
2022-12-02 01:01:48 +0100

    offline generation crash fix, restructured script, bumped semantics schema editor to 0.3.1

--- source/standalone_examples/replicator/offline_generation.py
+++ source/standalone_examples/replicator/offline_generation.py
@@ -13,7 +13,7 @@ import os
 
 # Set rendering parameters and create an instance of kit
 CONFIG = {"renderer": "RayTracedLighting", "headless": True, "width": 1024, "height": 1024, "num_frames": 10}
-kit = SimulationApp(launch_config=CONFIG)
+simulation_app = SimulationApp(launch_config=CONFIG)
 
 ENV_URL = "/Isaac/Environments/Simple_Warehouse/full_warehouse.usd"
 FORKLIFT_URL = "/Isaac/Props/Forklift/forklift.usd"
@@ -47,8 +47,7 @@ rep.settings.carb_settings("/omni/replicator/RTSubframes", 2)
 def prefix_with_isaac_asset_server(relative_path):
     assets_root_path = get_assets_root_path()
     if assets_root_path is None:
-        carb.log_error("Nucleus server not found, could not get Isaac Sim assets folder")
-        return
+        raise Exception("Nucleus server not found, could not access Isaac Sim assets folder")
     return assets_root_path + relative_path
 
 
@@ -58,13 +57,19 @@ def register_scatter_boxes(prim):
     bb_cache = create_bbox_cache()
     bbox3d_gf = bb_cache.ComputeLocalBound(prim)
     prim_tf_gf = omni.usd.get_world_transform_matrix(prim)
+
+    # Calculate the bounds of the prim
     bbox3d_gf.Transform(prim_tf_gf)
     range_size = bbox3d_gf.GetRange().GetSize()
+
+    # Get the quaterion of the prim in xyzw format from usd
+    prim_quat_gf = prim_tf_gf.ExtractRotation().GetQuaternion()
+    prim_quat_xyzw = (prim_quat_gf.GetReal(), *prim_quat_gf.GetImaginary())
 
     # Create a plane on the pallet to scatter the boxes on
     plane_scale = (range_size[0] * 0.8, range_size[1] * 0.8, 1)
     plane_pos_gf = prim_tf_gf.ExtractTranslation() + Gf.Vec3d(0, 0, range_size[2])
-    plane_rot_euler_deg = quat_to_euler_angles(np.array(forklift_quat_xyzw), degrees=True)
+    plane_rot_euler_deg = quat_to_euler_angles(np.array(prim_quat_xyzw), degrees=True)
     scatter_plane = rep.create.plane(
         scale=plane_scale, position=plane_pos_gf, rotation=plane_rot_euler_deg, visible=False
     )
@@ -129,7 +134,7 @@ def register_lights_placement(forklift_prim, pallet_prim):
     rep.randomizer.register(randomize_lights)
 
 
-def simulate_falling_objects(prim, num_sim_steps=200, num_boxes=8):
+def simulate_falling_objects(prim, num_sim_steps=250, num_boxes=8):
     # Create a simulation ready world
     world = World(physics_dt=1.0 / 90.0, stage_units_in_meters=1.0)
 
@@ -145,13 +150,6 @@ def simulate_falling_objects(prim, num_sim_steps=200, num_boxes=8):
         usd_path=prefix_with_isaac_asset_server(PALLET_URL),
         semantic_label="Pallet",
     )
-
-    # Change the collision aproximation to convex hull
-    collision_api = UsdPhysics.CollisionAPI.Apply(pallet_prim)
-    physx_collision_api = PhysxSchema.PhysxCollisionAPI.Apply(pallet_prim)
-    pallet_mesh_collision_api = UsdPhysics.MeshCollisionAPI.Apply(pallet_prim)
-    pallet_mesh_collision_api.GetApproximationAttr().Set("convexHull")
-    # UsdPhysics.MeshCollisionAPI(pallet_prim).GetApproximationAttr().Set("convexHull")
 
     # Get the height of the pallet
     bb_cache = create_bbox_cache()
@@ -180,13 +178,6 @@ def simulate_falling_objects(prim, num_sim_steps=200, num_boxes=8):
             semantic_label="Cardbox",
         )
 
-        # Change the collision aproximation to convex hull
-        collision_api = UsdPhysics.CollisionAPI.Apply(box_prim)
-        physx_collision_api = PhysxSchema.PhysxCollisionAPI.Apply(box_prim)
-        mesh_collision_api = UsdPhysics.MeshCollisionAPI.Apply(box_prim)
-        mesh_collision_api.GetApproximationAttr().Set("convexHull")
-        # UsdPhysics.MeshCollisionAPI(box_prim).GetApproximationAttr().Set("convexHull")
-
         # Add the height of the box to the current spawn height
         curr_spawn_height += bb_cache.ComputeLocalBound(box_prim).GetRange().GetSize()[2] * 1.1
 
@@ -212,7 +203,7 @@ def simulate_falling_objects(prim, num_sim_steps=200, num_boxes=8):
     for i in range(num_sim_steps):
         world.step(render=False)
         if last_box and np.linalg.norm(last_box.get_linear_velocity()) < 0.001:
-            print(f"Simulation stopped early after {i} steps")
+            print(f"Simulation stopped after {i} steps")
             break
 
 
@@ -222,119 +213,127 @@ def run_orchestrator():
 
     # Wait until started
     while not rep.orchestrator.get_is_started():
-        kit.update()
+        simulation_app.update()
 
     # Wait until stopped
     while rep.orchestrator.get_is_started():
-        kit.update()
+        simulation_app.update()
 
     rep.BackendDispatch.wait_until_done()
     rep.orchestrator.stop()
 
 
-# Open the environment in a new stage
-print(f"Loading Stage {ENV_URL}")
-open_stage(prefix_with_isaac_asset_server(ENV_URL))
+def main():
+    # Open the environment in a new stage
+    print(f"Loading Stage {ENV_URL}")
+    open_stage(prefix_with_isaac_asset_server(ENV_URL))
 
-# Create a custom scope for newly added prims
-stage = get_current_stage()
-scope = UsdGeom.Scope.Define(stage, SCOPE_NAME)
+    # Create a custom scope for newly added prims
+    stage = get_current_stage()
+    scope = UsdGeom.Scope.Define(stage, SCOPE_NAME)
 
-# Spawn a new forklift at a random pose
-forklift_prim = prims.create_prim(
-    prim_path=f"{SCOPE_NAME}/Forklift",
-    position=(random.uniform(-20, -2), random.uniform(-1, 3), 0),
-    orientation=euler_angles_to_quat([0, 0, random.uniform(0, math.pi)]),
-    usd_path=prefix_with_isaac_asset_server(FORKLIFT_URL),
-    semantic_label="Forklift",
-)
+    # Spawn a new forklift at a random pose
+    forklift_prim = prims.create_prim(
+        prim_path=f"{SCOPE_NAME}/Forklift",
+        position=(random.uniform(-20, -2), random.uniform(-1, 3), 0),
+        orientation=euler_angles_to_quat([0, 0, random.uniform(0, math.pi)]),
+        usd_path=prefix_with_isaac_asset_server(FORKLIFT_URL),
+        semantic_label="Forklift",
+    )
 
-# Spawn the pallet in front of the forklift with a random offset on the Y axis
-forklift_tf = omni.usd.get_world_transform_matrix(forklift_prim)
-pallet_offset_tf = Gf.Matrix4d().SetTranslate(Gf.Vec3d(0, random.uniform(-1.2, -2.4), 0))
-pallet_pos_gf = (pallet_offset_tf * forklift_tf).ExtractTranslation()
-forklift_quat_gf = forklift_tf.ExtractRotation().GetQuaternion()
-forklift_quat_xyzw = (forklift_quat_gf.GetReal(), *forklift_quat_gf.GetImaginary())
+    # Spawn the pallet in front of the forklift with a random offset on the Y axis
+    forklift_tf = omni.usd.get_world_transform_matrix(forklift_prim)
+    pallet_offset_tf = Gf.Matrix4d().SetTranslate(Gf.Vec3d(0, random.uniform(-1.2, -2.4), 0))
+    pallet_pos_gf = (pallet_offset_tf * forklift_tf).ExtractTranslation()
+    forklift_quat_gf = forklift_tf.ExtractRotation().GetQuaternion()
+    forklift_quat_xyzw = (forklift_quat_gf.GetReal(), *forklift_quat_gf.GetImaginary())
 
-pallet_prim = prims.create_prim(
-    prim_path=f"{SCOPE_NAME}/Pallet",
-    position=pallet_pos_gf,
-    orientation=forklift_quat_xyzw,
-    usd_path=prefix_with_isaac_asset_server(PALLET_URL),
-    semantic_label="Pallet",
-)
+    pallet_prim = prims.create_prim(
+        prim_path=f"{SCOPE_NAME}/Pallet",
+        position=pallet_pos_gf,
+        orientation=forklift_quat_xyzw,
+        usd_path=prefix_with_isaac_asset_server(PALLET_URL),
+        semantic_label="Pallet",
+    )
 
-# Simulate dropping objects on a pile behind the forklift
-simulate_falling_objects(forklift_prim)
+    # Simulate dropping objects on a pile behind the forklift
+    simulate_falling_objects(forklift_prim)
 
-# Register randomizers
-register_scatter_boxes(pallet_prim)
-register_cone_placement(forklift_prim, pallet_prim)
-register_lights_placement(forklift_prim, pallet_prim)
+    # Register randomizers
+    register_scatter_boxes(pallet_prim)
+    register_cone_placement(forklift_prim, pallet_prim)
+    register_lights_placement(forklift_prim, pallet_prim)
 
-# Spawn a camera in the driver's location looking at the pallet
-foklift_pos_gf = forklift_tf.ExtractTranslation()
-driver_cam_pos_gf = foklift_pos_gf + Gf.Vec3d(0.0, 0.0, 1.9)
-rotate_180_on_y_quat_gf = Gf.Quatf(0, 0, 1, 0)
-look_at_pallet_quat_gf = lookat_to_quatf(driver_cam_pos_gf, pallet_pos_gf, (0, 0, 1)) * rotate_180_on_y_quat_gf
-look_at_pallet_xyzw = (look_at_pallet_quat_gf.GetReal(), *look_at_pallet_quat_gf.GetImaginary())
+    # Spawn a camera in the driver's location looking at the pallet
+    foklift_pos_gf = forklift_tf.ExtractTranslation()
+    driver_cam_pos_gf = foklift_pos_gf + Gf.Vec3d(0.0, 0.0, 1.9)
+    rotate_180_on_y_quat_gf = Gf.Quatf(0, 0, 1, 0)
+    look_at_pallet_quat_gf = lookat_to_quatf(driver_cam_pos_gf, pallet_pos_gf, (0, 0, 1)) * rotate_180_on_y_quat_gf
+    look_at_pallet_xyzw = (look_at_pallet_quat_gf.GetReal(), *look_at_pallet_quat_gf.GetImaginary())
 
-driver_cam_prim = prims.create_prim(
-    prim_path=f"{SCOPE_NAME}/DriverCamera",
-    prim_type="Camera",
-    position=driver_cam_pos_gf,
-    orientation=look_at_pallet_xyzw,
-    attributes={"focusDistance": 400, "focalLength": 24, "clippingRange": (0.1, 10000000)},
-)
+    driver_cam_prim = prims.create_prim(
+        prim_path=f"{SCOPE_NAME}/DriverCamera",
+        prim_type="Camera",
+        position=driver_cam_pos_gf,
+        orientation=look_at_pallet_xyzw,
+        attributes={"focusDistance": 400, "focalLength": 24, "clippingRange": (0.1, 10000000)},
+    )
 
-# Camera looking at the pallet
-pallet_cam = rep.create.camera()
+    # Camera looking at the pallet
+    pallet_cam = rep.create.camera()
 
-# Camera looking at the forklift from a top view with large min clipping to see the scene through the ceiling
-top_view_cam = rep.create.camera(clipping_range=(6.0, 1000000.0))
+    # Camera looking at the forklift from a top view with large min clipping to see the scene through the ceiling
+    top_view_cam = rep.create.camera(clipping_range=(6.0, 1000000.0))
 
-with rep.trigger.on_frame(num_frames=CONFIG["num_frames"]):
-    rep.randomizer.scatter_boxes()
-    rep.randomizer.place_cones()
-    rep.randomizer.randomize_lights()
-    pos_min = (pallet_pos_gf[0] - 2, pallet_pos_gf[1] - 2, 2)
-    pos_max = (pallet_pos_gf[0] + 2, pallet_pos_gf[1] + 2, 4)
-    with pallet_cam:
-        rep.modify.pose(position=rep.distribution.uniform(pos_min, pos_max), look_at=str(pallet_prim.GetPrimPath()))
-    with top_view_cam:
-        rep.modify.pose(
-            position=rep.distribution.uniform(
-                (foklift_pos_gf[0], foklift_pos_gf[1], 9), (foklift_pos_gf[0], foklift_pos_gf[1], 11)
-            ),
-            rotation=rep.distribution.uniform((0, -90, 0), (0, -90, 180)),
-        )
+    with rep.trigger.on_frame(num_frames=CONFIG["num_frames"]):
+        rep.randomizer.scatter_boxes()
+        rep.randomizer.place_cones()
+        rep.randomizer.randomize_lights()
+        pos_min = (pallet_pos_gf[0] - 2, pallet_pos_gf[1] - 2, 2)
+        pos_max = (pallet_pos_gf[0] + 2, pallet_pos_gf[1] + 2, 4)
+        with pallet_cam:
+            rep.modify.pose(position=rep.distribution.uniform(pos_min, pos_max), look_at=str(pallet_prim.GetPrimPath()))
+        with top_view_cam:
+            rep.modify.pose(
+                position=rep.distribution.uniform(
+                    (foklift_pos_gf[0], foklift_pos_gf[1], 9), (foklift_pos_gf[0], foklift_pos_gf[1], 11)
+                ),
+                rotation=rep.distribution.uniform((0, -90, 0), (0, -90, 180)),
+            )
 
-# Initialize and attach writer
-writer = rep.WriterRegistry.get("BasicWriter")
-output_directory = os.getcwd() + "/_output_headless"
-print("Outputting data to ", output_directory)
-writer.initialize(
-    output_dir=output_directory,
-    rgb=True,
-    bounding_box_2d_tight=True,
-    bounding_box_2d_loose=True,
-    semantic_segmentation=True,
-    instance_segmentation=True,
-    distance_to_camera=True,
-    distance_to_image_plane=True,
-    bounding_box_3d=True,
-    occlusion=True,
-    normals=True,
-    motion_vectors=True,
-)
+    # Initialize and attach writer
+    writer = rep.WriterRegistry.get("BasicWriter")
+    output_directory = os.getcwd() + "/_output_headless"
+    print("Outputting data to ", output_directory)
+    writer.initialize(
+        output_dir=output_directory,
+        rgb=True,
+        bounding_box_2d_tight=True,
+        semantic_segmentation=True,
+        instance_segmentation=True,
+        distance_to_image_plane=True,
+        bounding_box_3d=True,
+        occlusion=True,
+        normals=True,
+    )
 
-RESOLUTION = (CONFIG["width"], CONFIG["height"])
-driver_rp = rep.create.render_product(str(driver_cam_prim.GetPrimPath()), RESOLUTION)
-pallet_rp = rep.create.render_product(pallet_cam, RESOLUTION)
-forklift_rp = rep.create.render_product(top_view_cam, RESOLUTION)
-writer.attach([driver_rp, forklift_rp, pallet_rp])
+    RESOLUTION = (CONFIG["width"], CONFIG["height"])
+    driver_rp = rep.create.render_product(str(driver_cam_prim.GetPrimPath()), RESOLUTION)
+    pallet_rp = rep.create.render_product(pallet_cam, RESOLUTION)
+    forklift_rp = rep.create.render_product(top_view_cam, RESOLUTION)
+    writer.attach([driver_rp, forklift_rp, pallet_rp])
 
-run_orchestrator()
+    run_orchestrator()
+    simulation_app.update()
 
-kit.update()
-kit.close()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        carb.log_error(f"Exception: {e}")
+        import traceback
+
+        traceback.print_exc()
+    finally:
+        simulation_app.close()
