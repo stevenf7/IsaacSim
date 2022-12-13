793861f8213ce60ea4cfeec8fc08b262cbc02f5b
Nathan Ratliff <nratliff@nvidia.com>
2022-12-12 21:38:32 -0800

    Simple variant on the existing sync demo to support the tutorial on the physical Franka controller.

--- source/standalone_examples/api/omni.isaac.cortex/example_cortex_sync_belief_main.py
+++ source/standalone_examples/api/omni.isaac.cortex/example_cortex_sync_belief_main.py
@@ -10,12 +10,12 @@ from omni.isaac.kit import SimulationApp
 
 import argparse
 
-parser = argparse.ArgumentParser("example_cortex_sync")
+parser = argparse.ArgumentParser("example_cortex_sync_belief")
 parser.add_argument(
     "--behavior",
     type=str,
-    default="behaviors/franka/block_stacking_behavior.py",
-    help="Which behavior to run. See behavior/franka for available behavior files.",
+    default=None,
+    help="Which behavior to run. See behavior/franka for available behavior files. By default, it launches no behavior.",
 )
 parser.add_argument(
     "--auto_sync_objects", action="store_true", help="Automatically sync the objects with their measured poses."
@@ -30,21 +30,13 @@ import omni
 
 from omni.isaac.core.utils.extensions import enable_extension
 from omni.isaac.core.objects import DynamicCuboid
-from omni.isaac.core.prims import XFormPrim
-
 from omni.isaac.cortex.cortex_object import CortexObject
 from omni.isaac.cortex.cortex_world import CortexWorld
 from omni.isaac.cortex.robot import add_franka_to_stage
 from omni.isaac.cortex.cortex_utils import load_behavior_module
 
 enable_extension("omni.isaac.cortex_sync")
-from omni.isaac.cortex_sync.cortex_ros import (
-    cortex_init_ros_node,
-    CortexControlRos,
-    CortexSimRobotRos,
-    CortexObjectsRos,
-    CortexSimObjectsRos,
-)
+from omni.isaac.cortex_sync.cortex_ros import cortex_init_ros_node, CortexControlRos, CortexObjectsRos
 
 
 class CubeSpec:
@@ -54,16 +46,10 @@ class CubeSpec:
 
 
 def main():
-    cortex_init_ros_node("example_cortex_sync")
+    cortex_init_ros_node("example_cortex_sync_belief")
 
     world = CortexWorld()
     robot = world.add_robot(add_franka_to_stage(name="franka", prim_path="/World/Franka"))
-
-    sim_prim = XFormPrim(prim_path="/Sim")
-    sim_prim.set_world_pose(position=np.array([-2.0, 0.0, 0.0]))
-    sim_robot = world.add_robot(
-        add_franka_to_stage(name="franka_sim", prim_path="/Sim/Franka", use_motion_commander=False)
-    )
 
     obs_specs = [
         CubeSpec("RedCube", [0.7, 0.0, 0.0]),
@@ -73,7 +59,6 @@ def main():
     ]
     width = 0.0515
     cortex_objects = {}
-    sim_objects = {}
     for i, (x, spec) in enumerate(zip(np.linspace(0.3, 0.7, len(obs_specs)), obs_specs)):
         obj = world.scene.add(
             DynamicCuboid(
@@ -87,25 +72,14 @@ def main():
         cortex_objects[spec.name] = CortexObject(obj)
         robot.register_obstacle(cortex_objects[spec.name])
 
-        sim_obj = world.scene.add(
-            DynamicCuboid(
-                prim_path="/Sim/Obs/{}".format(spec.name),
-                name="{}_sim".format(spec.name),
-                size=width,
-                color=spec.color,
-                translation=np.array([x, -0.4, width / 2]),
-            )
-        )
-        sim_objects[spec.name] = sim_obj
     world.scene.add_default_ground_plane()
 
-    cortex_sim = CortexSimRobotRos(sim_robot)
-    cortex_sim_objects_ros = CortexSimObjectsRos(sim_objects)
     cortex_control = CortexControlRos(robot)
     cortex_objects_ros = CortexObjectsRos(cortex_objects, auto_sync_objects=args.auto_sync_objects)
 
-    decider_network = load_behavior_module(args.behavior).make_decider_network(robot)
-    world.add_decider_network(decider_network)
+    if args.behavior is not None:
+        decider_network = load_behavior_module(args.behavior).make_decider_network(robot)
+        world.add_decider_network(decider_network)
 
     world.run(simulation_app)
     simulation_app.close()
--- source/standalone_examples/api/omni.isaac.cortex/example_cortex_sync_sim_main.py
+++ source/standalone_examples/api/omni.isaac.cortex/example_cortex_sync_sim_main.py
@@ -10,16 +10,7 @@ from omni.isaac.kit import SimulationApp
 
 import argparse
 
-parser = argparse.ArgumentParser("example_cortex_sync")
-parser.add_argument(
-    "--behavior",
-    type=str,
-    default="behaviors/franka/block_stacking_behavior.py",
-    help="Which behavior to run. See behavior/franka for available behavior files.",
-)
-parser.add_argument(
-    "--auto_sync_objects", action="store_true", help="Automatically sync the objects with their measured poses."
-)
+parser = argparse.ArgumentParser("example_cortex_sync_sim")
 args, _ = parser.parse_known_args()
 
 simulation_app = SimulationApp({"headless": False})
@@ -30,21 +21,13 @@ import omni
 
 from omni.isaac.core.utils.extensions import enable_extension
 from omni.isaac.core.objects import DynamicCuboid
-from omni.isaac.core.prims import XFormPrim
 
-from omni.isaac.cortex.cortex_object import CortexObject
 from omni.isaac.cortex.cortex_world import CortexWorld
 from omni.isaac.cortex.robot import add_franka_to_stage
 from omni.isaac.cortex.cortex_utils import load_behavior_module
 
 enable_extension("omni.isaac.cortex_sync")
-from omni.isaac.cortex_sync.cortex_ros import (
-    cortex_init_ros_node,
-    CortexControlRos,
-    CortexSimRobotRos,
-    CortexObjectsRos,
-    CortexSimObjectsRos,
-)
+from omni.isaac.cortex_sync.cortex_ros import cortex_init_ros_node, CortexSimRobotRos, CortexSimObjectsRos
 
 
 class CubeSpec:
@@ -54,13 +37,10 @@ class CubeSpec:
 
 
 def main():
-    cortex_init_ros_node("example_cortex_sync")
+    cortex_init_ros_node("example_cortex_sync_sim")
 
     world = CortexWorld()
-    robot = world.add_robot(add_franka_to_stage(name="franka", prim_path="/World/Franka"))
 
-    sim_prim = XFormPrim(prim_path="/Sim")
-    sim_prim.set_world_pose(position=np.array([-2.0, 0.0, 0.0]))
     sim_robot = world.add_robot(
         add_franka_to_stage(name="franka_sim", prim_path="/Sim/Franka", use_motion_commander=False)
     )
@@ -72,21 +52,8 @@ def main():
         CubeSpec("GreenCube", [0.0, 0.7, 0.0]),
     ]
     width = 0.0515
-    cortex_objects = {}
     sim_objects = {}
     for i, (x, spec) in enumerate(zip(np.linspace(0.3, 0.7, len(obs_specs)), obs_specs)):
-        obj = world.scene.add(
-            DynamicCuboid(
-                prim_path="/World/Obs/{}".format(spec.name),
-                name=spec.name,
-                size=width,
-                color=spec.color,
-                translation=np.array([x, -0.4, width / 2]),
-            )
-        )
-        cortex_objects[spec.name] = CortexObject(obj)
-        robot.register_obstacle(cortex_objects[spec.name])
-
         sim_obj = world.scene.add(
             DynamicCuboid(
                 prim_path="/Sim/Obs/{}".format(spec.name),
@@ -101,11 +68,6 @@ def main():
 
     cortex_sim = CortexSimRobotRos(sim_robot)
     cortex_sim_objects_ros = CortexSimObjectsRos(sim_objects)
-    cortex_control = CortexControlRos(robot)
-    cortex_objects_ros = CortexObjectsRos(cortex_objects, auto_sync_objects=args.auto_sync_objects)
-
-    decider_network = load_behavior_module(args.behavior).make_decider_network(robot)
-    world.add_decider_network(decider_network)
 
     world.run(simulation_app)
     simulation_app.close()
