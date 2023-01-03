ae3bd1b0d2611956051c1320ba58777a5f03005a
Hammad Mazhar <hmazhar@nvidia.com>
2023-01-03 14:12:11 -0800

    Add omni.anim.people startup test

--- source/extensions/omni.isaac.tests/omni/isaac/tests/tests/test_external.py
+++ source/extensions/omni.isaac.tests/omni/isaac/tests/tests/test_external.py
@@ -17,19 +17,19 @@ class TestExternalDependencies(omni.kit.test.AsyncTestCase):
     async def setUp(self):
         await omni.usd.get_context().new_stage_async()
         await omni.kit.app.get_app().next_update_async()
-        ext_manager = omni.kit.app.get_app().get_extension_manager()
-        ext_id = ext_manager.get_enabled_extension_id("omni.isaac.tests")
-        self._extension_path = ext_manager.get_extension_path(ext_id)
+        self.ext_manager = omni.kit.app.get_app().get_extension_manager()
+        ext_id = self.ext_manager.get_enabled_extension_id("omni.isaac.tests")
+        self._extension_path = self.ext_manager.get_extension_path(ext_id)
 
     async def tearDown(self):
         await omni.kit.app.get_app().next_update_async()
         pass
 
     async def test_semantic_schema_editor(self):
-        ext_manager = omni.kit.app.get_app().get_extension_manager()
-        self.assertTrue(ext_manager.set_extension_enabled_immediate("semantics.schema.editor", True))
-        await omni.kit.app.get_app().next_update_async()
-        self.assertTrue(ext_manager.set_extension_enabled_immediate("semantics.schema.editor", False))
+        self.assertTrue(self.ext_manager.set_extension_enabled_immediate("semantics.schema.editor", True))
+        import semantics.schema.editor
+
+        self.assertTrue(self.ext_manager.set_extension_enabled_immediate("semantics.schema.editor", False))
 
     async def test_asset_converter(self):
         import omni.kit.asset_converter
@@ -61,7 +61,13 @@ class TestExternalDependencies(omni.kit.test.AsyncTestCase):
         self.assertTrue(os.path.isfile(output_usd))
 
     async def test_cuopt_example(self):
-        ext_manager = omni.kit.app.get_app().get_extension_manager()
-        self.assertTrue(ext_manager.set_extension_enabled_immediate("omni.cuopt.examples", True))
-        await omni.kit.app.get_app().next_update_async()
-        self.assertTrue(ext_manager.set_extension_enabled_immediate("omni.cuopt.examples", False))
+        self.assertTrue(self.ext_manager.set_extension_enabled_immediate("omni.cuopt.examples", True))
+        import omni.cuopt.examples
+
+        self.assertTrue(self.ext_manager.set_extension_enabled_immediate("omni.cuopt.examples", False))
+
+    async def test_people(self):
+        self.assertTrue(self.ext_manager.set_extension_enabled_immediate("omni.anim.people", True))
+        import omni.anim.people
+
+        self.assertTrue(self.ext_manager.set_extension_enabled_immediate("omni.anim.people", False))
