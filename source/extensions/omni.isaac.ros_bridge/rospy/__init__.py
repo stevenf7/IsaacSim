ca8563b2cf53b9de1f5502b5172d3b9858fe19f8
Hammad Mazhar <hmazhar@nvidia.com>
2023-01-19 10:50:58 -0800

    fixes for unit tests

--- source/extensions/omni.isaac.ros_bridge/rospy/__init__.py
+++ source/extensions/omni.isaac.ros_bridge/rospy/__init__.py
@@ -15,7 +15,11 @@ class Extension(omni.ext.IExt):
     def on_startup(self, ext_id: str):
         ext_manager = omni.kit.app.get_app().get_extension_manager()
         self._ros_extension_path = ext_manager.get_extension_path(ext_id)
-        sys.path.append(os.path.join(os.path.dirname(self._ros_extension_path + "/noetic/lib/python3/dist-packages/")))
+        sys.path.append(
+            os.path.join(os.path.dirname(self._ros_extension_path + "/noetic/lib/python3.7/site-packages/"))
+        )
 
     def on_shutdown(self):
-        sys.path.remove(os.path.join(os.path.dirname(self._ros_extension_path + "/noetic/lib/python3/dist-packages/")))
+        sys.path.remove(
+            os.path.join(os.path.dirname(self._ros_extension_path + "/noetic/lib/python3.7/site-packages/"))
+        )
