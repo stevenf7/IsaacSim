72a1a148e0782bd708e072bd067e7f8fdf44cb07
Ossama Samir Ahmed <oahmed@nvidia.com>
2023-02-15 18:41:40 -0800

    fixed hello_world api test

--- premake5.lua
+++ premake5.lua
@@ -460,6 +460,7 @@ group "python_samples"
     python_sample_test("tests-nativepython-replicator.composer.flying_things_4d", "tools/composer/src/main.py", "--input parameters/flying_things_4d.yaml --num-scenes 1 --headless --overwrite --nucleus-server "..nucleus_server)
     
     -- tests that are not shipped
+    python_sample_test("tests-internalnativepython-omni.isaac.core.hello_world", "standalone_examples/testing/omni.isaac.core/hello_world.py", "--test")
     python_sample_test("tests-internalnativepython-omni.isaac.core.test_time_stepping", "standalone_examples/testing/omni.isaac.core/test_time_stepping.py")
     python_sample_test("tests-internalnativepython-omni.isaac.core.test_save_stage", "standalone_examples/testing/omni.isaac.core/test_save_stage.py")
     python_sample_test("tests-internalnativepython-omni.isaac.core.test_delete_in_contact", "standalone_examples/testing/omni.isaac.core/test_delete_in_contact.py")
