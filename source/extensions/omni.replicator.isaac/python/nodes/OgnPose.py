Fix issue with incorrect rotation matrice on certain usd assets

source/extensions/omni.replicator.isaac/python/nodes/OgnPose.py
@@ -11,7 +11,7 @@ import json
 import numpy as np
 import omni.graph.core as og
 from omni.isaac.core.utils.rotations import euler_angles_to_quat
-from omni.isaac.core.utils.transformations import tf_matrix_from_pose
+from omni.isaac.core.utils.transformations import tf_matrix_from_pose, get_transform_with_normalized_rotation
 from omni.replicator.isaac.scripts.utils import get_image_space_points
 
 
@@ -53,7 +53,8 @@ class OgnPose:
         semantic_token_map = db.inputs.sdIMSemanticTokenMap
 
         prims_to_world_row_major = db.inputs.sdIMSemanticWorldTransform.view(np.float32).reshape((num_semantics, 4, 4))
-        prims_to_world_row_major[:, :-1, :-1] = prims_to_world_row_major[:, :-1, :-1] * 100.0
+        for i in range(num_semantics):
+            prims_to_world_row_major[i] = get_transform_with_normalized_rotation(prims_to_world_row_major[i])
 
         coord_types = [("x_min", "<i4"), ("y_min", "<i4"), ("x_max", "<i4"), ("y_max", "<i4")]
         desired_data_dtype = np.dtype(coord_types)

source/extensions/omni.replicator.isaac/python/scripts/writers/dope_writer.py
@@ -143,7 +143,7 @@ class DOPEWriter(Writer):
                 "class": class_name,
                 "visibility": object["visibility"].astype(np.float),
                 "location": object["location"].astype(np.float),
-                "quaternion_xyzw": object["rotation"].astype(np.float),
+                "quaternion_wxyz": object["rotation"].astype(np.float),
                 "projected_cuboid": object["projected_cuboid"].astype(np.float),
             }
 