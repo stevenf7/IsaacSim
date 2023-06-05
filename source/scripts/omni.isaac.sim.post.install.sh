61bb035813ab666337fab1ba49975c3eea18bdfe
Sheikh Dawood Abdul Ajees <sabdulajees@nvidia.com>
2023-06-05 13:29:42 -0700

    OM-91391 add warmup tests and remove api docs Windows package

--- source/scripts/omni.isaac.sim.post.install.sh
+++ source/scripts/omni.isaac.sim.post.install.sh
@@ -17,17 +17,18 @@ echo "Installing Python packages..."
 ${SCRIPT_DIR}/python.sh -m pip install -r ${SCRIPT_DIR}/requirements.txt
 echo "Installing Icon"
 ${SCRIPT_DIR}/python.sh ${SCRIPT_DIR}/icon/install_icon.py
-# # Warm up cache
-# # Run command below to skip warm up
-# #  echo "export ISAACSIM_SKIP_WARMUP=Y" >> ~/.profile
-# if [[ -z "${ISAACSIM_SKIP_WARMUP}" ]]; then
-#     set +e # Workaround post-install script failure
-#     echo "Warming up cache for python app..."
-#     ${SCRIPT_DIR}/python.sh ${SCRIPT_DIR}/standalone_examples/api/omni.isaac.kit/hello_world.py
 
-#     echo "Warming up cache for main app..."
-#     ${SCRIPT_DIR}/omni.isaac.sim.warmup.sh
-#     set -e
-# fi
+# Warm up cache
+# Run command below to skip warm up
+#  echo "export ISAACSIM_SKIP_WARMUP=Y" >> ~/.profile
+if [[ -z "${ISAACSIM_SKIP_WARMUP}" ]]; then
+    set +e # Workaround post-install script failure
+    echo "Warming up cache for python app..."
+    ${SCRIPT_DIR}/python.sh ${SCRIPT_DIR}/standalone_examples/api/omni.isaac.kit/hello_world.py
+
+    echo "Warming up cache for main app..."
+    ${SCRIPT_DIR}/omni.isaac.sim.warmup.sh
+    set -e
+fi
 
 echo "Isaac Sim post installation script completed!"
