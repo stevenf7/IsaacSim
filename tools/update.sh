df7846b581a27f66d255cd83dfab1dff1bd6946b
Hammad Mazhar <hmazhar@nvidia.com>
2023-08-02 10:53:17 -0700

    update to latest kit-sdk, try and fix startup errors

--- tools/update.sh
+++ tools/update.sh
@@ -8,7 +8,7 @@ cd "$SCRIPT_DIR"
 
 
 ../repo.sh update kit-sdk
-../repo.sh update omni_physics --include-pre-release
+../repo.sh update omni_physics --include-pre-release --patch
 ../repo.sh update_extscache
 # ../repo.sh update repo_
 # ./packman/packman update -y
