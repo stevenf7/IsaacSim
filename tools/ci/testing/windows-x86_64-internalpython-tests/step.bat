fa2687695e082ab7adface6dbc0b401c176d8cba
sabdulajees <sabdulajees@nvidia.com>
2023-08-02 09:39:37 -0500

    Fix TC tests not passing in bucket number

--- tools/ci/testing/windows-x86_64-internalpython-tests/step.bat
+++ tools/ci/testing/windows-x86_64-internalpython-tests/step.bat
@@ -1,6 +1,6 @@
 @echo off
 
 :: tests
-call "%~dp0..\..\..\..\tools\test.bat" --suite internalpythontests --from-package --config release
+call "%~dp0..\..\..\..\tools\test.bat" --suite internalpythontests --from-package --config release %*
 if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
