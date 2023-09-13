27269306d2cfde196c23cbae67be368b34461037
Ossama Ahmed <oahmed@nvidia.com>
2023-09-13 14:26:09 -0400

    [OM-106395] PhysX errors in bin filling example

--- source/extensions/omni.isaac.core/omni/isaac/core/controllers/articulation_controller.py
+++ source/extensions/omni.isaac.core/omni/isaac/core/controllers/articulation_controller.py
@@ -59,7 +59,7 @@ class ArticulationController(object):
             joint_positions = self._articulation_view._backend_utils.expand_dims(joint_positions, 0)
             if control_actions.joint_indices is None:
                 for i in range(control_actions.get_length()):
-                    if joint_positions[0][i] is None:
+                    if joint_positions[0][i] is None or np.isnan(joint_positions[0][i]):
                         joint_positions[0][i] = applied_actions.joint_positions[i]
         joint_velocities = control_actions.joint_velocities
         if control_actions.joint_velocities is not None:
@@ -69,7 +69,7 @@ class ArticulationController(object):
             joint_velocities = self._articulation_view._backend_utils.expand_dims(joint_velocities, 0)
             if control_actions.joint_indices is None:
                 for i in range(control_actions.get_length()):
-                    if joint_velocities[0][i] is None:
+                    if joint_velocities[0][i] is None or np.isnan(joint_velocities[0][i]):
                         joint_velocities[0][i] = applied_actions.joint_velocities[i]
         joint_efforts = control_actions.joint_efforts
         if control_actions.joint_efforts is not None:
