252c892b1110c4efd4330878c1cf4b39f6bba3d3
Renato Gasoto <rgasoto@nvidia.com>
2023-09-27 00:04:30 -0700

    OM-86960 - update surface gripper ogn

--- include/omni/isaac/surface_gripper/SurfaceGripper.h
+++ include/omni/isaac/surface_gripper/SurfaceGripper.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2022, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2020-2023, NVIDIA CORPORATION. All rights reserved.
 //
 // NVIDIA CORPORATION and its licensors retain all intellectual property
 // and proprietary rights in and to this software, related documentation
@@ -178,7 +178,7 @@ public:
      * @return true when an object is close and joint is effectively closed
      * @return false when no object is near the gripper. joint is not closed.
      */
-    bool attemptClose()
+    bool attemptClose(float additional_offset = 0)
     {
         DcHandle rb_0 = mDc->getRigidBody(mProps.parentPath.c_str());
         if (!rb_0)
@@ -188,75 +188,92 @@ public:
         }
         DcTransform t_0 = mDc->getRigidBodyPose(rb_0);
         DcTransform _t_0 = (t_0 * mProps.offset);
-        carb::Float3 p = _t_0.p;
         carb::Float3 dir = getBasisVectorX(_t_0.r);
         // CARB_LOG_WARN("gripper position: (%f, %f, %f)", p.x, p.y, p.z);
         // CARB_LOG_WARN("gripper direction: (%f, %f, %f)", dir.x, dir.y, dir.z);
         // DcTransform threshOffset;
         // threshOffset.p.x = mProps.gripThreshold;
         // _t_0 = _t_0 * threshOffset; //Disabling until we get soft meshes for grippers
-        DcRayCastResult hit = mDc->rayCast(p, dir, mProps.gripThreshold);
-
-        if (hit.hit)
+        uint32_t attempts = 1000000;
+        additional_offset = 0.0f;
+        DcRayCastResult hit;
+        while (attempts)
         {
+            attempts--;
+            carb::Float3 p = _t_0.p + dir * additional_offset;
+            hit = mDc->rayCast(p, dir, mProps.gripThreshold);
 
-            CARB_LOG_INFO("Gripping prim %s at distance %f with parent %s", mDc->getRigidBodyPath(hit.rigidBody),
-                          hit.distance, mProps.parentPath.c_str());
-            if (hit.rigidBody == rb_0)
+            if (hit.hit)
             {
-                CARB_LOG_WARN(
-                    "Surface Gripper is inside the parent mesh. please move it outside to be able to use the Gripper");
-                return false;
+
+
+                if (hit.rigidBody == rb_0)
+                {
+                    // CARB_LOG_WARN(
+                    //     "Surface Gripper is inside the parent mesh. please move it outside to be able to use the
+                    //     Gripper (%f, %d)", hit.distance, attempts);
+                    additional_offset += 1.0e-3f;
+                    continue;
+                }
+                CARB_LOG_INFO("Gripping prim %s at distance %f with parent %s", mDc->getRigidBodyPath(hit.rigidBody),
+                              hit.distance, mProps.parentPath.c_str());
+                DcTransform t_1 = inverse(mDc->getRigidBodyPose(hit.rigidBody)) * _t_0;
+
+                mJointProperties.body0 = rb_0;
+                mJointProperties.body1 = hit.rigidBody;
+                mDc->setRigidBodyDisableGravity(mJointProperties.body1, mProps.disableGravity);
+                mJointProperties.pose0 = mProps.offset;
+                mJointProperties.pose1 = t_1;
+                mJointProperties.axes = kDcAxisAll;
+
+
+                mJointProperties.stiffness = mProps.stiffness;
+                mJointProperties.damping = mProps.damping;
+                mJointProperties.limitStiffness = mProps.stiffness;
+                mJointProperties.limitDamping = mProps.damping;
+                if (mProps.bendAngle > 0)
+                {
+                    mJointProperties.softLimit = true;
+                    mJointProperties.lowerLimit = mProps.bendAngle;
+                    mJointProperties.upperLimit = mProps.bendAngle;
+                    mJointProperties.jointType = DcJointType::eSpherical;
+                    mJointProperties.hasLimits[4] = true;
+                    mJointProperties.hasLimits[5] = true;
+                }
+                else
+                {
+                    mJointProperties.hasLimits[4] = false;
+                    mJointProperties.hasLimits[5] = false;
+                    mJointProperties.softLimit = false;
+                    mJointProperties.lowerLimit = 0;
+                    mJointProperties.upperLimit = 0;
+                    mJointProperties.jointType = DcJointType::eFixed;
+                }
+
+
+                mJointProperties.forceLimit = mProps.forceLimit;
+                mJointProperties.torqueLimit = mProps.torqueLimit;
+                if (!mJointHandle)
+                {
+                    std::string s(mProps.d6JointPath);
+                    mJointProperties.name = (char*)(s).c_str();
+                    mJointHandle = mDc->createD6Joint(&mJointProperties);
+                }
+                mDc->setD6JointProperties(mJointHandle, &mJointProperties);
+
+                mIsClosed = true;
             }
-            DcTransform t_1 = inverse(mDc->getRigidBodyPose(hit.rigidBody)) * _t_0;
-
-            mJointProperties.body0 = rb_0;
-            mJointProperties.body1 = hit.rigidBody;
-            mDc->setRigidBodyDisableGravity(mJointProperties.body1, mProps.disableGravity);
-            mJointProperties.pose0 = mProps.offset;
-            mJointProperties.pose1 = t_1;
-            mJointProperties.axes = kDcAxisAll;
-
-
-            mJointProperties.stiffness = mProps.stiffness;
-            mJointProperties.damping = mProps.damping;
-            mJointProperties.limitStiffness = mProps.stiffness;
-            mJointProperties.limitDamping = mProps.damping;
-            if (mProps.bendAngle > 0)
-            {
-                mJointProperties.softLimit = true;
-                mJointProperties.lowerLimit = mProps.bendAngle;
-                mJointProperties.upperLimit = mProps.bendAngle;
-                mJointProperties.jointType = DcJointType::eSpherical;
-                mJointProperties.hasLimits[4] = true;
-                mJointProperties.hasLimits[5] = true;
-            }
-            else
-            {
-                mJointProperties.hasLimits[4] = false;
-                mJointProperties.hasLimits[5] = false;
-                mJointProperties.softLimit = false;
-                mJointProperties.lowerLimit = 0;
-                mJointProperties.upperLimit = 0;
-                mJointProperties.jointType = DcJointType::eFixed;
-            }
-
-
-            mJointProperties.forceLimit = mProps.forceLimit;
-            mJointProperties.torqueLimit = mProps.torqueLimit;
-            if (!mJointHandle)
-            {
-                std::string s(mProps.d6JointPath);
-                mJointProperties.name = (char*)(s).c_str();
-                mJointHandle = mDc->createD6Joint(&mJointProperties);
-            }
-            mDc->setD6JointProperties(mJointHandle, &mJointProperties);
-
-            mIsClosed = true;
+            break;
         }
-        else
+        if (attempts == 0)
         {
             CARB_LOG_INFO("Raycast Failed");
+        }
+        if (additional_offset)
+        {
+            CARB_LOG_WARN(
+                "Surface Gripper is inside the parent Rigid body collider. please move it forward in the X offset direction by %f to avoid wasted computation",
+                additional_offset);
         }
         return hit.hit;
     }
