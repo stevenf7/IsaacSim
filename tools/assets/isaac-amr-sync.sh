2be70419a4d7677076501de01efdc90730b429ac
sabdulajees <sabdulajees@nvidia.com>
2023-09-29 12:12:28 -0500

    Add Isaac_AMR/2.1 for 2022.2.1

--- tools/assets/isaac-amr-sync.sh
+++ tools/assets/isaac-amr-sync.sh
@@ -8,8 +8,8 @@ SCRIPT_DIR="$(dirname "$(realpath $0)")"/../../_assets
 
 echo Removing $SCRIPT_DIR/Isaac_AMR...
 rm -rf $SCRIPT_DIR/Isaac_AMR
-echo run $OMNICLI copy omniverse://isaac-dev.ov.nvidia.com/Isaac/Samples/Isaac_AMR/2.1 $SCRIPT_DIR/Isaac_AMR
-$OMNICLI copy omniverse://isaac-dev.ov.nvidia.com/Isaac/Samples/Isaac_AMR/2.1 $SCRIPT_DIR/Isaac_AMR
+echo run $OMNICLI copy omniverse://isaac-dev.ov.nvidia.com/Isaac/Samples/Isaac_AMR/2.1 $SCRIPT_DIR/Isaac_AMR/2.1
+$OMNICLI copy omniverse://isaac-dev.ov.nvidia.com/Isaac/Samples/Isaac_AMR/2.1 $SCRIPT_DIR/Isaac_AMR/2.1
 
 echo run aws s3 sync $SCRIPT_DIR/Isaac_AMR s3://omniverse-content-staging/Assets/Isaac/2022.2.1/Isaac/Samples/Isaac_AMR --delete
 aws s3 sync $SCRIPT_DIR/Isaac_AMR s3://omniverse-content-staging/Assets/Isaac/2022.2.1/Isaac/Samples/Isaac_AMR --delete
