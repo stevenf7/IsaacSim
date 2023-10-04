d5f9d14819c1b98a75743a8a428daf898661fe3f
Ossama Ahmed <oahmed@nvidia.com>
2023-10-04 15:14:31 -0400

    [OM-86604] The python script exits without showing the content/scene

--- source/standalone_examples/api/omni.isaac.core/simulate_robot.py
+++ source/standalone_examples/api/omni.isaac.core/simulate_robot.py
@@ -14,11 +14,13 @@ simulation_app = SimulationApp({"headless": False})
 from omni.isaac.core import SimulationContext
 from omni.isaac.core.utils.nucleus import get_assets_root_path
 from omni.isaac.core.utils.stage import add_reference_to_stage, is_stage_loading
+from omni.isaac.core.utils.prims import create_prim
 
 assets_root_path = get_assets_root_path()
 asset_path = assets_root_path + "/Isaac/Robots/Franka/franka_alt_fingers.usd"
 simulation_context = SimulationContext()
 add_reference_to_stage(asset_path, "/Franka")
+create_prim("/DistantLight", "DistantLight")
 # wait for things to load
 simulation_app.update()
 while is_stage_loading():
