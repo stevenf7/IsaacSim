Fix invalid Rotation Matrix in UR10 bin stacking behavior waypoints

source/extensions/omni.isaac.cortex.sample_behaviors/omni/isaac/cortex/sample_behaviors/ur10/bin_stacking_behavior.py
@@ -543,9 +543,9 @@ class ReleaseFlipStationBin(DfState):
         self.ax = ax
         self.v = v
 
-        target_ax = normalized(np.array([0.0, -0.0, 1.0]))
+        target_ax = normalized(np.array([1.0, -0.0, 0.0]))
         target_ay = normalized(np.array([0.0, -1.0, 0.0]))
-        target_az = np.cross(target_ay, target_ax)
+        target_az = np.cross(target_ax, target_ay)
         target_R = math_util.pack_R(target_ax, target_ay, target_az)
 
         # This target pose is a little below the bin, but off to the side with the end