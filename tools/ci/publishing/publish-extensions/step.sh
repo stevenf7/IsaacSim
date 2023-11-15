65596e56b8b604c884c5884a97e4d2ecde3bf31e
Hammad Mazhar <hmazhar@nvidia.com>
2023-11-14 18:44:36 -0800

    OM-106898 Add scripts to publish extensions

---
+++ tools/ci/publishing/publish-extensions/step.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+SCRIPT_DIR=$(dirname ${BASH_SOURCE})
+source "$SCRIPT_DIR/../../../../repo.sh" build -x $@ || exit $?
+source "$SCRIPT_DIR/../../../../repo.sh" publish_exts -c release $@ || exit $?
+source "$SCRIPT_DIR/../../../../repo.sh" publish_exts -c debug $@ || exit $?
+
+# keeping manual command below for reference
+# ../build.sh
+# ../_build/linux-x86_64/release/isaac-sim.sh --ext-folder exts --ext-folder apps --publish
+
