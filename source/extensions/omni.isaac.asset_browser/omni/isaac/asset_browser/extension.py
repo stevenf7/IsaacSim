a3576779fa0cc38a4c37475edf644e4d7ed91c2e
Sheikh Dawood Abdul Ajees <sabdulajees@nvidia.com>
2023-11-14 19:18:52 -0800

    OM-113211 Update to omni.kit.browser.asset-1.3.7

--- source/extensions/omni.isaac.asset_browser/omni/isaac/asset_browser/extension.py
+++ source/extensions/omni.isaac.asset_browser/omni/isaac/asset_browser/extension.py
@@ -1,38 +1,61 @@
-# Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
 # and proprietary rights in and to this software, related documentation
-# and any modifications thereto. Any use, reproduction, disclosure or
+# and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
-#
+
+from typing import Optional
+
 import carb.settings
 import omni.ext
-import omni.kit.ui
+import omni.kit.menu.utils
+import omni.ui as ui
+from omni.kit.browser.folder.core import TreeFolderBrowserWidget
 
 from .window import AssetBrowserWindow
 
-ASSET_BROWSER_MENU_PATH = "Window/Browsers/Isaac Assets (Beta)"
 _extension_instance = None
+BROWSER_MENU_ROOT = "Window"
 SETTING_ROOT = "/exts/omni.isaac.asset_browser/"
 SETTING_VISIBLE_AFTER_STARTUP = SETTING_ROOT + "visible_after_startup"
 
 
 class AssetBrowserExtension(omni.ext.IExt):
+    @property
+    def window(self) -> Optional[AssetBrowserWindow]:
+        return self._window
+
+    @property
+    def browser_widget(self) -> Optional[TreeFolderBrowserWidget]:
+        return self._window._widget
+
     def on_startup(self, ext_id):
-        visible = carb.settings.get_settings().get_as_bool(SETTING_VISIBLE_AFTER_STARTUP)
-
-        self._menu = omni.kit.ui.get_editor_menu().add_item(
-            ASSET_BROWSER_MENU_PATH, self._on_click, toggle=True, value=visible
+        self._window = None
+        ui.Workspace.set_show_window_fn(
+            AssetBrowserWindow.WINDOW_TITLE,
+            self._show_window,  # pylint: disable=unnecessary-lambda
         )
+        self._register_menuitem()
 
-        self._window = AssetBrowserWindow(visible)
-        self._window.set_visibility_changed_fn(self._on_visibility_changed)
+        visible = carb.settings.get_settings().get_as_bool(SETTING_VISIBLE_AFTER_STARTUP)
+        if visible:
+            self._show_window(True)
+        else:
+            # Warmup model
+            warmup = carb.settings.get_settings().get("/app/warmupMode") or False
+            if warmup:
+                from .model import AssetBrowserModel
+
+                self.__model = AssetBrowserModel(run_warmup=True)
 
         global _extension_instance
         _extension_instance = self
 
     def on_shutdown(self):
+        omni.kit.menu.utils.remove_menu_items(self._menu_entry, name=BROWSER_MENU_ROOT)
+
         if self._window is not None:
             self._window.destroy()
             self._window = None
@@ -40,11 +63,37 @@ class AssetBrowserExtension(omni.ext.IExt):
         global _extension_instance
         _extension_instance = None
 
-    def _on_click(self, *args):
-        self._window.visible = not self._window.visible
+    def _show_window(self, visible) -> None:
+        if visible:
+            if self._window is None:
+                self._window = AssetBrowserWindow(visible=True)
+                self._window.set_visibility_changed_fn(self._on_visibility_changed)
+            else:
+                self._window.visible = True
+        else:
+            self._window.visible = False
+
+    def _toggle_window(self):
+        self._show_window(not self._is_visible())
+
+    def _register_menuitem(self):
+        self._menu_entry = [
+            omni.kit.menu.utils.MenuItemDescription(
+                name="Browsers",
+                sub_menu=[
+                    omni.kit.menu.utils.MenuItemDescription(
+                        name="Isaac", ticked=True, ticked_fn=self._is_visible, onclick_fn=self._toggle_window
+                    )
+                ],
+            )
+        ]
+        omni.kit.menu.utils.add_menu_items(self._menu_entry, BROWSER_MENU_ROOT)
+
+    def _is_visible(self):
+        return self._window.visible if self._window else False
 
     def _on_visibility_changed(self, visible):
-        omni.kit.ui.get_editor_menu().set_value(ASSET_BROWSER_MENU_PATH, visible)
+        omni.kit.menu.utils.refresh_menu_items(BROWSER_MENU_ROOT)
 
 
 def get_instance():
