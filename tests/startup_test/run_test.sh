d2d699e600803003e6d08c8f5d6fe39f0e1859bb
Hammad Mazhar <hmazhar@nvidia.com>
2024-02-09 14:42:53 -0800

    Remove unused tests folder and fix pip.cloud version in changelog

--- tests/startup_test/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-THIS_SCRIPT_LOCATION=$(dirname $(readlink -m $BASH_SOURCE))
-TOOLS=$(dirname $(readlink -m $BASH_SOURCE))/tools
-PACKMAN=${TOOLS}/packman
-
-cd $THIS_SCRIPT_LOCATION
-exec ${PACKMAN}/python.sh $TOOLS/ovat/run_test.py $*
