69b63141bfba6c099af73b96e6c7b7dc15c46b7c
Steven Feng <stevfeng@nvidia.com>
2024-02-16 16:32:33 -0800

    OM-118917 Menu Test sensor and environment

--- source/extensions/omni.isaac.menu/docs/CHANGELOG.md
+++ source/extensions/omni.isaac.menu/docs/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.2.0] - 2024-01-30
+### Changed
+- Changed sensor menu tests to verify sensor functionality through the sensor interfaces
+- Added golden image test for the environment
+
 ## [0.1.0] - 2024-01-19
 ### Added
 - Initial version of extension, moving menu code out of omni.isaac.utils extension
--- source/extensions/omni.isaac.menu/omni/isaac/menu/tests/test_menu.py
+++ source/extensions/omni.isaac.menu/omni/isaac/menu/tests/test_menu.py
@@ -7,7 +7,10 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 #
 
+from pathlib import Path
+
 import carb
+import carb.settings
 import carb.tokens
 import omni.kit.commands
 
@@ -17,16 +20,26 @@ import omni.kit.commands
 import omni.kit.test
 import omni.kit.ui_test as ui_test
 import omni.ui as ui
+import omni.usd
+from omni.isaac.core.objects import DynamicCuboid
 from omni.isaac.core.utils.prims import get_prim_path
 from omni.isaac.core.utils.stage import clear_stage, create_new_stage, traverse_stage
+from omni.isaac.core.utils.viewports import set_camera_view
+from omni.isaac.range_sensor import _range_sensor
+from omni.isaac.sensor import _sensor
 from omni.kit.mainwindow import get_main_window
 from omni.kit.ui_test.menu import *
 from omni.kit.ui_test.query import *
-from pxr import UsdPhysics
+from omni.kit.viewport.utility import get_active_viewport, get_active_viewport_window
+from omni.ui.tests.test_base import OmniUiTest
+from pxr import UsdGeom, UsdPhysics
 
+EXTENSION_FOLDER_PATH = Path(omni.kit.app.get_app().get_extension_manager().get_extension_path_by_module(__name__))
+TEST_DATA_PATH = EXTENSION_FOLDER_PATH.joinpath("data/tests")
 
 # Having a test class dervived from omni.kit.test.AsyncTestCase declared on the root of module will make it auto-discoverable by omni.kit.test
-class TestMenuAssets(omni.kit.test.AsyncTestCase):
+# OmniUiTest is derived from omni.kit.test.AsyncTestCase
+class TestMenuAssets(OmniUiTest):
     # Before running each test
     async def setUp(self):
         self._timeline = omni.timeline.get_timeline_interface()
@@ -34,21 +47,35 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
         # Make sure the stage loaded
         self.assertTrue(result)
         await omni.kit.app.get_app().next_update_async()
+        self._golden_img_dir = TEST_DATA_PATH.absolute().joinpath("golden_img").absolute()
 
+        self.usd_selection: omni.usd.Selection = omni.usd.get_context().get_selection()
         # list all the Isaac Sim's Robot assets' menu path
         window = get_main_window()
         self.menu_dict = await get_context_menu(window._ui_main_window.main_menu_bar, get_all=False)
-
+        self.lidar_sensor_interface = _range_sensor.acquire_lidar_sensor_interface()
+        self.ultrasonic_sensor_interface = _range_sensor.acquire_ultrasonic_sensor_interface()
+        self.generic_sensor_interface = _range_sensor.acquire_generic_sensor_interface()
+        self.contact_sensor_interface = _sensor.acquire_contact_sensor_interface()
+        self.imu_sensor_interface = _sensor.acquire_imu_sensor_interface()
+        self.carb_settings = carb.settings.get_settings()
+        self.carb_settings.set("/persistent/app/viewport/displayOptions", 0)
+        self.carb_settings.set("/rtx/rendermode", "RayTracedLighting")
+        self.carb_settings.set("/rtx-transient/resourcemanager/enableTextureStreaming", False)
+        self.viewport_window = get_active_viewport_window()
+        self.viewport_window.visible = False
         pass
 
     # After running each test
     async def tearDown(self):
-        # self.my_world.stop()
-        # self.my_world.clear_instance()
+        # need to move the window out of the way so it doesn't block the menu bar for the other tests
+        self.viewport_window.visible = True
         await omni.kit.app.get_app().next_update_async()
         # In some cases the test will end before the asset is loaded, in this case wait for assets to load
         while omni.usd.get_context().get_stage_loading_status()[2] > 0:
             await omni.kit.app.get_app().next_update_async()
+        await super().tearDown()
+        self.carb_settings.set("/persistent/app/viewport/displayOptions", 1)
         pass
 
     # Actual test, notice it is "async" function, so "await" can be used if needed
@@ -114,7 +141,7 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
                     has_robot = True
                     prim_path = get_prim_path(prim)
                     print(f"articulation root found at {prim_path}")
-                    break
+
             if not has_robot:
                 print(f"failed to find articulation at {test_path}")
                 failed_robots.append(test_path)
@@ -151,7 +178,6 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
         clear_stage()
         await omni.kit.app.get_app().next_update_async()
         await omni.kit.app.get_app().next_update_async()
-        has_robot = False
         await menu_click(apriltag_menu_list[0], human_delay_speed=10)
         await omni.kit.app.get_app().next_update_async()
 
@@ -166,13 +192,11 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
 
     async def test_loading_environment(self):
 
-        self.robot_menu_dict = self.menu_dict["Create"]["Isaac"]["Environments"]
-        ## check everything under "Robot"
-
+        self.environment_menu_dict = self.menu_dict["Create"]["Isaac"]["Environments"]
+        ## check everything under "Environment"
         environment_root_path = "Create/Isaac/Environments"
 
         def get_menu_path(d, path, result, root_path):
-
             for key, value in d.items():
                 if key != "_":
                     new_path = path + "/" + str(key)
@@ -190,14 +214,14 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
         failed_environments = []
 
         empty_path = ""
-        robot_menu_list = get_menu_path(self.robot_menu_dict, empty_path, empty_list, environment_root_path)
+        environment_menu_list = get_menu_path(self.environment_menu_dict, empty_path, empty_list, environment_root_path)
+        try:
+            carb.windowing.acquire_windowing_interface()
+        except:
+            carb.log_error("unable to acquire windowing interface")
+            return
 
-        test_list = robot_menu_list
-
-        print(test_list)
-
-        for test_path in test_list:
-            print(test_path)
+        for test_path in environment_menu_list:
             if test_path in skip_list:
                 print("skipping ", test_path)
                 continue
@@ -205,38 +229,60 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
             clear_stage()
             await omni.kit.app.get_app().next_update_async()
             await omni.kit.app.get_app().next_update_async()
-            has_robot = False
+            # hide window to make sure it doesn't block the menu
+            self.viewport_window.visible = False
             await menu_click(test_path, human_delay_speed=10)
             for i in range(20):
                 await omni.kit.app.get_app().next_update_async()
 
-            # # waiting for stage to load
+            # waiting for stage to load
             while omni.usd.get_context().get_stage_loading_status()[2] > 0:
                 await omni.kit.app.get_app().next_update_async()
 
-            # count the numver of prims in the stage
+            self.viewport_window.visible = True
+            await omni.kit.app.get_app().next_update_async()
+            # the golden image test file name and test file name are the last section of the path
+            golden_img_name = test_path.split("/")[-1] + ".png"
+            viewport_api = get_active_viewport()
+            viewport_api.resolution = (1280, 720)
+            await omni.kit.app.get_app().next_update_async()
+            # set camera position
+            if "Office" in test_path or "Hospital" in test_path or "Warehouse" in test_path:
+                set_camera_view(eye=[2, -2, 2], target=[0, 0, 1])
+            else:
+                set_camera_view(eye=[3, -3, 3], target=[0, 0, 0])
+
+            # full screen the view port
+            await self.docked_test_window(window=get_active_viewport_window(), width=1920, height=1080)
+
+            for i in range(10):
+                await omni.kit.app.get_app().next_update_async()
+
+            # capture and compare with golden image
+            await self.finalize_test(golden_img_dir=self._golden_img_dir, golden_img_name=golden_img_name, threshold=10)
+            # count the number of prims in the stage
             num_prims = 0
             for prim in traverse_stage():
                 num_prims += 1
 
-            if num_prims == 0:
+            # There should be at least 9 prims
+            # `/Render/Vars/LdrColor` `/Render/Vars` `/Render/RenderProduct_omni_kit_widget_viewport_ViewportTexture_0`
+            # `/Render` `/OmniverseKit_Right` `/OmniverseKit_Top` `/OmniverseKit_Front` `/OmniverseKit_Persp`
+            # `at least 1 environment prim`
+
+            if num_prims < 9:
                 print(f"failed to find any prims at {test_path}")
                 failed_environments.append(test_path)
 
         print(failed_environments)
-
         # if failed_environments array has 0 entries, then test passed
         self.assertEqual(len(failed_environments), 0)
 
     async def test_loading_sensors(self):
-        print(self.menu_dict["Create"]["Isaac"])
-        self.robot_menu_dict = self.menu_dict["Create"]["Isaac"]["Sensors"]
-        ## check everything under "Sensors"
-
+        self.sensor_menu_dict = self.menu_dict["Create"]["Isaac"]["Sensors"]
         sensor_root_path = "Create/Isaac/Sensors"
 
         def get_menu_path(d, path, result, root_path):
-
             for key, value in d.items():
                 if key != "_":
                     new_path = path + "/" + str(key)
@@ -250,18 +296,16 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
             return result
 
         empty_list = []
-        skip_list = []
+        skip_list = ["Create/Isaac/Sensors/Ultrasonic/Emitter", "Create/Isaac/Sensors/Ultrasonic/FiringGroup"]
         failed_sensors = []
 
+        # each type of sensor will get a different prim type test, RGBD and RTX will check for camera prim
+        sensor_test_types = ["Rotating", "Generic", "Ultrasonic", "Contact", "Imu", "RGBD", "RTX"]
+
         empty_path = ""
-        robot_menu_list = get_menu_path(self.robot_menu_dict, empty_path, empty_list, sensor_root_path)
+        sensor_menu_list = get_menu_path(self.sensor_menu_dict, empty_path, empty_list, sensor_root_path)
 
-        test_list = robot_menu_list
-
-        print(test_list)
-
-        for test_path in test_list:
-            print(test_path)
+        for test_path in sensor_menu_list:
             if test_path in skip_list:
                 print("skipping ", test_path)
                 continue
@@ -269,7 +313,16 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
             clear_stage()
             await omni.kit.app.get_app().next_update_async()
             await omni.kit.app.get_app().next_update_async()
-            has_robot = False
+
+            for test_type in sensor_test_types:
+                if test_type in test_path:
+                    sensor_test = test_type
+
+            # IMU and contact sensors interface have to be attached to a rigid body parent to function
+            if sensor_test == "Contact" or sensor_test == "Imu":
+                cube = DynamicCuboid(prim_path="/Cube")
+                self.usd_selection.set_selected_prim_paths(["/Cube"], True)
+
             await menu_click(test_path, human_delay_speed=10)
             for i in range(20):
                 await omni.kit.app.get_app().next_update_async()
@@ -279,12 +332,43 @@ class TestMenuAssets(omni.kit.test.AsyncTestCase):
                 await omni.kit.app.get_app().next_update_async()
 
             num_prims = 0
-            # count the number of prims on the stage, shoudl be greater than 1
+            sensor_passed = False
+
+            # count the number of prims on the stage, should be greater than 1
+            # also for each prim, look for the matching type of sensor
             for prim in traverse_stage():
                 num_prims += 1
+                if sensor_test == "Rotating" and self.lidar_sensor_interface.is_lidar_sensor(get_prim_path(prim)):
+                    sensor_passed = True
 
-            if num_prims == 0:
-                print(f"failed to find any prims at {test_path}")
+                elif sensor_test == "Generic" and self.generic_sensor_interface.is_generic_sensor(get_prim_path(prim)):
+                    sensor_passed = True
+
+                elif sensor_test == "Ultrasonic" and self.ultrasonic_sensor_interface.is_ultrasonic_sensor(
+                    get_prim_path(prim)
+                ):
+                    sensor_passed = True
+
+                elif sensor_test == "Contact" and self.contact_sensor_interface.is_contact_sensor(get_prim_path(prim)):
+                    sensor_passed = True
+
+                elif sensor_test == "Imu" and self.imu_sensor_interface.is_imu_sensor(get_prim_path(prim)):
+                    sensor_passed = True
+
+                elif (sensor_test == "RGBD" or sensor_test == "RTX") and prim.IsA(UsdGeom.Camera):
+                    sensor_passed = True
+
+                if sensor_passed:
+                    break
+
+            # expected prims on stage
+            # `/Render/Vars/LdrColor` `/Render/Vars` `/Render/RenderProduct_omni_kit_widget_viewport_ViewportTexture_0`
+            # `/Render` `/OmniverseKit_Right` `/OmniverseKit_Top` `/OmniverseKit_Front` `/OmniverseKit_Persp`
+            # `at least 1 Sensor prim`
+            # if is_foo_sensor() is called on an non sensor prim, there maybe an error message generated that can be ignored
+
+            if not sensor_passed:
+                print(f"{test_path} did not pass, missing prim or wrong prim type")
                 failed_sensors.append(test_path)
 
         print(failed_sensors)
