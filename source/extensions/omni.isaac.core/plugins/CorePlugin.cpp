3968cae16d6752a1ae058c55d1caba9c3e8d3169
Milad Rakhsha <mrakhsha@nvidia.com>
2024-04-10 21:53:30 -0700

    Temporary fix for the ROS crash on develop

--- source/extensions/omni.isaac.core/plugins/CorePlugin.cpp
+++ source/extensions/omni.isaac.core/plugins/CorePlugin.cpp
@@ -12,40 +12,40 @@
 #include <isaacSim/Version.h>
 #include <omni/ext/IExt.h>
 
-namespace omni::isaac::core
-{
-class coreExt : public omni::ext::IExt
-{
-public:
-    void onStartup(const char* extId) override{};
-    void onShutdown() override{};
-};
-}
+// namespace omni::isaac::core
+// {
+// class coreExt : public omni::ext::IExt
+// {
+// public:
+//     void onStartup(const char* extId) override{};
+//     void onShutdown() override{};
+// };
+// }
 
-const struct carb::PluginImplDesc kPluginImpl = { "omni.isaac.core.plugin", "Core interface to Isaac sim", "NVIDIA",
-                                                  carb::PluginHotReload::eEnabled, "dev" };
-CARB_PLUGIN_IMPL(kPluginImpl, omni::isaac::core::coreExt)
+// const struct carb::PluginImplDesc kPluginImpl = { "omni.isaac.core.plugin", "Core interface to Isaac sim", "NVIDIA",
+//                                                   carb::PluginHotReload::eEnabled, "dev" };
+// CARB_PLUGIN_IMPL(kPluginImpl, omni::isaac::core::coreExt)
 
-void addCrashreporterMetadata()
-{
-    carb::crashreporter::addCrashMetadata("lib_isaacSim_buildVersion", ISAACSIM_BUILD_VERSION);
-    carb::crashreporter::addCrashMetadata(
-        "lib_isaacSim_buildRepo", "gitlab-master.nvidia.com/omniverse/isaac/omni_isaac_sim");
-    carb::crashreporter::addCrashMetadata("lib_isaacSim_buildHash", ISAACSIM_BUILD_SHA);
-    carb::crashreporter::addCrashMetadata("lib_isaacSim_buildBranch", ISAACSIM_BUILD_BRANCH);
-    carb::crashreporter::addCrashMetadata("lib_isaacSim_buildDate", ISAACSIM_BUILD_DATE);
-}
+// void addCrashreporterMetadata()
+// {
+//     carb::crashreporter::addCrashMetadata("lib_isaacSim_buildVersion", ISAACSIM_BUILD_VERSION);
+//     carb::crashreporter::addCrashMetadata(
+//         "lib_isaacSim_buildRepo", "gitlab-master.nvidia.com/omniverse/isaac/omni_isaac_sim");
+//     carb::crashreporter::addCrashMetadata("lib_isaacSim_buildHash", ISAACSIM_BUILD_SHA);
+//     carb::crashreporter::addCrashMetadata("lib_isaacSim_buildBranch", ISAACSIM_BUILD_BRANCH);
+//     carb::crashreporter::addCrashMetadata("lib_isaacSim_buildDate", ISAACSIM_BUILD_DATE);
+// }
 
-CARB_EXPORT void carbOnPluginStartup()
-{
-    addCrashreporterMetadata();
-}
+// CARB_EXPORT void carbOnPluginStartup()
+// {
+//     addCrashreporterMetadata();
+// }
 
-CARB_EXPORT void carbOnPluginShutdown()
-{
-}
+// CARB_EXPORT void carbOnPluginShutdown()
+// {
+// }
 
-void fillInterface(omni::isaac::core::coreExt& iface)
-{
-    iface = {};
-}
+// void fillInterface(omni::isaac::core::coreExt& iface)
+// {
+//     iface = {};
+// }
