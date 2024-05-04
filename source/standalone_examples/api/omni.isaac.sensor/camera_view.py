7f2517357e23b8bce73710b0fad4fb073a0da292
Andrew Guo <andrewg@nvidia.com>
2024-05-03 18:30:16 -0700

    Fix get_rgba() and get_depth for CameraView

--- source/standalone_examples/api/omni.isaac.sensor/camera_view.py
+++ source/standalone_examples/api/omni.isaac.sensor/camera_view.py
@@ -16,8 +16,9 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import omni.isaac.core.utils.numpy.rotations as rot_utils
+import omni.replicator.core as rep
 from omni.isaac.core import World
-from omni.isaac.core.objects import DynamicCuboid
+from omni.isaac.core.objects import VisualCuboid
 from omni.isaac.core.utils.prims import define_prim
 from omni.isaac.core.utils.render_product import set_camera_prim_path
 from omni.isaac.sensor import Camera, CameraView
@@ -26,42 +27,39 @@ from pxr import Gf, UsdGeom
 
 my_world = World(stage_units_in_meters=1.0)
 
-cube_2 = my_world.scene.add(
-    DynamicCuboid(
-        prim_path="/new_cube_2",
+cube_1 = my_world.scene.add(
+    VisualCuboid(
+        prim_path="/new_cube_1",
         name="cube_1",
-        position=np.array([5.0, 3, 1.0]),
-        scale=np.array([0.6, 0.5, 0.2]),
-        size=1.0,
-        color=np.array([255, 0, 0]),
-    )
-)
-
-cube_3 = my_world.scene.add(
-    DynamicCuboid(
-        prim_path="/new_cube_3",
-        name="cube_2",
         position=np.array([0, 0, 0.2]),
         scale=np.array([0.1, 0.1, 0.1]),
         size=1.0,
         color=np.array([0, 0, 255]),
-        linear_velocity=np.array([0, 0, 0.4]),
     )
 )
 
+cube_2 = my_world.scene.add(
+    VisualCuboid(
+        prim_path="/new_cube_2",
+        name="cube_2",
+        position=np.array([0, 0.5, 0.2]),
+        scale=np.array([0.1, 0.1, 0.1]),
+        size=1.0,
+        color=np.array([0, 0, 255]),
+    )
+)
 
-camera_01 = UsdGeom.Camera(define_prim(prim_path="/World/camera_01", prim_type="Camera"))
-camera_02 = UsdGeom.Camera(define_prim(prim_path="/World/camera_02", prim_type="Camera"))
-camera_03 = UsdGeom.Camera(define_prim(prim_path="/World/camera_03", prim_type="Camera"))
-
-camera_01.AddTranslateOp().Set(value=(0.0, 0.0, 20.0))
-camera_02.AddTranslateOp().Set(value=(0.0, 0.5, 2.0))
-camera_03.AddTranslateOp().Set(value=(1.0, 1.5, 10.0))
+camera_01 = rep.create.camera(position=(1, 1, 1), look_at=(0, 0, 0))
+camera_02 = rep.create.camera(position=(0, 1, 1), look_at=(0, 0, 0))
+camera_03 = rep.create.camera(position=(1, 1, 0), look_at=(0, 0, 0))
+camera_04 = rep.create.camera(position=(1, 0, 1), look_at=(0, 0, 0))
 
 my_world.scene.add_default_ground_plane()
 my_world.reset()
 
-camera_view = CameraView(name="camera_prim_view", prim_paths_expr="/World/camera_*")
+camera_view = CameraView(
+    name="camera_prim_view", prim_paths_expr="/Replicator/Camera_Xform*/Camera", output_annotators=["rgb", "depth"]
+)
 
 i = 0
 os.makedirs("output_camera_view", exist_ok=True)
@@ -69,11 +67,12 @@ while simulation_app.is_running():
     my_world.step(render=True)
 
     rgba = camera_view.get_rgba().astype(np.uint8)
-    print(rgba.shape)
+    print(f"rgba.shape: {rgba.shape}")
     rgba_img = Image.fromarray(rgba)
     rgba_img.save(f"output_camera_view/{str(i).zfill(6)}_rgba.png")
 
     depth = camera_view.get_depth().astype(np.uint8)
+    print(f"depth.shape: {depth.shape}")
 
     depth_img = Image.fromarray(depth)
     depth_img.save(f"output_camera_view/{str(i).zfill(6)}_depth.png")
