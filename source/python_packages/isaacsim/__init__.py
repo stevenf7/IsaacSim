417a1ea56047495076605fa2ec7114b7b37d424c
Antonio Serrano Munoz <aserranomuno@nvidia.com>
2024-05-22 15:43:47 -0700

    Migrate isaacsim-kernel python package to Isaac Sim repo

--- source/python_packages/isaacsim/__init__.py
+++ source/python_packages/isaacsim/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2023, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2018-2024, NVIDIA CORPORATION. All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
 # and proprietary rights in and to this software, related documentation
@@ -7,37 +7,65 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 #
 
+import ctypes
 import os
+import sys
 
-if not os.getenv("CARB_APP_PATH"):
-    import ctypes
-    import os
-    import sys
 
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    root_path = os.path.normpath(os.path.join(root_path, "..", ".."))
+def bootstrap_kernel():
+    using_inner_kernel = False
 
-    # preload carb libraries
-    if sys.platform == "win32":
-        ctypes.PyDLL(os.path.join(root_path, "kit", "carb.dll"), mode=ctypes.RTLD_GLOBAL)
+    # isaac-sim path
+    isaacsim_path = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+
+    # check if it is a non-Python package manager installation
+    split_path = isaacsim_path.split(os.sep)
+    if len(split_path) >= 2 and split_path[-1] == "isaacsim" and split_path[-2] == "python_packages":
+        return
+
+    # kit path (internal kernel)
+    if os.path.isdir(os.path.join(isaacsim_path, "kit", "extscore")):
+        kit_path = os.path.join(isaacsim_path, "kit")
+        using_inner_kernel = True
+    # kit path (omniverse-kit kernel package)
     else:
-        ctypes.PyDLL(os.path.join(root_path, "kit", "libcarb.so"), mode=ctypes.RTLD_GLOBAL)
+        try:
+            import omni.kit_app  # importing 'omni.kit_app' will bootstrap kernel
+
+            kit_path = os.path.dirname(os.path.abspath(os.path.realpath(omni.kit_app.__file__)))
+        except ModuleNotFoundError:
+            print("Unable to find 'omniverse-kit' package")
+            exit()
+
+    # preload libcarb.so
+    if using_inner_kernel:
+        carb_library = "carb.dll" if sys.platform == "win32" else "libcarb.so"
+        ctypes.PyDLL(os.path.join(kit_path, carb_library), mode=ctypes.RTLD_GLOBAL)
 
     # set environment variables
     if not os.environ.get("CARB_APP_PATH", None):
-        os.environ["CARB_APP_PATH"] = os.path.join(root_path, "kit")
+        os.environ["CARB_APP_PATH"] = kit_path
     if not os.environ.get("EXP_PATH", None):
-        os.environ["EXP_PATH"] = os.path.join(root_path, "apps")
+        os.environ["EXP_PATH"] = os.path.join(isaacsim_path, "apps")
     if not os.environ.get("ISAAC_PATH", None):
-        os.environ["ISAAC_PATH"] = os.path.join(root_path)
+        os.environ["ISAAC_PATH"] = os.path.join(isaacsim_path)
+
+    # set environment variables (Jupyter)
+    if os.environ.get("JPY_PARENT_PID", None):
+        os.environ["ISAAC_JUPYTER_PYTHON_PACKAGE"] = "1"
 
     # set PYTHONPATH
-    paths = [
-        # kit
-        os.path.join(root_path, "kit", "kernel", "py"),
-        # isaac-sim
-        os.path.join(root_path, "exts", "omni.isaac.kit"),
+    paths = []
+    # kit
+    if using_inner_kernel:
+        paths += [
+            os.path.join(kit_path, "kernel", "py"),
+        ]
+    # isaac-sim
+    paths += [
+        os.path.join(isaacsim_path, "exts", "omni.isaac.kit"),
     ]
+    # update sys.path
     for path in paths:
         if not path in sys.path:
             if not os.path.exists(path):
@@ -45,6 +73,68 @@ if not os.getenv("CARB_APP_PATH"):
                 continue
             sys.path.insert(0, path)
 
+    # log info
+    import carb
 
-# expose Isaac Sim API
-from omni.isaac.kit import AppFramework, SimulationApp
+    carb.log_info(f"Isaac Sim path: {isaacsim_path}")
+    carb.log_info(f"Kit path: {kit_path}")
+    carb.log_info(f"Using inner kernel: {using_inner_kernel}")
+
+
+def main():
+    args = sys.argv[1:]
+    using_inner_kernel = False
+
+    # get paths
+    # isaac-sim path
+    isaacsim_path = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+    # kit path (internal kernel)
+    if os.path.isdir(os.path.join(isaacsim_path, "kit", "extscore")):
+        kit_path = os.path.join(isaacsim_path, "kit")
+        using_inner_kernel = True
+    # kit path (omniverse-kit kernel package)
+    else:
+        try:
+            import omni.kit_app  # importing 'omni.kit_app' will bootstrap kernel
+
+            kit_path = os.path.dirname(os.path.abspath(os.path.realpath(omni.kit_app.__file__)))
+        except ModuleNotFoundError:
+            print("Unable to find 'omniverse-kit' package")
+            exit()
+
+    # experience file
+    experience = args[0] if len(args) and not args[0].startswith("-") else "omni.app.mini"
+    experience = experience if experience.endswith(".kit") else f"{experience}.kit"
+    if not os.path.isfile(experience):
+        for experience_dir in [os.path.join(isaacsim_path, "apps"), os.path.join(kit_path, "apps")]:
+            if os.path.isfile(os.path.join(experience_dir, experience)):
+                experience = os.path.join(experience_dir, experience)
+                if len(args) and not args[0].startswith("-"):
+                    args = args[1:]
+                break
+    if not os.path.isfile(experience):
+        print(f"Invalid experience (.kit) file: {args[0] if len(args) else ''}")
+        exit()
+
+    # launch app
+    if using_inner_kernel:
+        sys.path.append(kit_path)
+        from kit_app import KitApp
+    else:
+        from omni.kit_app import KitApp
+
+    app = KitApp()
+    app.startup([experience, "--ext-folder", os.path.join(isaacsim_path, "apps")] + args)
+    while app.is_running():
+        app.update()
+    sys.exit(app.shutdown())
+
+
+bootstrap_kernel()
+
+
+# make omni.isaac.kit discoverable
+try:
+    from omni.isaac.kit import AppFramework, SimulationApp
+except ImportError:
+    AppFramework, SimulationApp = None, None
