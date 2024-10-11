b1947c6744b6257502e468cf0d88e75c1356b6f8
Hammad Mazhar <hmazhar@nvidia.com>
2024-10-11 14:39:27 -0700

    Cleanup all python copyrights

--- source/deprecated/omni.isaac.motion_generation/omni/isaac/motion_generation/world_interface.py
+++ source/deprecated/omni.isaac.motion_generation/omni/isaac/motion_generation/world_interface.py
@@ -5,6 +5,7 @@
 # and any modifications thereto. Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
 
 import carb
 
