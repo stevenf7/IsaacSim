844b04b7db8f39d4ae862325543dc0c0cc35a2f3
Hammad Mazhar <hmazhar@nvidia.com>
2024-10-11 15:20:29 -0700

    move assets_check to deprecated folder

--- source/deprecated/omni.isaac.assets_check/premake5.lua
+++ source/deprecated/omni.isaac.assets_check/premake5.lua
@@ -1,5 +1,6 @@
 -- Use folder name to build extension name and tag. 
 local ext = get_current_extension_info()
+ext.target_dir = deprecated_exts_path.."/"..ext.id
 
 project_ext (ext, { 
     define_test = false
