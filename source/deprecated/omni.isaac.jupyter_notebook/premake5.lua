cc3c6107d41bbc3d16b35ec106aa1d85aa289c73
Hammad Mazhar <hmazhar@nvidia.com>
2024-10-23 00:15:32 -0700

    Cleaning up and updating extension cache and kit files

--- source/deprecated/omni.isaac.jupyter_notebook/premake5.lua
+++ source/deprecated/omni.isaac.jupyter_notebook/premake5.lua
@@ -1,7 +1,9 @@
 local ext = get_current_extension_info()
 ext.target_dir = deprecated_exts_path.."/"..ext.id
 
-project_ext (ext)
+project_ext (ext, { 
+    define_test = false
+})
 
 repo_build.prebuild_link {
     { "docs", ext.target_dir.."/docs" },
