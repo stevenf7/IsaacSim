51ee0a89f99ad4b4e3882d4c0c0a6e73ad28237c
Qian Wan <qwan@nvidia.com>
2024-11-06 16:04:37 -0800

    reviving import wizard [ISIM-2272]

--- source/extensions/isaacsim.robot_setup.import_wizard/isaacsim/robot_setup/import_wizard/tests/__init__.py
+++ source/extensions/isaacsim.robot_setup.import_wizard/isaacsim/robot_setup/import_wizard/tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2024, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2018-2023, NVIDIA CORPORATION. All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
 # and proprietary rights in and to this software, related documentation
@@ -6,9 +6,5 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 #
-import carb
 
-old_extension_name = "omni.isaac.import_wizard"
-new_extension_name = "isaacsim.robot_setup.import_wizard"
-# Provide deprecation warning to user
-carb.log_warn(f"{old_extension_name} has been deprecated in favor of {new_extension_name}.")
+scan_for_test_modules = True
