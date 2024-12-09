8c548f1dd23dcace6bad4efd85d473821ca67d75
Rafael Wiltz <rwiltz@nvidia.com>
2024-12-09 11:29:58 -0800

    Using instanced cubes to avoid performance issues

--- source/standalone_examples/api/isaacsim.xr.openxr/hand_tracking/hand_tracking_sample.py
+++ source/standalone_examples/api/isaacsim.xr.openxr/hand_tracking/hand_tracking_sample.py
@@ -19,35 +19,49 @@ simulation_app = SimulationApp(
 from isaacsim.core.api import World
 from isaacsim.core.api.materials.omni_pbr import OmniPBR
 from isaacsim.core.api.objects import VisualCuboid
+from isaacsim.core.utils.prims import create_prim, set_prim_visibility
 from isaacsim.xr.openxr import OpenXR, OpenXRSpec
+from omni.isaac.core.prims import XFormPrim
+from pxr import Gf, Sdf, Usd, UsdGeom
 
 openxr = OpenXR()
 my_world = World(stage_units_in_meters=1.0)
 
+hidden_prim = create_prim("/Hidden/Prototypes", "Scope")
+base_cube_path = "/Hidden/Prototypes/BaseCube"
+VisualCuboid(
+    prim_path=base_cube_path,
+    size=0.01,
+    color=np.array([255, 0, 0]),
+)
+set_prim_visibility(hidden_prim, False)
 
-for joint_idx in range(int(OpenXRSpec.HandJointEXT.XR_HAND_JOINT_LITTLE_TIP_EXT) + 1):
-    my_world.scene.add(
-        VisualCuboid(
-            prim_path=f"/left_cube_{joint_idx}",
-            name=f"left_cube_{joint_idx}",
-            position=np.array([0, 0, 0.5]),
-            size=0.01,
-            color=np.array([255, 0, 0]),
-        )
-    )
+instancer_path = "/World/CubeInstancer"
+point_instancer = UsdGeom.PointInstancer.Define(my_world.stage, instancer_path)
+point_instancer.CreatePrototypesRel().SetTargets([Sdf.Path(base_cube_path)])
 
-    my_world.scene.add(
-        VisualCuboid(
-            prim_path=f"/right_cube_{joint_idx}",
-            name=f"right_cube_{joint_idx}",
-            position=np.array([0, 0, 0.5]),
-            size=0.01,
-            color=np.array([255, 0, 0]),
-        )
-    )
+hand_joint_count = int(OpenXRSpec.HandJointEXT.XR_HAND_JOINT_LITTLE_TIP_EXT) + 1
+joint_count = hand_joint_count * 2
+
+# Initially hide all cubes until hands are tracked
+point_instancer.CreateProtoIndicesAttr().Set([1 for _ in range(joint_count)])
+
+positions = [Gf.Vec3f(0.0, 0.0, 0.0) for i in range(joint_count)]
+point_instancer.CreatePositionsAttr().Set(positions)
+
+orientations = [Gf.Quath(1.0, 0.0, 0.0, 0.0) for _ in range(joint_count)]
+point_instancer.CreateOrientationsAttr().Set(orientations)
+
+instancer_prim = XFormPrim(prim_path=instancer_path)
+my_world.scene.add(instancer_prim)
 
 my_world.reset()
 reset_needed = False
+
+positions_attr = point_instancer.GetPositionsAttr()
+orientations_attr = point_instancer.GetOrientationsAttr()
+proto_idx_attr = point_instancer.GetProtoIndicesAttr()
+
 while simulation_app.is_running():
     my_world.step(render=True)
     if my_world.is_stopped() and not reset_needed:
@@ -57,26 +71,34 @@ while simulation_app.is_running():
             my_world.reset()
             reset_needed = False
 
-        def update_joints(joints, prim_prefix):
-            if joints is not None:
-                for joint_idx in range(int(OpenXRSpec.HandJointEXT.XR_HAND_JOINT_LITTLE_TIP_EXT) + 1):
-                    location_flags = joints[joint_idx].locationFlags
-                    if (
-                        location_flags & OpenXRSpec.XR_SPACE_LOCATION_POSITION_VALID_BIT
-                        and location_flags & OpenXRSpec.XR_SPACE_LOCATION_ORIENTATION_VALID_BIT
-                    ):
-                        joint_pos = joints[joint_idx].pose.position
-                        joint_quat = joints[joint_idx].pose.orientation
-                        obj = my_world.scene.get_object(f"{prim_prefix}{joint_idx}")
-                        obj.set_world_pose(
-                            (joint_pos.x, joint_pos.y, joint_pos.z),
-                            (joint_quat.w, joint_quat.x, joint_quat.y, joint_quat.z),
-                        )
-                        obj.set_visibility(True)
-                    else:
-                        my_world.scene.get_object(f"{prim_prefix}{joint_idx}").set_visibility(False)
+        current_positions = positions_attr.Get()
+        current_orientations = orientations_attr.Get()
+        proto_indices = proto_idx_attr.Get()
 
-        update_joints(openxr.locate_hand_joints(OpenXRSpec.XrHandEXT.XR_HAND_LEFT_EXT), "left_cube_")
-        update_joints(openxr.locate_hand_joints(OpenXRSpec.XrHandEXT.XR_HAND_RIGHT_EXT), "right_cube_")
+        left_joints = openxr.locate_hand_joints(OpenXRSpec.XrHandEXT.XR_HAND_LEFT_EXT) or [None] * hand_joint_count
+        right_joints = openxr.locate_hand_joints(OpenXRSpec.XrHandEXT.XR_HAND_RIGHT_EXT) or [None] * hand_joint_count
+        joints = left_joints + right_joints
+
+        for joint_idx in range(joint_count):
+            if joints[joint_idx] is not None:
+                location_flags = joints[joint_idx].locationFlags
+
+                if (
+                    location_flags & OpenXRSpec.XR_SPACE_LOCATION_POSITION_VALID_BIT
+                    and location_flags & OpenXRSpec.XR_SPACE_LOCATION_ORIENTATION_VALID_BIT
+                ):
+                    joint_pos = joints[joint_idx].pose.position
+                    joint_quat = joints[joint_idx].pose.orientation
+                    current_positions[joint_idx] = Gf.Vec3f(joint_pos.x, joint_pos.y, joint_pos.z)
+                    current_orientations[joint_idx] = Gf.Quath(joint_quat.w, joint_quat.x, joint_quat.y, joint_quat.z)
+                    proto_indices[joint_idx] = 0
+                else:
+                    proto_indices[joint_idx] = 1
+            else:
+                proto_indices[joint_idx] = 1
+
+        positions_attr.Set(current_positions)
+        orientations_attr.Set(current_orientations)
+        proto_idx_attr.Set(proto_indices)
 
 simulation_app.close()
