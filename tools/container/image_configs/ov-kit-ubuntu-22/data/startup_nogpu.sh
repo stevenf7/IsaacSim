e7aaf6f66ae1895de2d5278022bb10ef926c5b84
Sheikh Dawood <sabdulajees@nvidia.com>
2025-05-14 08:41:33 -0500

    [ci] Remove base containers

--- tools/container/image_configs/ov-kit-ubuntu-22/data/startup_nogpu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -u
-
-echo 'Running: [...]kit in Non-GPU mode, run startup.sh if a GPU is required.' $@
-
-exec "/opt/nvidia/omniverse/kit-sdk-launcher/kit" \
-    "--no-window" \
-    $@
