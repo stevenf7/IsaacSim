a70fef30d8bce0ca095d3348788e1f48ad7a11be
Ben Johnston <benj@nvidia.com>
2025-05-27 11:12:56 -0700

    Update check externals to only use a single linux runner!!

---
+++ tools/check_externals/__init__.py
@@ -0,0 +1,4 @@
+from .check_externals import gather_package_data, verify_externals, write_output_files
+from .license_finder import find_license_file
+
+__all__ = ["verify_externals", "gather_package_data", "write_output_files", "find_license_file"]
