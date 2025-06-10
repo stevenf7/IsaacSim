cc0f1b50e47ef069597116f3926373cbbe5c651b
Hammad Mazhar <hmazhar@nvidia.com>
2025-06-09 17:32:38 -0700

    Add missing licenses

--- premake5-tests.lua
+++ premake5-tests.lua
@@ -1,3 +1,18 @@
+-- SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- Function to define test startup experience with app name, kit file, and optional extra arguments
 function define_test_startup_experience(app_name, kit_file, extra_args)
     local script_dir_token = (os.target() == "windows") and "%~dp0" or "$SCRIPT_DIR"
