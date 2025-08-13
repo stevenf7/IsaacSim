d6db6fc0c957fa96c7bee3ef38cab536f89ab908
Sachin Sulkunte <ssulkunte@nvidia.com>
2025-08-13 16:05:04 -0700

    [ISIM-4223] Remove viewport update disable default from scene loading

--- source/standalone_examples/benchmarks/benchmark_scene_loading.py
+++ source/standalone_examples/benchmarks/benchmark_scene_loading.py
@@ -29,7 +29,6 @@ parser.add_argument(
 )
 parser.add_argument("--gpu-frametime", action="store_true", help="Enable GPU frametime measurement")
 parser.add_argument("--non-headless", action="store_false", help="Run with GUI - nonheadless mode")
-parser.add_argument("--viewport-updates", action="store_false", help="Enable viewport updates when headless")
 parser.add_argument("--num-gpus", type=int, default=None, help="Number of GPUs on machine.")
 parser.add_argument(
     "--backend-type",
@@ -57,9 +56,7 @@ if (cam_pos and not cam_target) or (cam_target and not cam_pos):
 import numpy as np
 from isaacsim import SimulationApp
 
-simulation_app = SimulationApp(
-    {"headless": headless, "max_gpu_count": n_gpu, "disable_viewport_updates": viewport_updates}
-)
+simulation_app = SimulationApp({"headless": headless, "max_gpu_count": n_gpu})
 
 import carb
 import omni
