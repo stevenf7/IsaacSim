c355ad2a8aa030e4e41e65cbb559f225fae886b3
Antonio Serrano Munoz <aserranomuno@nvidia.com>
2025-10-23 13:07:08 -0700

    [ISIM-3078] Expose function to import a deprecated/removed module safely

---
+++ source/extensions/isaacsim.core.deprecation_manager/isaacsim/core/deprecation_manager/api.py
@@ -0,0 +1,70 @@
+import importlib
+import sys
+from types import ModuleType
+
+import carb
+import omni.kit.app
+
+
+def import_module(name: str) -> ModuleType:
+    """Try to import a Python package/module and return it.
+
+    If a package or module is not found, an error message will be logged and the application will exit.
+    A notice with further instructions will also be logged for the following packages:
+
+    - ``torch``
+
+    Args:
+        name: The name of the package/module to import.
+
+    Example:
+
+    .. code-block:: python
+
+        >>> from isaacsim.core.deprecation_manager import import_module
+        >>>
+        >>> numpy = import_module("numpy")
+    """
+
+    def exit_app():
+        # test mode
+        if carb.settings.get_settings().get_as_bool("/exts/omni.kit.test/runTestsAndQuit"):
+            sys.exit(1)
+        omni.kit.app.get_app().shutdown()
+
+    # PyTorch
+    if name == "torch":
+        try:
+            return importlib.import_module(name)
+        except (ModuleNotFoundError, ImportError) as e:
+            msg = """
+============================================================================
+========================== IMPLEMENTATION NOTICE ===========================
+============================================================================
+
+PyTorch (torch) dependency is not installed/enabled by default in Isaac Sim.
+Please, follow the instructions below to install and use it.
+
+For a specific PyTorch version, see: https://pytorch.org/get-started/locally
+----------------------------------------------------------------------------
+
+ * Isaac Sim - Binary installation (Linux: ./python.sh, Windows: python.bat):
+  
+    ./python.sh -m pip install torch
+  
+ * Isaac Sim - Python Packages (pip) installation:
+  
+    pip install torch
+
+============================================================================
+"""
+            carb.log_error(f"Import error: {str(e)}")
+            carb.log_warn(msg)
+            exit_app()
+    # any other module
+    else:
+        try:
+            return importlib.import_module(name)
+        except (ModuleNotFoundError, ImportError) as e:
+            carb.log_error(f"Import error: {str(e)}")
+            exit_app()
