4354cde0fd7e10b3bf9cef028dd7624709e81d5c
Rushane Hua <rhua@nvidia.com>
2025-11-18 10:20:46 -0800

    [ISIM-4707] UCX bridge extension

---
+++ source/extensions/isaacsim.ucx.bridge/premake5.lua
@@ -0,0 +1,37 @@
+-- SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+if os.target() == "linux" then
+
+    local ext = get_current_extension_info()
+
+    -- This is now a pure Python umbrella extension
+    -- All C++ functionality has been moved to isaacsim.ros2.core and isaacsim.ros2.nodes
+    project_ext(ext)
+
+    -- Copy/link necessary files for packaging
+    repo_build.prebuild_link {
+        { "python/impl", ext.target_dir .. "/isaacsim/ucx/bridge/impl" },
+        { "docs", ext.target_dir .. "/docs" },
+        { "data", ext.target_dir .. "/data" },
+    }
+
+    repo_build.prebuild_copy {
+        { "python/*.py", ext.target_dir .. "/isaacsim/ucx/bridge" },
+    }
+else
+	print("SKIPPING BUILD - Only supported on linux-x86_64")
+end
+
