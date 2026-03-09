86c809ed41d260040536f132df94e70da0cd34cd
Avinash Devalla <adevalla@nvidia.com>
2026-03-09 11:01:56 -0700

    [5956235] Fix indentation in RTX Radar snippet.

--- docs/isaacsim/snippets/sensors/isaacsim_sensors_rtx_radar/create_an_rtx_radar_using_command.py
+++ docs/isaacsim/snippets/sensors/isaacsim_sensors_rtx_radar/create_an_rtx_radar_using_command.py
@@ -7,12 +7,7 @@ sensor_attributes = {"omni:sensor:tickRate": 10}
 _, sensor = omni.kit.commands.execute(
     "IsaacSensorCreateRtxRadar",
     translation=Gf.Vec3d(0, 0, 0),
-    orientation=Gf.Quatd(
-        1,
-        0,
-        0,
-        0,
-    ),
+    orientation=Gf.Quatd(1, 0, 0, 0),
     path="/radar",
     parent=None,
     visibility=False,
