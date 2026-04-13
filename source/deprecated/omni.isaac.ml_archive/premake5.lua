b5f83f17fd49ea612e161030d80813a00877c7bc
Antonio Serrano Munoz <aserranomuno@nvidia.com>
2026-04-13 15:28:19 -0700

    [ISIM-4660] Deprecate omni.isaac.ml_archive extension

--- source/deprecated/omni.isaac.ml_archive/premake5.lua
+++ source/deprecated/omni.isaac.ml_archive/premake5.lua
@@ -14,7 +14,7 @@
 -- limitations under the License.
 
 local ext = get_current_extension_info()
-
+ext.target_dir = deprecated_exts_path .. "/" .. ext.id
 project_ext(ext)
 
 repo_build.prebuild_link {
