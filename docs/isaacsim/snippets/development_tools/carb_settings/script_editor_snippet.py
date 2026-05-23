870605716353b1e428be93172327a80edfb7e2e7
Hammad Mazhar <hmazhar@nvidia.com>
2026-05-23 10:55:54 -0700

    Fix random test failures

--- docs/isaacsim/snippets/development_tools/carb_settings/script_editor_snippet.py
+++ docs/isaacsim/snippets/development_tools/carb_settings/script_editor_snippet.py
@@ -3,8 +3,9 @@ import omni.kit
 
 ## Set Carb Setting
 settings = carb.settings.get_settings()
-settings.set("/exts/isaacsim.my.extension/data/foo", True)
+settings.set("/exts/isaacsim.code_editor.python_server/keepalive_interval", 5)
 
 ## Restart Extension to Apply Changes
-omni.kit.app.get_app().get_extension_manager().set_extension_enabled_immediate("isaacsim.my.extension", False)
-omni.kit.app.get_app().get_extension_manager().set_extension_enabled_immediate("isaacsim.my.extension", True)
+extension_manager = omni.kit.app.get_app().get_extension_manager()
+extension_manager.set_extension_enabled_immediate("isaacsim.code_editor.python_server", False)
+extension_manager.set_extension_enabled_immediate("isaacsim.code_editor.python_server", True)
